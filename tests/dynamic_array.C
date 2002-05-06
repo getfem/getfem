7d5c1c9365da39572c8b1a9d53017ede34fc33e0
Yves Renard <yves.renard@insa-lyon.fr>
2002-05-06 11:45:11 +0000

    *** empty log message ***

--- tests/dynamic_array.C
+++ tests/dynamic_array.C
@@ -1,54 +1,54 @@
 #include <dal_basic.h>
 #include <deque>
 
-using namespace std;
+// using namespace std;
 
 int main(void)
 {
   
-  cout << "size of int : " << sizeof(int) << endl;
-  cout << "size of size_t : " << sizeof(size_t) << endl;
-  cout << "size of (int *) : " << sizeof(int *) << endl;
-  cout << "size of short int : " << sizeof(short int) << endl;
-  cout << "size of long int : " << sizeof(long int) << endl;
-  cout << "size of char : " << sizeof(char) << endl;
-  cout << "size of float : " << sizeof(float) << endl;
-  cout << "size of double : " << sizeof(double) << endl;
-  cout << "size of long double : " << sizeof(long double) << endl;
+  std::cout << "size of int : " << sizeof(int) << endl;
+  std::cout << "size of size_t : " << sizeof(size_t) << endl;
+  std::cout << "size of (int *) : " << sizeof(int *) << endl;
+  std::cout << "size of short int : " << sizeof(short int) << endl;
+  std::cout << "size of long int : " << sizeof(long int) << endl;
+  std::cout << "size of char : " << sizeof(char) << endl;
+  std::cout << "size of float : " << sizeof(float) << endl;
+  std::cout << "size of double : " << sizeof(double) << endl;
+  std::cout << "size of long double : " << sizeof(long double) << endl;
 
   dal::dynamic_array<int, 4> t;
 
   t[64] = 13;
-  cout << "capacity : (should be 80) " << t.capacity() << endl;
+  std::cout << "capacity : (should be 80) " << t.capacity() << endl;
   assert(t.capacity() == 80);
 
   dal::dynamic_array<int, 4>::iterator itb = t.begin(), ite = t.end();
   dal::dynamic_array<int, 4>::iterator ita;
   ita = itb++;
-  cout << "range : " << (ita - t.begin()) << endl;
+  std::cout << "range : " << (ita - t.begin()) << endl;
 
   while (itb != ite)
   {
-    // cout << (itb - t.begin()) << " : " << endl;
+    // std::cout << (itb - t.begin()) << " : " << endl;
     *itb++ = int(3);
     //   itb++;
-    // cout << (itb - t.begin()) << endl; 
+    // std::cout << (itb - t.begin()) << endl; 
   }
 
   // std::fill(t.begin(), t.end(), int(3));
 
-  cout << "capacity : (should be 80) " << t.capacity() << endl;
+  std::cout << "capacity : (should be 80) " << t.capacity() << endl;
   assert(t.capacity() == 80);
-  cout << "t[64] = (should be 3) " << t[64] << endl;
+  std::cout << "t[64] = (should be 3) " << t[64] << endl;
   assert(t[64] == 3);
 
 
   t.clear();
-  cout << "capacity : (should be 0) " << t.capacity() << endl;
+  std::cout << "capacity : (should be 0) " << t.capacity() << endl;
   assert(t.capacity() == 0);
   std::fill(t.begin(), t.end(), int(3));
 
-  cout << "capacity : (should be 0) " << t.capacity() << endl;
+  std::cout << "capacity : (should be 0) " << t.capacity() << endl;
   assert(t.capacity() == 0);
 
   t[64] = 6;
@@ -58,7 +58,7 @@ int main(void)
   t2[64] = 12;
   t3 = t2 = t;
 
-  cout << "capacity : (should be 80) " << t.capacity() << endl;
+  std::cout << "capacity : (should be 80) " << t.capacity() << endl;
   assert(t.capacity() == 80);
 
   {
@@ -72,7 +72,7 @@ int main(void)
     for ( ; it1 != ite; it1++, it2++, it3++)
     {
       size_t ind = it1 - itb;
-//       cout << "ind : " << ind
+//       std::cout << "ind : " << ind
 // 	   << " pointers : " << &(*it1) << " : " << &(*it2)
 // 	   << " : " << &(*it3) << endl;
       
@@ -96,7 +96,7 @@ int main(void)
     for ( ; it1 != ite; it1++, it2++, it3++)
     {
       size_t ind = it1 - itb;
-   //    cout << "ind : " << ind
+   //    std::cout << "ind : " << ind
 // 	   << " pointers : " << &(*it1) << " : " << &(*it2)
 // 	   << " : " << &(*it3) << endl;
       
@@ -114,9 +114,9 @@ int main(void)
 
   t[64] = 11;
   
-  cout << "t2[64] = (should be 6 6) " << t2[64] << " " << t3[64] << endl;
+  std::cout << "t2[64] = (should be 6 6) " << t2[64] << " " << t3[64] << endl;
   assert(t2[64] == 6); assert(t3[64] == 6);
-  cout << "capacity : (should be 80) " << t3.capacity() << endl;
+  std::cout << "capacity : (should be 80) " << t3.capacity() << endl;
   assert(t.capacity() == 80);
   
   
