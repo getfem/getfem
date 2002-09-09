a1af7e8d3f6bbc513d06110e5e1bdeac8776fca3
Yves Renard <yves.renard@insa-lyon.fr>
2002-09-09 10:34:06 +0000

    *** empty log message ***

--- tests/geo_trans_inv.C
+++ tests/geo_trans_inv.C
@@ -1,12 +1,18 @@
 #include <bgeot_geotrans_inv.h>
 
 using bgeot::size_type;
+using bgeot::dim_type;
+using bgeot::scalar_type;
+
+dim_type N, MESH_TYPE;
+scalar_type LX, LY, LZ;
+size_type NX;
 
 int main(void)
 {
   try {
     
-
+    
     
     
   }
