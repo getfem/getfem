d28194b70886f2f9ee2e626dafd3fc7a9b6d929b
Julien Pommier <pommier@pianoteq.com>
2003-02-03 10:24:57 +0000

    correction bug sur la recherche d'aretes

--- src/getfem_export.C
+++ src/getfem_export.C
@@ -87,24 +87,39 @@ namespace getfem
       while (ncs != 0) {
 	ncs--;
 	cvs = cvstab[ncs];
+	std::vector< size_type > pts = indpttab[ncs];
 	if (cvs->dim() == 1) { // il faudrait étendre aux autres cas classiques.
 	  
-	  for (size_type j = 1; j < cvs->nb_points(); ++j)
+	  for (size_type j = 1; j < cvs->nb_points(); ++j) {
+	    cerr << "ncs=" << ncs << "j=" << j << ", ajout de " << (indpttab[ncs])[j-1] << "," << (indpttab[ncs])[j] << endl;
 	    el.add(edge_list_elt((indpttab[ncs])[j-1],(indpttab[ncs])[j],ncv));
+	  }
 	}
 	else {
 	  size_type nf = cvs->nb_faces();
+	  cerr << "ncs = " << ncs << ",cvs->dim=" << int(cvs->dim()) << ", nb_faces=" << nf << endl;
 	  for (size_type f = 0; f < nf; ++f) {
 	    if (cvs->dim() > 2) ++f;
 	    cvstab[ncs+f] = (cvs->faces_structure())[f];
 	    indpttab[ncs+f].resize(cvs->nb_points_of_face(f));
+
+
+	    cerr << "   -> f=" << f << ", cvs->nb_points_of_face(f)=" << 
+	      cvs->nb_points_of_face(f) << "=[";
+	    for (size_type k = 0; k < cvs->nb_points_of_face(f); ++k) 
+	      cerr << (std::string(k==0?"":",")) 
+		   << int(cvs->ind_points_of_face(f)[k]) << "{" << pts[cvs->ind_points_of_face(f)[k]] << "}";
+	    cerr << "]" << endl;
+
+
 	    for (size_type k = 0; k < cvs->nb_points_of_face(f); ++k)
 	      (indpttab[ncs+f])[k]
-		= (indpttab[ncs])[(cvs->ind_points_of_face(f))[k]];
+		//		= (indpttab[ncs])[(cvs->ind_points_of_face(f))[k]];
+		= pts[cvs->ind_points_of_face(f)[k]];
 	  }
-	  cvstab[ncs] = cvstab[ncs + nf - 1];
-	  indpttab[ncs] = indpttab[ncs + nf - 1];
-	  ncs += nf - 1;
+	  //	  cvstab[ncs] = cvstab[ncs + nf - 1];
+	  //indpttab[ncs] = indpttab[ncs + nf - 1];
+	  ncs += nf;
 	}
       }
     }
