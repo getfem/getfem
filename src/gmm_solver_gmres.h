2372778d57cd6d6328efa21a84a4cc4bcf32fb31
Yves Renard <yves.renard@insa-lyon.fr>
2003-02-10 13:05:36 +0000

    *** empty log message ***

--- src/gmm_solver_gmres.h
+++ src/gmm_solver_gmres.h
@@ -54,116 +54,97 @@
 /*                                                                         */
 /* *********************************************************************** */
 
-#ifndef ITL_KRYLOV_GMRES_H
-#define ITL_KRYLOV_GMRES_H
+#ifndef GMM_KRYLOV_GMRES_H
+#define GMM_KRYLOV_GMRES_H
 
-#include <vector>
-#include <algorithm>
-#include "itl/itl.h"
-#include "itl/givens_rotation.h"
-#include "itl/number_traits.h"
-#include <itl/modified_gram_schmidt.h>
+#include <gmm_solvers.h>
 
-namespace itl {
+namespace gmm {
 
   // Generalized Minimum Residual
   //
   //   This solve the unsymmetric linear system Ax = b using restarted GMRES.
   //
-  //   A return value of 0 indicates convergence within the
-  //   maximum number of iterations (determined by the iter object).
-  //   A return value of 1 indicates a failure to converge.
-  //
-  //   On instantiating Iteration object outer, the first parameter
-  //   Vector w must be the precondtioned one, i.e., solve(M, b, w) where
-  //   b is right side of linear system. See test_gmres.cc for example.
-  //
   //   See: Y. Saad and M. Schulter. GMRES: A generalized minimum residual
   //   algorithm for solving nonsysmmetric linear systems, SIAM
   //   J. Sci. Statist. Comp.  7(1986), pp, 856-869
   //
-  /* required operations: mult,copy,dot_conj,add,scaled,two_norm,tri_solve */
-template < class Matrix, class Vector, class VectorB, class Preconditioner, 
-	   class Iter, class Basis >
-int 
-gmres(const Matrix &A, Vector &x, const VectorB &b,
-      const Preconditioner &M, int restart, Iter& outer, Basis& KS)
-{
-  typedef typename itl_traits<Vector>::value_type T;
-  typedef typename itl_traits<Vector>::size_type size_type;
 
-  typedef std::vector<T> TmpVec;
-  typedef Vector internal_vector;
+  template < class Matrix, class Vector, class VectorB, class Preconditioner, 
+	     class Basis >
+  void gmres(const Matrix &A, Vector &x, const VectorB &b,
+	     const Preconditioner &M, int restart,
+	     iteration &outer, Basis& KS) {
 
-  //These must be arithmetically compatible with x
-  internal_vector w(size(x)), r(size(x)), u(size(x));
-
-  typedef typename number_traits<T>::magnitude_type Real;
-  typedef typename internal_matrix_traits<T>::Matrix HMat;
-  HMat H(restart+1, restart); //Elements in H must be real type
-
-  TmpVec s(restart+1);
+    typedef typename linalg_traits<Vector>::value_type T;
+    typedef typename linalg_traits<Vector>::size_type size_type;
     
-  std::vector< itl::givens_rotation<T> > rotations(restart+1);
+    typedef std::vector<T> TmpVec;
+    typedef typename temporary_vector<Vector>::vector_type internal_vector;
+    
+    internal_vector w(vect_size(x)), r(vect_size(x)), u(vect_size(x));
+    
+    typedef typename number_traits<T>::magnitude_type Real;
+    typedef typename row_matrix<TmpVec> HMat;
 
-  itl::mult(A, itl::scaled(x, -1.0), b, w);
-
-  itl::solve(M, w, r);
-  Real beta = std::abs(itl::two_norm(r));
-
-  while (! outer.finished(beta)) {
-
-    itl::copy(itl::scaled(r, 1./beta), KS[0]);
-    std::fill(s.begin(), s.end(), 0.0);
-    s[0] = beta;
-
-    size_type i = 0;
-    Iter inner(outer.normb(), restart, outer.tol(), outer.atol());
-
-    do {
-      size_type k;
-      itl::mult(A, KS[i], u);
-      itl::solve(M, u, KS[i+1]);
-
-      itl::orthogonalize(KS, H[i], i);
-
-      Real H_ip1_i = itl::two_norm(KS[i+1]);
-      H(i+1, i) = H_ip1_i;
-      itl::scale(KS[i+1], 1./H_ip1_i);
-
-      for (k = 0; k < i; k++)
-        rotations[k].scalar_apply(H(k,i), H(k+1,i));
-
-      rotations[i] = itl::givens_rotation<T>(H(i,i), H(i+1,i));
-      rotations[i].scalar_apply(H(i,i), H(i+1,i));
-      rotations[i].scalar_apply(s[i], s[i+1]);
+    HMat H(restart+1, restart);
+    TmpVec s(restart+1);
+    
+    std::vector< gmm::givens_rotation<T> > rotations(restart+1);
+    
+    mult(A, scaled(x, -1.0), b, w);
+    
+    mult(M, w, r);
+    Real beta = dal::abs(vect_norm2(r));
+    
+    while (! outer.finished(beta)) {
       
-      ++inner, ++outer, ++i;
-
-    } while (! inner.finished(std::abs(s[i]))); 
-
-    itl::upper_tri_solve(H, s, i);
-
-    itl::combine(KS, s, x, i);
-
-    itl::mult(A, itl::scaled(x, -1.0), b, w);
-    itl::solve(M, w, r);
-    beta = std::abs(itl::two_norm(r));
+      gmm::copy(gmm::scaled(r, 1./beta), KS[0]);
+      gmm::clear(s);
+      s[0] = beta;
+      
+      size_type i = 0;
+      iteration inner(outer.normb(), restart, outer.tol(), outer.atol()); ??
+      
+      do {
+	size_type k;
+	gmm::mult(A, KS[i], u);
+	gmm::mult(M, u, KS[i+1]);
+	
+	orthogonalize(KS, H[i], i);
+	
+	Real H_ip1_i = vect_norm2(KS[i+1]);
+	H(i+1, i) = H_ip1_i;
+	gmm::scale(KS[i+1], 1./H_ip1_i);
+	
+	for (k = 0; k < i; k++)
+	  rotations[k].scalar_apply(H(k,i), H(k+1,i));
+	
+	rotations[i] = givens_rotation<T>(H(i,i), H(i+1,i));
+	rotations[i].scalar_apply(H(i,i), H(i+1,i));
+	rotations[i].scalar_apply(s[i], s[i+1]);
+	
+	++inner, ++outer, ++i;
+	
+      } while (! inner.finished(dal::abs(s[i]))); 
+      
+      gmm::upper_tri_solve(H, s, i);
+      
+      gmm::combine(KS, s, x, i);
+      
+      gmm::mult(A, gmm::scaled(x, -1.0), b, w);
+      gmm::mult(M, w, r);
+      beta = dal::abs(vect_norm2(r));
+    }
   }
-  
-  return outer.error_code();
-}
 
 
-template < class Matrix, class Vector, class VectorB, class Preconditioner, 
-	   class Iter >
-int 
-gmres(const Matrix &A, Vector &x, const VectorB &b,
-      const Preconditioner &M, int restart, Iter& outer)
-{
-  itl::modified_gram_schmidt<Vector> orth(restart, size(x));
-  return itl::gmres(A, x, b, M, restart, outer, orth); 
-}
+  template < class Matrix, class Vector, class VectorB, class Preconditioner >
+  void gmres(const Matrix &A, Vector &x, const VectorB &b,
+	     const Preconditioner &M, int restart, iteration& outer) {
+    modified_gram_schmidt<Vector> orth(restart, size(x));
+    gmres(A, x, b, M, restart, outer, orth); 
+  }
 
 }
 
