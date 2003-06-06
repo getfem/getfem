2e906918637b929197d3b097ca4a1f485f12124f
Yves Renard <yves.renard@insa-lyon.fr>
2003-06-06 15:10:51 +0000

    *** empty log message ***

--- src/gmm_precond_cholesky.h
+++ src/gmm_precond_cholesky.h
@@ -26,7 +26,7 @@
 /* *********************************************************************** */
 /*                                                                         */
 /* Library :  Generic Matrix Methods  (gmm)                                */
-/* File    :  gmm_precond_diagonal.h : diagonal preconditoner.             */
+/* File    :  gmm_precond_cholesky.h : form I.T.L.                         */
 /*     									   */
 /* Date : June 5, 2003.                                                    */
 /* Author : Yves Renard, Yves.Renard@gmm.insa-tlse.fr                      */
@@ -55,209 +55,162 @@
 #ifndef GMM_PRECOND_CHOLESKY_H
 #define GMM_PRECOND_CHOLESKY_H
 
-//: Incomplete Cholesky Preconditioner.
-//  For use with symmetric matrices.
-//
-//Notes: The idea under a concrete Preconditioner such 
-//as Incomplete Cholesky is to create a Preconditioner
-//object to use in iterative methods. 
-//
+// importation not finished ...
 
-
+// Incomplete Level 0 Cholesky Preconditioner.
+// For use with symmetric sparse matrices.
+//
+// Notes: The idea under a concrete Preconditioner such 
+//        as Incomplete Cholesky is to create a Preconditioner
+//        object to use in iterative methods. 
+//
 
 #include <gmm_solvers.h>
 
 namespace gmm {
-  
+
   template < class Matrix >
-    class cholesky_precond {
-    typedef typename Matrix::value_type T;
-    typedef typename Matrix::orientation Orien;
-    typedef Matrix SymMatrix;
-    enum { Orien_id = Orien::id };
+  class cholesky_precond {
 
-    typedef typename mtl::matrix< T, mtl::rectangle<>, 
-                         mtl::compressed<int, mtl::external>, mtl::column_major >::type Matrix1; 
+  protected :
+    typedef typename linalg_traits<Matrix>::value_type value_type;
+    typedef typename principal_orientation_type<typename
+      linalg_traits<Matrix>::sub_orientation>::potype sub_orientation;
 
-    typedef typename mtl::matrix< T, mtl::rectangle<>, 
-                         mtl::compressed<int, mtl::external>, mtl::row_major >::type Matrix2; 
-
-    typedef typename mtl::IF< EQUAL < Orien_id, mtl::ROW_MAJOR >::RET, 
-                         Matrix2,
-            typename mtl::IF< EQUAL < Orien_id, mtl::COL_MAJOR >::RET, 
-                         Matrix1,
-			 mtl::generators_error
-    >::RET
-    >::RET TriMatrix;
-
-
-  public:
-
-    typedef preconditioner < Matrix1, Matrix2, mtl::lower, mtl::upper> Precond;
-    typedef preconditioner1< Matrix1, Matrix2, mtl::lower, mtl::upper> Left;
-    typedef preconditioner2< Matrix1, Matrix2, mtl::lower, mtl::upper> Right;
-  
-    cholesky(const SymMatrix& A) 
-      : Tri_val(A.nnz()), Tri_ind(A.nnz()), Tri_ptr(A.nrows()+1)
-    {
-          typedef typename mtl::matrix_traits<SymMatrix>::shape Shape;
-      check_symm(Shape()); // JGS change to compile-time test
-      do_cholesky(A, Orien()); 
-    }
-  private:
+    csr_matrix_ref<T *, size_type *, size_type *, 0> row_trimatrix;
+    csc_matrix_ref<T *, size_type *, size_type *, 0> col_trimatrix;
+    std::vector<value_type> Tri_val;
+    std::vector<size_type> Tri_ind, Tri_ptr;
  
-    void 
-    do_cholesky(const SymMatrix& A, mtl::row_tag)
-    {
-      using std::copy;
-      if ( A.is_upper() ) { 
-        int Tri_loc= 0;
-        Tri_ptr[0] = 0;
-        typename SymMatrix::const_iterator A_i = A.begin();
-        if (mtl::not_at(A_i,A.end())) do {
-          typename SymMatrix::OneD A_row = *A_i;
-          
-          int i=A_i.index(); 
-          
-          std::copy(A_row.begin(), A_row.end(), Tri_val.begin()+Tri_loc);
-          std::copy(A_row.nz_struct().begin(), A_row.nz_struct().end(), 
-		    Tri_ind.begin()+Tri_loc);
-          
-          Tri_loc += A_row.nnz();
-          Tri_ptr[i+1] = Tri_loc;
-          
-          ++A_i;
-        } while (mtl::not_at(A_i,A.end()));
-        
-        int d, g, h, i, j, k, n = A.nrows();
-        T z;
-        
-        for (k = 0; k < n - 1; k++) {
-          d = Tri_ptr[k];
-          z = Tri_val[d] = sqrt(Tri_val[d]);
-          
-          for (i = d + 1; i < Tri_ptr[k+1]; i++)
-            Tri_val[i] /= z;
-          
-          for (i = d + 1; i < Tri_ptr[k+1]; i++) {
-            z = Tri_val[i];
-            h = Tri_ind[i];
-            g = i;
-            
-            for (j = Tri_ptr[h] ; j < Tri_ptr[h+1]; j++)
-              for ( ; g < Tri_ptr[k+1] && Tri_ind[g+1] <= Tri_ind[j]; g++)
-                if (Tri_ind[g] == Tri_ind[j])
-                  Tri_val[j] -= z * Tri_val[g];
-          }
-        }
-        d = Tri_ptr[n-1];
-        Tri_val[d] = sqrt(Tri_val[d]);
-        
+    void do_cholesky(const Matrix& A, row_major) {
+      size_type Tri_loc, nnz, n = mat_nrows(A), d, g, h, i, j, k;
+      value_type z;
+      Tri_ptr[0] = 0;
 
-        Tri = TriMatrix(A.nrows(), A.ncols(), 
-                        Tri_loc, &Tri_val[0], &Tri_ptr[0], 
-                        &Tri_ind[0]);
-      } else { 
-        std::cout << "Warning: It is not so efficient as symmetric row-wise upper Matrix" << std::endl;
-        assert(0);
+      for (int count = 0; count < 2; ++count) {
+	if (count) { Tri_val.resize(Tri_loc); Tri_ind.resize(Tri_loc); }
+	Tri_loc = 0;
+	for (size_type i = 0; i < n; ++i) {
+	  typedef typename linalg_traits<Matrix>::const_sub_row_type row_type;
+	  row_type row = mat_const_row(A, i);
+	  typedef typename linalg_traits<row_type>::const_iterator
+	    it = vect_const_begin(row), ite = vect_const_end(row);
+	  
+	  for (nnz = 0; it != ite; ++it)
+	    if (it.index >= i) {
+	      if (count) {
+		Tri_val[Tri_loc+nnz] = *it;
+		Tri_ind[Tri_loc+nnz] = it.index();
+	      }
+	      ++nnz;
+	    }
+	  Tri_loc += nnz;
+	  if (count) Tri_ptr[i+1] = Tri_loc;
+	}
       }
-    }
-    void 
-    do_cholesky(const SymMatrix& A, mtl::column_tag)
-    {
-      using std::copy;
-      if ( A.is_upper() ) {
-        std::cout << "Warning: It is not so efficient as symmetric column-wise lower Matrix" << std::endl;
-        assert(0);
-      } else {
-        int Tri_loc= 0;
-        Tri_ptr[0] = 0;
-        typename SymMatrix::const_iterator A_i = A.begin();
-        if (mtl::not_at(A_i,A.end())) do {
-          typename SymMatrix::OneD A_row = *A_i;
-
-          int i=A_i.index(); 
-
-          std::copy(A_row.begin(), A_row.end(), Tri_val.begin()+Tri_loc);
-          std::copy(A_row.nz_struct().begin(), A_row.nz_struct().end(), 
-		    Tri_ind.begin()+Tri_loc);
-
-          Tri_loc += A_row.nnz();
-          Tri_ptr[i+1] = Tri_loc;
-
-          ++A_i;
-        } while (mtl::not_at(A_i,A.end()));
-
-        int d, g, h, i, j, k, n = A.nrows();
-        T z;
-
-        for (k = 0; k < n - 1; k++) {
-          d = Tri_ptr[k];
-          z = Tri_val[d] = sqrt(Tri_val[d]);
-
-          for (i = d + 1; i < Tri_ptr[k+1]; i++)
-            Tri_val[i] /= z;
-
-          for (i = d + 1; i < Tri_ptr[k+1]; i++) {
-            z = Tri_val[i];
-            h = Tri_ind[i];
-            g = i;
-
-            for (j = Tri_ptr[h] ; j < Tri_ptr[h+1]; j++)
-              for ( ; g < Tri_ptr[k+1] && Tri_ind[g+1] <= Tri_ind[j]; g++)
-                if (Tri_ind[g] == Tri_ind[j])
-                  Tri_val[j] -= z * Tri_val[g];
-          }
-        }
-        d = Tri_ptr[n-1];
-        Tri_val[d] = sqrt(Tri_val[d]);
-
-        Tri = TriMatrix(A.nrows(), A.ncols(), 
-                        Tri_loc, &Tri_val[0], &Tri_ptr[0], 
-                        &Tri_ind[0]);
+      
+      for (k = 0; k < n - 1; k++) {
+	d = Tri_ptr[k];
+	z = Tri_val[d] = sqrt(Tri_val[d]);
+	
+	for (i = d + 1; i < Tri_ptr[k+1]; i++)
+	  Tri_val[i] /= z;
+	
+	for (i = d + 1; i < Tri_ptr[k+1]; i++) {
+	  z = Tri_val[i];
+	  h = Tri_ind[i];
+	  g = i;
+	  
+	  for (j = Tri_ptr[h] ; j < Tri_ptr[h+1]; j++)
+	    for ( ; g < Tri_ptr[k+1] && Tri_ind[g+1] <= Tri_ind[j]; g++)
+	      if (Tri_ind[g] == Tri_ind[j])
+		Tri_val[j] -= z * Tri_val[g];
+	}
       }
+      d = Tri_ptr[n-1];
+      Tri_val[d] = sqrt(Tri_val[d]);
+
+      row_trimatrix = csr_matrix_ref<T *, size_type *, size_type *, 0>
+	(&Tri_val[0], &Tri_ptr[0], &Tri_ind[0], mat_nrows(A), mat_ncols(A));
     }
 
-    inline Precond pre0(mtl::row_tag) {
-      return  Precond(mtl::trans(Tri), Tri);
-    }
-    inline Precond pre0(mtl::column_tag) {
-      return  Precond(Tri, mtl::trans(Tri));
-    }
+    void do_cholesky(const Matrix& A, column_major) {
+      size_type Tri_loc, d, g, h, i, j, k, n = A.nrows();
+      value_type z;
+      Tri_ptr[0] = 0;
 
-    inline Left pre1(mtl::row_tag) {
-      return  Left(mtl::trans(Tri), Tri);
-    }
-    inline Left pre1(mtl::column_tag) {
-      return  Left(Tri, mtl::trans(Tri));
-    }
-    inline Right pre2(mtl::row_tag) {
-      return  Right(mtl::trans(Tri), Tri);
-    }
-    inline Right pre2(mtl::column_tag) {
-      return  Right(Tri, mtl::trans(Tri));
-    }
+      for (int count = 0; count < 2; ++count) {
+	if (count) { Tri_val.resize(Tri_loc); Tri_ind.resize(Tri_loc); }
+	Tri_loc = 0;
+	for (size_type i = 0; i < n; ++i) {
+	  typedef typename linalg_traits<Matrix>::const_sub_col_type col_type;
+	  col_type col = mat_const_col(A, i);
+	  typedef typename linalg_traits<col_type>::const_iterator
+	    it = vect_const_begin(col), ite = vect_const_end(col);
+	  
+	  for (nnz = 0; it != ite; ++it)
+	    if (it.index >= i) {
+	      if (count) {
+		Tri_val[Tri_loc+nnz] = *it;
+		Tri_ind[Tri_loc+nnz] = it.index();
+	      }
+	      ++nnz;
+	    }
+	  Tri_loc += nnz;
+	  if (count) Tri_ptr[i+1] = Tri_loc;
+	}
+      }
+      
+      for (k = 0; k < n - 1; k++) {
+	d = Tri_ptr[k];
+	z = Tri_val[d] = sqrt(Tri_val[d]);
+	
+	for (i = d + 1; i < Tri_ptr[k+1]; i++)
+	  Tri_val[i] /= z;
+	
+	for (i = d + 1; i < Tri_ptr[k+1]; i++) {
+	  z = Tri_val[i];
+	  h = Tri_ind[i];
+	  g = i;
+	  
+	  for (j = Tri_ptr[h] ; j < Tri_ptr[h+1]; j++)
+	    for ( ; g < Tri_ptr[k+1] && Tri_ind[g+1] <= Tri_ind[j]; g++)
+	      if (Tri_ind[g] == Tri_ind[j])
+		Tri_val[j] -= z * Tri_val[g];
+	}
+      }
+      d = Tri_ptr[n-1];
+      Tri_val[d] = sqrt(Tri_val[d]);
 
+      col_trimatrix = csc_matrix_ref<T *, size_type *, size_type *, 0>
+	(&Tri_val[0], &Tri_ptr[0], &Tri_ind[0], mat_nrows(A), mat_ncols(A));
+    }
 
   public:
-    //:return a left or right Preconditioner object
-    inline Precond operator()() { 
-      return pre0(Orien());
-    }
-    //: return a left part of Split Preconditioner object
-    inline Left left() { return pre1(Orien()); }
-    //: return a right part of Split Preconditioner object
-    inline Right right() { return pre2(Orien()); }
 
-    void print() {
-    }
-
-  private:
-    TriMatrix Tri;
-    std::vector<T> Tri_val;
-    std::vector<int> Tri_ind;
-    std::vector<int> Tri_ptr;
+    
+    size_type nrows(row_major) const { return mat_nrows(row_trimatrix); }
+    size_type nrows(col_major) const { return mat_nrows(col_trimatrix); }
+    size_type nrows(void) const { return nrows(sub_orientation()); }
+    size_type ncols(row_major) const { return mat_ncols(row_trimatrix); }
+    size_type ncols(col_major) const { return mat_ncols(col_trimatrix); }
+    size_type ncols(void) const { return ncols(sub_orientation()); }
+    
+    cholesky_precond(const Matrix& A) : Tri_ptr(A.nrows()+1)
+    { do_cholesky(A, sub_orientation()); }
   };
+
+
+  template <class Matrix, class V1, class V2> inline
+  void mult(const cholesky_precond<Matrix>& P, const V1 &v1, V2 &v2) {
+    if (P.nrows() != vect_size(v2))
+      DAL_THROW(dimension_error, "dimensions mismatch");
+   
+    ... à continuer .. avec les tri_solve
+
+  }
+
+
 }
 
 #endif 
