e26678de8c435fe8a89054077c1a3fcaecfb83d5
Julien Pommier <pommier@pianoteq.com>
2003-10-10 07:53:17 +0000

    test poly_substitute

--- tests/poly.C
+++ tests/poly.C
@@ -19,6 +19,7 @@
 /* *********************************************************************** */
 #include <bgeot_poly.h>
 
+typedef bgeot::polynomial<double> base_poly;
 
 int main(void)
 {
@@ -46,8 +47,8 @@ int main(void)
     cout << "Produit direct de P et Q : " << P << endl;
     
     Z.direct_product(P);
-    cout << "Produit direct de P et Z : " << Z << endl;
-    
+    cout << "Produit direct de P et Z : " << Z << endl;    
+
     P = bgeot::polynomial<double>(3,1,1);
     P *= 3.0; P *= bgeot::polynomial<double>(3,1,0);
     P += bgeot::polynomial<double>(3,1,1);
@@ -55,10 +56,27 @@ int main(void)
     P += bgeot::polynomial<double>(3,1,0);
     cout << "P = " << P << endl;
     
-
+    
     double tab[3]; tab[0] = 1.0; tab[1] = 2.0; tab[2] = -1.0;
     
     cout << "P(1.0, 2.0) = " << P.eval(&(tab[0])) << endl;
+    
+    Q = P;
+    P *= Q;
+    cout << "PP = " << P << "\n";
+    P.derivative(0);
+    cout << "PP.derivative(0)=" << P << "\n";
+
+    bgeot::power_index p(3);
+    for (int i=0; i < 20; ++i, ++p) {
+      cout << "i=" << i << ", p=";
+      for (unsigned k=0; k < p.size(); ++k) cout << p[k] << " ";
+      cout << "degree=" << p.degree() << ", global_index(p)=" << p.global_index() << "\n";      
+    }
+
+    base_poly S(1,2); S[0] = -2; S[1] = 3; S[2] = 1;
+    cout << "P=" << P << ", S=" << S << " \n";
+    cout << "P(S,x)=" << bgeot::poly_substitute_var(P,S,0) << "\n";
   }
   DAL_STANDARD_CATCH_ERROR;
 
