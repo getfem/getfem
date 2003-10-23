930d9b53abd4aebe0962f802ea6c0a5dd7a21e70
Julien Pommier <pommier@pianoteq.com>
2003-10-23 18:31:21 +0000

    added support for gssvx

--- src/gmm_superlu_interface.h
+++ src/gmm_superlu_interface.h
@@ -116,6 +116,22 @@ namespace gmm {
 	     SuperMatrix *U, SuperMatrix *B, int *info, std::complex<double>) 
   { SuperLU_Z::zgssv(A, p, q, L, U, B, info); }
 
+#define DECL_GSSVX(NAMESPACE,FNAME,FLOATTYPE,KEYTYPE) \
+  void SuperLU_gssvx(char *fact, char *trans, char *refact,                           \
+		    SuperMatrix *A, NAMESPACE::factor_param_t *factor_params, int *perm_c,      \
+		    int *perm_r, int *etree, char *equed, FLOATTYPE *R, FLOATTYPE *C,\
+		    SuperMatrix *L, SuperMatrix *U, void *work, int lwork,           \
+		    SuperMatrix *B, SuperMatrix *X, FLOATTYPE *recip_pivot_growth,   \
+		    FLOATTYPE *rcond, FLOATTYPE *ferr, FLOATTYPE *berr,              \
+		    int *info, KEYTYPE)                                              \
+  { NAMESPACE::mem_usage_t mem_usage;                                                \
+    NAMESPACE::FNAME(fact,trans,refact,A,factor_params,perm_c,perm_r,                \
+		     etree,equed,R,C,L,U,work,lwork,B,X,recip_pivot_growth,          \
+		     rcond,ferr,berr,&mem_usage,info); }                              
+DECL_GSSVX(SuperLU_S,sgssvx,float,float)
+DECL_GSSVX(SuperLU_C,cgssvx,float,std::complex<float>)
+DECL_GSSVX(SuperLU_D,dgssvx,double,double)
+DECL_GSSVX(SuperLU_Z,zgssvx,double,std::complex<double>)
 
   /* ********************************************************************* */
   /*   SuperLU solve                                                       */
@@ -157,6 +173,69 @@ namespace gmm {
     if (info != 0) DAL_THROW(failure_error, "SuperLU solve failed");
     gmm::copy(rhs, X);
     SuperLU_S::Destroy_SuperMatrix_Store(&SB);
+    SuperLU_S::Destroy_SuperMatrix_Store(&SA);
+    SuperLU_S::Destroy_SuperNode_Matrix(&SL);
+    SuperLU_S::Destroy_CompCol_Matrix(&SU);
+  }
+
+  template <class MAT, class VECTX, class VECTB>
+  void SuperLU_solve(const MAT &A, const VECTX &_X, const VECTB &B,
+		     double& _rcond, 
+		     int permc_spec = 1) {
+    /*
+     * Get column permutation vector perm_c[], according to permc_spec:
+     *   permc_spec = 0: use the natural ordering 
+     *   permc_spec = 1: use minimum degree ordering on structure of A'*A
+     *   permc_spec = 2: use minimum degree ordering on structure of A'+A
+     */
+    VECTX &X = const_cast<VECTX &>(_X);
+    typedef typename linalg_traits<MAT>::value_type T;
+    typedef typename number_traits<T>::magnitude_type R;
+
+    int m = mat_nrows(A), n = mat_ncols(A), nrhs = 1, info;
+
+    csc_matrix<T> csc_A(m, n); gmm::copy(A, csc_A);
+    std::vector<T> rhs(m), sol(m);
+    gmm::copy(B, rhs);
+
+    int nz = nnz(csc_A);
+    if ((2 * nz / n) >= m)
+      DAL_WARNING(2, "CAUTION : it seems that SuperLU has a problem"
+		  " for nearly dense sparse matrices");
+
+    SuperMatrix SA, SL, SU, SB, SX; // SuperLU format.
+    Create_CompCol_Matrix(&SA, m, n, nz, csc_A.pr,
+			  (int *)(csc_A.ir), (int *)(csc_A.jc));
+    Create_Dense_Matrix(&SB, m, nrhs, &rhs[0], m);
+    Create_Dense_Matrix(&SX, m, nrhs, &sol[0], m);
+
+    std::vector<int> etree(n);
+    char equed[] = "B",fact[] = "E", refact[] = "N", istrans[] = "N";
+    std::vector<R> Rscale(m),Cscale(n); // row scale factors
+    std::vector<R> ferr(nrhs), berr(nrhs);
+    R recip_pivot_gross, rcond;
+    // perm_c oversized to turn around a bug (?) of superlu with full matrices.
+    std::vector<int> perm_r(m), perm_c(3*n); 
+    SuperLU_S::get_perm_c(permc_spec, &SA, &perm_c[n]);
+    
+    SuperLU_gssvx(fact, istrans, refact, &SA, NULL, &perm_c[n], &perm_r[0], 
+		  &etree[0] /*output*/, equed /* output */, 
+		  &Rscale[0] /* row scale factors (output) */, 
+		  &Cscale[0] /* col scale factors (output) */, 
+		  &SL /* fact L (output)*/, &SU /* fact U (output)*/, 
+		  NULL /* work */, 
+		  0 /* lwork: superlu auto allocates (input)*/, 
+		  &SB /* rhs */, &SX /* solution */,
+		  &recip_pivot_gross /*reciprocal pivot growth factor max_j( norm(A_j)/norm(U_j) ).*/, 
+		  &rcond /*estimate of the reciprocal condition number of the matrix A after equilibration */,
+		  &ferr[0] /* estimated forward error */,
+		  &berr[0] /* relative backward error */,
+		  &info, T());
+    if (info != 0) DAL_THROW(failure_error, "SuperLU solve failed");
+    gmm::copy(sol, X);
+    _rcond = rcond;
+    SuperLU_S::Destroy_SuperMatrix_Store(&SB);
+    SuperLU_S::Destroy_SuperMatrix_Store(&SX);
     SuperLU_S::Destroy_SuperMatrix_Store(&SA);
     SuperLU_S::Destroy_SuperNode_Matrix(&SL);
     SuperLU_S::Destroy_CompCol_Matrix(&SU);
