cbb50e40c32b39e92def497433d9d16c338b92d7
Yves Renard <yves.renard@insa-lyon.fr>
2004-01-06 11:43:00 +0000

    *** empty log message ***

--- tests/gmm_torture02_baseop.C
+++ tests/gmm_torture02_baseop.C
@@ -7,14 +7,13 @@
 using gmm::size_type;
 bool print_debug = false;
 
-template <typename MAT1, typename VECT1, typename VECT2>
+template <typename MAT1, typename VECT1>
 bool test_procedure(const MAT1 &_m1, const VECT1 &_v1) {
   VECT1 &v1 = const_cast<VECT1 &>(_v1);
   MAT1  &m1 = const_cast<MAT1  &>(_m1);
   typedef typename gmm::linalg_traits<MAT1>::value_type T;
   typedef typename gmm::number_traits<T>::magnitude_type R;
   R prec = gmm::default_tol(R());
-  static size_type nb_iter = 0;
 
   size_type m = gmm::mat_nrows(m1);
 
@@ -35,7 +34,7 @@ bool test_procedure(const MAT1 &_m1, const VECT1 &_v1) {
       DAL_THROW(gmm::failure_error, "Error in basic operations");
     if (!(y == v1[i]))
       DAL_THROW(gmm::failure_error, "Error in basic operations");
-    normtest += gmm::abs(x);
+    normtest += gmm::abs_sqr(x);
   }
   
   if (gmm::abs(norm - normtest) > prec * R(100))
