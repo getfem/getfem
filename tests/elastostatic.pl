10505db0c1e0db9346914b6a662a3021bee33d9f
Yves Renard <yves.renard@insa-lyon.fr>
2004-01-27 17:26:25 +0000

    *** empty log message ***

--- tests/elastostatic.pl
+++ tests/elastostatic.pl
@@ -5,29 +5,28 @@ sub catch { `rm -f $tmp`; exit(1); }
 $SIG{INT} = 'catch';
 
 open(TMPF, ">$tmp") or die "Open file impossible : $!\n";
-print TMPF "N = 2;\n";
-print TMPF "PG = 9.81;\n";
-print TMPF "G = 1.0;\n";
+print TMPF "MU = 1.0;\n";
 print TMPF "LAMBDA = 1.0;\n";
 print TMPF "LX = 1.0;\n";
 print TMPF "LY = 1.0;\n";
 print TMPF "LZ = 1.0;\n";
+print TMPF "INCLINE = 0.0;\n";
+print TMPF "MESH_NOISE = 0.0;\n";
 print TMPF "FT = 0.1;\n";
-print TMPF "K = 2;\n";
-print TMPF "MESH_TYPE = 0;\n";
-print TMPF "KI = 1;\n";
-print TMPF "INTEGRATION = 0;\n";
+print TMPF "MESH_TYPE = 'GT_PK(2,1)';\n";
+print TMPF "INTEGRATION = 'IM_TRIANGLE(6)';\n";
 print TMPF "NX = 5;\n";
 print TMPF "RESIDU = 1E-9;\n";
-print TMPF "FEM_TYPE = 0;\n"; 
-print TMPF "ROOTFILENAME = 'elasto_statique';\n";
+print TMPF "FEM_TYPE = 'FEM_PK(2,1)';\n";
+print TMPF "ROOTFILENAME = 'elasto_static';\n";
+print TMPF "GENERIC_DIRICHLET = 0;\n";
 print TMPF "\n\n";
 close(TMPF);
 
 
 
 $er = 0;
-open F, "./elasto_statique $tmp 2>&1 |" or die;
+open F, "./elastostatic $tmp 2>&1 |" or die;
 while (<F>) {
   # print $_;
   if ($_ =~ /error has been detected/)
