c1c5be4ea2ce21d871b8a4a66c4b4e27112e5520
Yves Renard <yves.renard@insa-lyon.fr>
2005-01-27 11:57:45 +0000

    *** empty log message ***

--- tests/plate.cc
+++ tests/plate.cc
@@ -1,6 +1,6 @@
 /* *********************************************************************** */
 /*                                                                         */
-/* Copyright (C) 2002-2004 Yves Renard, Michel Salaün.                     */
+/* Copyright (C) 2002-2005 Yves Renard, Michel Salaün.                     */
 /*                                                                         */
 /* This program is free software; you can redistribute it and/or modify    */
 /* it under the terms of the GNU Lesser General Public License as          */
