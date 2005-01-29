1385bb92b56be8caf3dbcfdcef8bd07b4e8bbafb
Yves Renard <yves.renard@insa-lyon.fr>
2005-01-29 10:35:35 +0000

    *** empty log message ***

--- tests/navier_stokes.pl
+++ tests/navier_stokes.pl
@@ -9,30 +9,24 @@ print TMPF <<
 LX = 1.0;		% size in X.
 LY = 1.0;	        % size in Y.
 LZ = 1.0;		% size in Z.
-MU = 1.0;	        % Lamé coefficient.
-LAMBDA = 0.0;   	% Lamé coefficient.
-EPSILON = 0.01;          % thickness of the plate
-PRESSURE = 0.01;         % pressure on the top surface of the plate.
+NU = 1.0;	        % Lamé coefficient.
 MESH_TYPE = 'GT_QK(2,1)'; % linear rectangles
 NX = 10;            	          % space step.
 MESH_NOISE = 0; % Set to one if you want to "shake" the mesh
-FEM_TYPE_UT = 'FEM_QK(2,1)';
-FEM_TYPE_U3 = 'FEM_QK(2,1)';
-FEM_TYPE_THETA= 'FEM_QK(2,2)';
+FEM_TYPE = 'FEM_QK(2,2)';
+FEM_TYPE_P = 'FEM_QK(2,1)';
 DATA_FEM_TYPE = 'FEM_QK(2,1)';
 INTEGRATION = 'IM_GAUSS_PARALLELEPIPED(2,4)';
-INTEGRATION_CT = 'IM_GAUSS_PARALLELEPIPED(2,4)';
 RESIDU = 1E-9;     	% residu for conjugate gradient.
-ROOTFILENAME = 'plate';     % Root of data files.
-VTK_EXPORT = 2 % export solution to a .vtk file ?
-MIXED = 1;
-SYMMETRIZED = 1;
+ROOTFILENAME = 'navier_stokes';     % Root of data files.
+DX_EXPORT = 0 % export solution to a .vtk file ?
+DT_EXPORT = 0.01;
 
 ;
 close(TMPF);
 
 $er = 0;
-open F, "./stokes $tmp 2>&1 |" or die;
+open F, "./navier_stokes $tmp 2>&1 |" or die;
 while (<F>) {
   # print $_;
   if ($_ =~ /error has been detected/)
