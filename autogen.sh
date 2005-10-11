5b8a64eb9bc26fe8954eaefec8aa4b990590190c
Elie Chahine <chahineelie@gmail.com>
2005-10-11 11:23:12 +0000

    probleme avec automake 1.8

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # http://sources.redhat.com/automake/automake.html#Local-Macros
-aclocal -I ./m4
+aclocal-1.7 -I ./m4
 autoheader
 autoconf
 #pas de ./ devant les noms des makefiles !!!
-automake -a --gnu `find . -name Makefile.am | sed -e 's@\./\(.*\)\.am@\1@g'`
+automake-1.7 -a --gnu `find . -name Makefile.am | sed -e 's@\./\(.*\)\.am@\1@g'`
 
 
