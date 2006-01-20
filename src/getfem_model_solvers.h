5d7256a4e6d4af335eb3f45e35604a9218818779
Yves Renard <yves.renard@insa-lyon.fr>
2006-01-20 12:01:24 +0000

    *** empty log message ***

--- src/getfem_model_solvers.h
+++ src/getfem_model_solvers.h
@@ -37,13 +37,290 @@
 #define GETFEM_MODEL_SOLVERS_H__
 
 #include <getfem_modeling.h>
-#include <getfem_Newton.h>
 #include <gmm_MUMPS_interface.h>
 
 namespace getfem {
 
   template <typename T> struct sort_abs_val_
   { bool operator()(T x, T y) { return (gmm::abs(x) < gmm::abs(y)); } };
+
+  template <typename MAT> void print_eigval(const MAT &M) {
+    // just to test a stiffness matrix if needing
+    typedef typename gmm::linalg_traits<MAT>::value_type T;
+    size_type n = gmm::mat_nrows(M);
+    gmm::dense_matrix<T> MM(n, n), Q(n, n);
+    std::vector<T> eigval(n);
+    gmm::copy(M, MM);
+    // gmm::symmetric_qr_algorithm(MM, eigval, Q);
+    gmm::implicit_qr_algorithm(MM, eigval, Q);
+    std::sort(eigval.begin(), eigval.end(), sort_abs_val_<T>());
+    cout << "eival = " << eigval << endl;
+//     cout << "vectp : " << gmm::mat_col(Q, n-1) << endl;
+//     cout << "vectp : " << gmm::mat_col(Q, n-2) << endl;
+//     double emax, emin;
+//     cout << "condition number" << condition_number(MM,emax,emin) << endl;
+//     cout << "emin = " << emin << endl;
+//     cout << "emax = " << emax << endl;
+  }
+
+  /* ***************************************************************** */
+  /*     Line search definition                                        */
+  /* ***************************************************************** */
+
+  struct abstract_newton_line_search {
+    scalar_type conv_alpha, conv_r;
+    size_type it, itmax;
+    virtual void init_search(scalar_type r) = 0;
+    virtual scalar_type next_try(void) = 0;
+    virtual bool is_converged(scalar_type) = 0;
+    virtual scalar_type converged_value(void) { return conv_alpha; };
+    virtual scalar_type converged_residual(void) { return conv_r; };
+  };
+
+
+  struct simplest_newton_line_search : public abstract_newton_line_search {
+    scalar_type alpha, alpha_mult, first_res, alpha_max_ratio, alpha_min;
+    virtual void init_search(scalar_type r)
+    { conv_alpha = alpha = scalar_type(1); conv_r = first_res = r; it = 0; }
+    virtual scalar_type next_try(void)
+    { conv_alpha = alpha; alpha *= alpha_mult; ++it; return conv_alpha; }
+    virtual bool is_converged(scalar_type r) {
+      conv_r = r;
+      return ((it <= 1 && r < first_res)
+	      || (r <= first_res * alpha_max_ratio)
+	      || (conv_alpha <= alpha_min)
+	      || it >= itmax);
+    }
+    simplest_newton_line_search
+    (size_type imax = size_type(-1),
+     scalar_type a_max_ratio = scalar_type(6)/scalar_type(5),
+     scalar_type a_min = scalar_type(1)/scalar_type(1000),
+     scalar_type a_mult = scalar_type(3)/scalar_type(5))
+      : alpha_mult(a_mult), alpha_max_ratio(a_max_ratio), alpha_min(a_min)
+      { itmax = imax; }
+  };
+
+  struct basic_newton_line_search : public abstract_newton_line_search {
+    scalar_type alpha, alpha_mult, first_res, alpha_max_ratio;
+    scalar_type alpha_min, prev_res;
+    virtual void init_search(scalar_type r) {
+      conv_alpha = alpha = scalar_type(1);
+      prev_res = conv_r = first_res = r; it = 0;
+    }
+    virtual scalar_type next_try(void)
+    { conv_alpha = alpha; alpha *= alpha_mult; ++it; return conv_alpha; }
+    virtual bool is_converged(scalar_type r) {
+      if ((r < first_res / scalar_type(2)) || (conv_alpha <= alpha_min)
+	  || it >= itmax)
+	{ conv_r = r; return true; }
+      if (it > 1 && r > prev_res && prev_res < alpha_max_ratio * first_res)
+	return true;
+      prev_res = conv_r = r;
+      return false;
+    }
+    basic_newton_line_search
+    (size_type imax = size_type(-1),
+     scalar_type a_max_ratio = scalar_type(5)/scalar_type(3),
+     scalar_type a_min = scalar_type(1)/scalar_type(1000),
+     scalar_type a_mult = scalar_type(3)/scalar_type(5))
+      : alpha_mult(a_mult), alpha_max_ratio(a_max_ratio),
+	alpha_min(a_min) { itmax = imax; }
+  };
+
+
+  struct systematic_newton_line_search : public abstract_newton_line_search {
+    scalar_type alpha, alpha_mult, first_res;
+    scalar_type alpha_min, prev_res;
+    bool first;
+    virtual void init_search(scalar_type r) {
+      conv_alpha = alpha = scalar_type(1);
+      prev_res = conv_r = first_res = r; it = 0; first = true;
+    }
+    virtual scalar_type next_try(void)
+    { scalar_type a = alpha; alpha *= alpha_mult; ++it; return a; }
+    virtual bool is_converged(scalar_type r) {
+      // cout << "a = " << alpha / alpha_mult << " r = " << r << endl;
+      if (r < conv_r || first)
+	{ conv_r = r; conv_alpha = alpha / alpha_mult; first = false; }
+      if ((alpha <= alpha_min*alpha_mult) || it >= itmax) return true;
+      return false;
+    }
+    systematic_newton_line_search
+    (size_type imax = size_type(-1),
+     scalar_type a_min = scalar_type(1)/scalar_type(10000),
+     scalar_type a_mult = scalar_type(3)/scalar_type(5))
+      : alpha_mult(a_mult), alpha_min(a_min)  { itmax = imax; }
+  };
+
+  /* ***************************************************************** */
+  /*     Linear solvers definition                                     */
+  /* ***************************************************************** */
+
+  template <class MAT, class VECT> 
+  struct abstract_linear_solver {
+    virtual void operator ()(const MAT &, VECT &, const VECT &,
+			     gmm::iteration &) const  = 0;
+  };
+
+  template <class MAT, class VECT> 
+  struct linear_solver_cg_preconditioned_ildlt
+    : public abstract_linear_solver<MAT, VECT> {
+    void operator ()(const MAT &M, VECT &x, const VECT &b,
+		     gmm::iteration &iter)  const {
+      gmm::ildlt_precond<MAT> P(M);
+      gmm::cg(M, x, b, P, iter);
+      if (!iter.converged()) DAL_WARNING2("cg did not converge!");
+    }
+  };
+
+  template <class MAT, class VECT> 
+  struct linear_solver_gmres_preconditioned_ilu
+    : public abstract_linear_solver<MAT, VECT> {
+    void operator ()(const MAT &M, VECT &x, const VECT &b,
+		     gmm::iteration &iter)  const {
+      gmm::ilu_precond<MAT> P(M);
+      gmm::gmres(M, x, b, P, 300, iter);
+      if (!iter.converged()) DAL_WARNING2("gmres did not converge!");
+    }
+  };
+
+  template <class MAT, class VECT> 
+  struct linear_solver_gmres_preconditioned_ilut
+    : public abstract_linear_solver<MAT, VECT> {
+    void operator ()(const MAT &M, VECT &x, const VECT &b,
+		     gmm::iteration &iter)  const {
+      gmm::ilut_precond<MAT> P(M, 20, 1E-10);
+      gmm::gmres(M, x, b, P, 300, iter);
+      if (!iter.converged()) DAL_WARNING2("gmres did not converge!");
+    }
+  };
+
+  template <class MAT, class VECT> 
+  struct linear_solver_superlu
+    : public abstract_linear_solver<MAT, VECT> {
+    void operator ()(const MAT &M, VECT &x, const VECT &b,
+		     gmm::iteration &iter)  const {
+      double rcond;
+      SuperLU_solve(M, x, b, rcond);
+      if (iter.get_noisy()) cout << "condition number: " << 1.0/rcond<< endl;
+    }
+  };
+
+
+#ifdef GMM_USES_MUMPS
+  template <class MAT, class VECT> 
+  struct linear_solver_mumps : public abstract_linear_solver<MAT, VECT> {
+    void operator ()(const MAT &M, VECT &x, const VECT &b,
+		     gmm::iteration &) const 
+    { gmm::MUMPS_solve(MS, x, b); }
+  };
+#endif
+
+#if GETFEM_PARA_LEVEL > 1 && GETFEM_PARA_SOLVER == MUMPS_PARA_SOLVER
+  template <class MAT, class VECT> 
+  struct linear_solver_distributed_mumps
+    : public abstract_linear_solver<MAT, VECT> {
+    void operator ()(const MAT &M, VECT &x, const VECT &b,
+		     gmm::iteration &) const { 
+      double tt_ref=MPI_Wtime();
+      MUMPS_distributed_matrix_solve(M, x, b);
+      cout<<"temps MUMPS "<< MPI_Wtime() - tt_ref<<endl;
+    }
+  };
+#endif
+
+  /* ***************************************************************** */
+  /*     Newton algorithm.                                             */
+  /* ***************************************************************** */
+
+  template <typename PB> 
+  void classical_Newton(PB &pb, gmm::iteration &iter,
+			const abstract_linear_solver<typename PB::MATRIX,
+			typename PB::VECTOR> &linear_solver) {
+    // TODO : take iter into account for the Newton. compute a consistent 
+    //        max residu.
+    typedef typename gmm::linalg_traits<typename PB::VECTOR>::value_type T;
+    gmm::iteration iter_linsolv0 = iter;
+    iter_linsolv0.reduce_noisy();
+    iter_linsolv0.set_resmax(iter.get_resmax()/100.0);
+
+    pb.compute_tangent_matrix();
+    pb.compute_residual();
+
+    typename PB::VECTOR dr(gmm::vect_size(pb.residual()));
+    typename PB::VECTOR b(gmm::vect_size(pb.residual()));
+
+    while (!iter.finished(gmm::vect_norm2(pb.residual()))) {
+      gmm::iteration iter_linsolv = iter_linsolv0;
+      pb.compute_tangent_matrix();
+      gmm::clear(dr);
+      gmm::copy(gmm::scaled(pb.residual(), T(-1)), b);
+      linear_solver(pb.tangent_matrix(), dr, b, iter_linsolv);
+      pb.line_search(dr); // it is assumed that the line search execute
+      // a pb.compute_residual();
+      ++iter;
+    }
+  }
+
+  /* ***************************************************************** */
+  /*     Intermediary structure for Newton algorithms.                 */
+  /* ***************************************************************** */
+
+  template <typename MODEL_STATE> 
+  struct model_problem {
+
+    TYPEDEF_MODEL_STATE_TYPES;
+    typedef T_MATRIX MATRIX;
+
+    MODEL_STATE &MS;
+    mdbrick_abstract<MODEL_STATE> &pb;
+    abstract_newton_line_search &ls;
+    VECTOR stateinit, d;
+
+    void compute_tangent_matrix(void)
+    { pb.compute_tangent_matrix(MS);  MS.compute_reduced_system(); }
+
+    const T_MATRIX &tangent_matrix(void)
+    { return MS.reduced_tangent_matrix(); }
+    
+    void compute_residual(void)
+    { pb.compute_residual(MS); MS.compute_reduced_residual(); }
+
+    const VECTOR &residual(void) { return MS.reduced_residual(); }
+
+    void line_search(VECTOR &dr) {
+      gmm::resize(d, pb.nb_dof());
+      gmm::resize(stateinit, pb.nb_dof());
+      gmm::copy(MS.state(), stateinit);
+      MS.unreduced_solution(dr, d);
+      R alpha, res;
+      
+      ls.init_search(gmm::vect_norm2(residual()));
+      do {
+	alpha = ls.next_try();
+	gmm::add(stateinit, gmm::scaled(d, alpha), MS.state());
+	compute_residual();
+	res = MS.reduced_residual_norm();
+      } while (!ls.is_converged(res));
+      
+      if (alpha != ls.converged_value()) {
+	alpha = ls.converged_value();
+	gmm::add(stateinit, gmm::scaled(d, alpha), MS.state());
+	res = ls.converged_residual();
+	compute_residual();
+      }
+    }
+
+    model_problem(MODEL_STATE &MS_, mdbrick_abstract<MODEL_STATE> &pb_,
+		  abstract_newton_line_search &ls_)
+      : MS(MS_), pb(pb_), ls(ls_) {}
+
+  };
+
+  /* ***************************************************************** */
+  /*     Standard solve.                                               */
+  /* ***************************************************************** */
+
 
   /** A default solver for the model brick system.  
       
@@ -67,6 +344,83 @@ namespace getfem {
 		 gmm::iteration &iter) {
 
     TYPEDEF_MODEL_STATE_TYPES;
+    basic_newton_line_search ls;
+    model_problem<MODEL_STATE> mdpb(MS, problem, ls);
+
+    MS.adapt_sizes(problem); // to be sure it is ok, but should be done before
+    
+    std::auto_ptr<abstract_linear_solver<T_MATRIX, VECTOR> > lsolver;
+
+#if GETFEM_PARA_LEVEL == 1 && GETFEM_PARA_SOLVER == MUMPS_PARA_SOLVER
+    lsolver.reset(new linear_solver_mumps<T_MATRIX, VECTOR>);
+#elif GETFEM_PARA_LEVEL > 1 && GETFEM_PARA_SOLVER == MUMPS_PARA_SOLVER
+    lsolver.reset(new linear_solver_distributed_mumps<T_MATRIX, VECTOR>);
+#else
+    size_type ndof = problem.nb_dof(), max3d = 15000, dim = problem.dim();
+# ifdef GMM_USES_MUMPS
+    max3d = 100000;
+# endif
+    if ((ndof<200000 && dim<=2) || (ndof<max3d && dim<=3) || (ndof<1000)) {
+# ifdef GMM_USES_MUMPS
+      lsolver.reset(new linear_solver_mumps<T_MATRIX, VECTOR>);
+# else
+      lsolver.reset(new linear_solver_superlu<T_MATRIX, VECTOR>);
+# endif
+    }
+    else {
+      if (problem.is_coercive()) 
+	lsolver.reset
+	  (new linear_solver_cg_preconditioned_ildlt<T_MATRIX, VECTOR>);
+      else if (problem.mixed_variables().card() != 0)
+	lsolver.reset
+	  (new linear_solver_gmres_preconditioned_ilu<T_MATRIX, VECTOR>);
+      else 
+	lsolver.reset
+	  (new linear_solver_gmres_preconditioned_ilut<T_MATRIX, VECTOR>);
+    }
+#endif
+
+    if (problem.is_linear()) {
+      mdpb.compute_tangent_matrix();
+      mdpb.compute_residual();
+      VECTOR dr(gmm::vect_size(mdpb.residual())), d(problem.nb_dof());
+      VECTOR b(gmm::vect_size(dr));
+      gmm::copy(gmm::scaled(mdpb.residual(), value_type(-1)), b);
+      (*lsolver)(mdpb.tangent_matrix(), dr, b, iter);
+      MS.unreduced_solution(dr, d);
+      gmm::add(d, MS.state());
+    }
+    else
+      classical_Newton(mdpb, iter, *lsolver);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#if 0 // a garder pour Schwarz additif
+
+
+  template <typename MODEL_STATE> void
+  standard_solve(MODEL_STATE &MS, mdbrick_abstract<MODEL_STATE> &problem,
+		 gmm::iteration &iter) {
+
+    TYPEDEF_MODEL_STATE_TYPES;
 
     // TODO : take iter into account for the Newton. compute a consistent 
     //        max residu.
@@ -75,7 +429,7 @@ namespace getfem {
 
     bool is_linear = problem.is_linear();
     std::auto_ptr<abstract_newton_line_search>
-      pline_search(new basic_newton_line_search2());
+      pline_search(new basic_newton_line_search);
 
     R alpha(0);
     VECTOR stateinit;
@@ -345,8 +699,7 @@ namespace getfem {
       MS.unreduced_solution(dr,d);
     
       if (is_linear) {
-	gmm::add(d, MS.state());
-	return;
+	gmm::add(d, MS.state()); return;
       }
       else { // line search for the non-linear case.
 	gmm::resize(stateinit, ndof);
@@ -373,7 +726,21 @@ namespace getfem {
     }
     
   }
-  
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
 
 }  /* end of namespace getfem.                                             */
 
