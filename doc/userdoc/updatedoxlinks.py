9bf4587c84ae21d2a98cbb996eada815343e2ff2
Julien Pommier <pommier@pianoteq.com>
2006-03-31 11:31:19 +0000

    doc updates for 2.0

--- doc/userdoc/updatedoxlinks.py
+++ doc/userdoc/updatedoxlinks.py
@@ -40,6 +40,24 @@ getfem::generic_assembly
 getfem::mesh_im
 getfem::mesh_fem
 getfem::stored_mesh_slice
+getfem::slicer_action
+getfem::mesh_slice_cv_dof_data_base
+getfem::slicer_none
+getfem::slicer_boundary
+getfem::slicer_apply_deformation
+getfem::slicer_half_space
+getfem::slicer_sphere
+getfem::slicer_cylinder
+getfem::slicer_isovalues
+getfem::slicer_mesh_with_mesh
+getfem::slicer_union
+getfem::slicer_intersect
+getfem::slicer_complementary
+getfem::slicer_build_mesh
+getfem::slicer_build_edges_mesh
+getfem::slicer_build_stored_mesh_slice
+getfem::slicer_explode
+getfem::mesh_slicer
 getfem::dx_export
 getfem::vtk_export
 getfem::level_set
@@ -47,20 +65,53 @@ getfem::mesh_level_set
 getfem::mesh_im_level_set
 getfem::mesh_fem_level_set
 getfem::model_state
+getfem::mdbrick_abstract_common_base
 getfem::mdbrick_abstract
 getfem::mdbrick_parameter
 getfem::mdbrick_abstract_linear_pde
+getfem::mdbrick_generic_elliptic
+getfem::mdbrick_source_term
+getfem::mdbrick_constraint
+getfem::mdbrick_Dirichlet
+getfem::mdbrick_isotropic_linearized_elasticity
+getfem::mdbrick_QU_term
+getfem::mdbrick_linear_incomp
+getfem::mdbrick_plasticity
+getfem::mdbrick_isotropic_linearized_plate
+getfem::mdbrick_mixed_isotropic_linearized_plate
+getfem::mdbrick_plate_source_term
+getfem::mdbrick_plate_simple_support
+getfem::mdbrick_plate_clamped_support
+getfem::mdbrick_plate_closing
+getfem::mdbrick_nonlinear_elasticity
+getfem::mdbrick_nonlinear_incomp
+struct getfem::abstract_hyperelastic_law
+struct getfem::SaintVenant_Kirchhoff_hyperelastic_law
+struct getfem::Ciarlet_Geymonat_hyperelastic_law
+struct getfem::Mooney_Rivlin_hyperelastic_law 
+gmm::iteration
 """
 
-for c in classes.split():
-    ll=c.split('/')
+for c in classes.split('\n'):
+    if (len(c) == 0):
+        continue
+    ftype = "class";
+    ll=c.split(' ')
+    if (len(ll)>1):
+        ftype = ll[0]
+        ll=ll[1]
+    else:
+        ll=ll[0]
+
+    ll=ll.split('/')
     n = doxrename(ll[0])
 
     print "doing class %s" % (n,)
 
-    out.write('\\newcommand{\\%s}{\\doxref{%s}{class%s}}\\xspace\n' % (n[0],n[2], n[1]))
+
+    out.write('\\newcommand{\\%s}{\\doxref{%s}{%s%s}}\\xspace\n' % (n[0],n[2], ftype, n[1]))
 
     if (len(ll)>1):
         m = doxrename(ll[1])
         print "doing alias %s" % (ll[1],)
-        out.write('\\newcommand{\\%s}{\\doxref{%s}{class%s}}\\xspace\n' % (m[0],m[2], n[1]))
+        out.write('\\newcommand{\\%s}{\\doxref{%s}{%s%s}}\\xspace\n' % (m[0],m[2], ftype, n[1]))
