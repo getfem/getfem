2d5e1f48c04246408c9bed4a5501d55312a91df7
Yves Renard <yves.renard@insa-lyon.fr>
2006-06-05 15:48:42 +0000

    work in progress

--- contrib/xfem_contact/xfem_contact.cc
+++ contrib/xfem_contact/xfem_contact.cc
@@ -70,10 +70,11 @@ double rhs(const base_node &p) {
 }
 
 /*
- * Test procedures
+ * Test procedure
  */
 
-void test_mim(getfem::mesh_im_level_set &mim, getfem::mesh_fem &mf_rhs) {
+void test_mim(getfem::mesh_im_level_set &mim, getfem::mesh_fem &mf_rhs,
+	      bool bound) {
   unsigned N =  mim.linked_mesh().dim();
   size_type nbdof = mf_rhs.nb_dof();
   plain_vector V(nbdof), W(1);
@@ -83,60 +84,46 @@ void test_mim(getfem::mesh_im_level_set &mim, getfem::mesh_fem &mf_rhs) {
   assem.push_mi(mim); assem.push_mf(mf_rhs); assem.push_data(V);
   assem.push_vec(W);
   assem.assembly(getfem::mesh_region::all_convexes());
-  double exact(0);
+  double exact(0), R2 = Radius*Radius, R3 = R2*Radius;
   switch (N) {
-    case 1: exact = 2*Radius; break;
-    case 2: exact = Radius*Radius*M_PI; break;
-    case 3: exact = 4.0*M_PI*Radius*Radius*Radius/3.0; break;
-    default: assert(N <= 3);
+  case 1: exact = bound ? 2.0 : 2*Radius; break;
+  case 2: exact = bound ? 2.0*Radius*M_PI : R2*M_PI; break;
+  case 3: exact = bound ? 4.0*M_PI*R2 : 4.0*M_PI*R3/3.0; break;
+  default: assert(N <= 3);
   }
-  cout << "Area: " << W[0] << " should be " << exact << endl;
-
-
-//   base_matrix G;
-//   for (dal::bv_visitor i(mim.linked_mesh().convex_index());
-//        !i.finished(); ++i) {
-//     double area = 0;
-//     getfem::papprox_integration pai
-//       = mim.int_method_of_element(i)->approx_method();
-//     if (!pai) continue;
-//     bgeot::vectors_to_base_matrix(G, mim.linked_mesh().points_of_convex(i));
-//     bgeot::geotrans_interpolation_context 
-//       c(mim.linked_mesh().trans_of_convex(i), pai->point(0), G);
-//     for (size_type j = 0; j < pai->nb_points_on_convex(); ++j) {
-//       c.set_xref(pai->point(j));
-//       area += pai->coeff(j) * c.J(); 
-//     }
-//     cout << "area of elt " << i << ": " << area << endl;
-//   }
-
-
-}
-
-void test_mimbound(getfem::mesh_im_level_set &mim, getfem::mesh_fem &mf_rhs) {
-  unsigned N =  mim.linked_mesh().dim();
-  size_type nbdof = mf_rhs.nb_dof();
-  plain_vector V(nbdof), W(1);
-  std::fill(V.begin(), V.end(), 1.0);
-
-  getfem::generic_assembly assem("u = data(#1); V()+=comp(Base(#1))(i).u(i)");
-  assem.push_mi(mim); assem.push_mf(mf_rhs); assem.push_data(V);
-  assem.push_vec(W);
-  assem.assembly(getfem::mesh_region::all_convexes());
-  double exact(0);
-  switch (N) {
-    case 1: exact = 2.0; break;
-    case 2: exact = 2.0*Radius*M_PI; break;
-    case 3: exact = 4.0*M_PI*Radius*Radius; break;
-    default: assert(N <= 3);
-  }
-  cout << "Boundary length: " << W[0] << " should be " << exact << endl;
+  if (bound) cout << "Boundary length: "; else cout << "Area: ";
+  cout << W[0] << " should be " << exact << endl;
   assert(gmm::abs(exact-W[0])/exact < 0.1); 
 }
 
+/* 
+ * Matrix definition for the CG
+ */
 
+namespace gmm {
 
+  struct SMatrix {
+    const sparse_matrix &K, &B;
+    SMatrix(const sparse_matrix &KK, const sparse_matrix &BB) : K(KK), B(BB) {}
+  };
+  
+  template <class vecU, class vecV>
+  void mult(const SMatrix &K, const vecU &U, vecV &V) {
+    plain_vector W(gmm::mat_ncols(K.B));
+    gmm::mult(gmm::transposed(K.B), U, W);
+    double rcond;
+    plain_vector X(gmm::mat_ncols(K.B));
+    // A LU decomposition should be done only once !!
+    SuperLU_solve(K.K, X, W, rcond);
+    gmm::mult(K.B, X, V);
+  }
+  
+  
+  template <class vecU, class vecV, class vecW>
+  void mult(const SMatrix &K, const vecU &U, const vecV &V, vecW &W)
+  { mult(K, U, W); gmm::add(V, W); }
 
+}
 
 /* 
  * Main program 
@@ -232,8 +219,8 @@ int main(int argc, char *argv[]) {
 
 
     // Mass matrix on the boundary
-    sparse_matrix B(nb_dof, nb_dof_mult);
-    getfem::asm_mass_matrix(B, mimbound, mf, mf_mult);
+    sparse_matrix B(nb_dof_mult, nb_dof);
+    getfem::asm_mass_matrix(B, mimbound, mf_mult, mf);
 
     // rhs
     plain_vector F(nb_dof), FD(nb_dof_rhs);
@@ -243,23 +230,87 @@ int main(int argc, char *argv[]) {
 
 
     // Tests
-    test_mim(mim, mf_rhs);
-    test_mimbound(mimbound, mf_rhs);
+    test_mim(mim, mf_rhs, false);
+    test_mim(mimbound, mf_rhs, true);
+
+    // Dof selection for the solution and the multipliers
+    dal::bit_vector dof_u, dof_mult;
+    for (dal::bv_visitor i(mesh.convex_index());  !i.finished(); ++i) {
+      if (mim.int_method_of_element(i)->approx_method() != 0) {
+	for (unsigned j = 0; j < mf.nb_dof_of_element(i); ++j)
+	  dof_u.add(mf.ind_dof_of_element(i)[j]);
+      }
+      if (mls.is_convex_cut(i)) {
+	for (unsigned j = 0; j < mf_mult.nb_dof_of_element(i); ++j)
+	  dof_mult.add(mf_mult.ind_dof_of_element(i)[j]);
+      }
+    }
+    // cout << "dof_u = " << dof_u << endl;
+    // cout << "dof_mult = " << dof_mult << endl;
+    std::vector<size_type> ind_u;
+    for (dal::bv_visitor i(dof_u); !i.finished(); ++i) ind_u.push_back(i);
+    gmm::sub_index I(ind_u);
+    size_type nb_real_dof = ind_u.size();
+    std::vector<size_type> ind_mult;
+    for (dal::bv_visitor i(dof_mult); !i.finished(); ++i)
+      ind_mult.push_back(i);
+    gmm::sub_index J(ind_mult);
+    size_type nb_real_dof_mult = ind_mult.size();
+
+    // reduced matrices and rhs
+    sparse_matrix K2(nb_real_dof, nb_real_dof);
+    gmm::copy(gmm::sub_matrix(K, I), K2);
+    sparse_matrix B2(nb_real_dof_mult, nb_real_dof);
+    gmm::copy(gmm::sub_matrix(B, J, I), B2);
+    plain_vector F2(nb_real_dof);
+    gmm::copy(gmm::sub_vector(F, I), F2);
+    
+    // global system
+    sparse_matrix A(nb_real_dof+nb_real_dof_mult,
+		    nb_real_dof+nb_real_dof_mult);
+    gmm::sub_interval II(0, nb_real_dof), JJ(nb_real_dof, nb_real_dof_mult);
+    gmm::copy(K2, gmm::sub_matrix(A, II));
+    gmm::copy(gmm::transposed(B2), gmm::sub_matrix(A, II, JJ));
+    gmm::copy(B2, gmm::sub_matrix(A, JJ, II));
+    plain_vector gF(nb_real_dof+nb_real_dof_mult);
+    gmm::copy(F2, gmm::sub_vector(gF, II));
+
+    double rcond; 
+    plain_vector XX(nb_real_dof+nb_real_dof_mult);
+    SuperLU_solve(A, XX, gF, rcond);
+    cout << "condition number : " << 1.0 / rcond << endl;
+
+    plain_vector U(nb_dof);
+    gmm::copy(gmm::sub_vector(XX, II), gmm::sub_vector(U, I));
+
+    // computation of max error.
+    double errmax = 0.0;
+    for (size_type i = 0; i < nb_dof; ++i)
+      if (gmm::vect_norm2(mf.point_of_dof(i)) < Radius)
+	errmax = std::max(errmax, gmm::abs(U[i]-u_exact(mf.point_of_dof(i))));
+    cout << "Linfty error : " << errmax << endl;
+      
+    // export de la solution au format vtk.
+    getfem::vtk_export exp("xfem_contact.vtk", (2==1));
+    exp.exporting(mf); 
+    exp.write_point_data(mf, U, "solution");
+    cout << "export done, you can view the data file with (for example)\n"
+      "mayavi -d xfem_contact.vtk  -f WarpScalar -m BandedSurfaceMap -m Outline\n";
+    
+    
+    
     
 
+//     // rhs for the problem on multipliers
+//     plain_vector X(nb_real_dof), FF(nb_dof_mult);
+//     SuperLU_solve(K2, X, F2, rcond);
+//     gmm::mult(B2, X, FF);
 
-    gmm::clean(K, 1E-9);
-    // cout << "K = " << K << endl;
-
-
-    gmm::clean(B, 1E-9);
-    // cout << "B = " << B << endl;
-
-    // TODO:
-    // - sélectionner les ddls utiles pour la solution.
-    // - retenir les ddls utiles pour les multiplicateurs.
-    // - résoudre le système.
-    // - calculer l'erreur.
+//     // cg on the problem on multipliers
+//     gmm::SMatrix SK(K2, B2);
+//     plain_vector L(nb_dof_mult);
+//     gmm::iteration iter(1e-10);
+//     gmm::cg(SK, L, FF, gmm::identity_matrix(), iter);
    
   }
   DAL_STANDARD_CATCH_ERROR;
