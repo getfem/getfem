2373c6f7b7d3f7dad26b456c90e79283816d5164
Julien Pommier <pommier@pianoteq.com>
2006-09-07 14:03:31 +0000

    minor change

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 # http://sources.redhat.com/automake/automake.html#Local-Macros
-aclocal -I ./m4
-autoheader
-autoconf
-#pas de ./ devant les noms des makefiles !!!
-automake -a --gnu `find . -name Makefile.am | sed -e 's@\./\(.*\)\.am@\1@g'`
+function die {
+      echo "ERROR: $1";
+          exit 1
+}
 
+aclocal -I ./m4 || die "aclocal failed";
+autoheader || die "autoheader failed";
+autoconf || die "autoconf failed";
+#pas de ./ devant les noms des makefiles !!!
+automake -a --gnu `find . -name Makefile.am | sed -e 's@\./\(.*\)\.am@\1@g'` || die "automake failed";
+echo "autogen.sh is ok, you can run the ./configure script"
 
