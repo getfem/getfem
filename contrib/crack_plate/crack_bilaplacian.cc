bc102ee5c2f1e7bbd38ef88761dab2799c97a7d8
Jeremie Lasry <jeremie.lasry@siemens.com>
2008-02-04 10:40:13 +0000

    cleaning of the file

--- contrib/crack_plate/crack_bilaplacian.cc
+++ contrib/crack_plate/crack_bilaplacian.cc
@@ -77,56 +77,13 @@ int main(int argc, char *argv[]) {
        p.compute_error_beta(U) ;
     }
     
+    //p.compute_H2_error_field(U) ;
     
-    if (p.PARAM.int_value("VTK_EXPORT")){
-    // Post-traitement pour l'affichage, le plus simple :
-    getfem::mesh mcut;
-    p.mls.global_cut_mesh(mcut);
-    unsigned Q = p.mf_u().get_qdim();
-    assert( Q == 1 ) ;
-    getfem::mesh_fem mf(mcut, Q);
-    mf.set_classical_discontinuous_finite_element(3, 0.001);
-    plain_vector V(mf.nb_dof()) ;
-    getfem::interpolation(p.mf_u(), mf, U, V);  
+    // -------------------------------------------------------------------
+    // Print estimation of SIF (from the value of the coefficient 
+    //  of the singularity, on integral matching)
+    // -------------------------------------------------------------------
     
-    size_type N = mcut.dim() ;
-    getfem::mesh_fem mf_grad(mcut, Q);
-    mf_grad.set_classical_discontinuous_finite_element(2, 0.001);
-    plain_vector GRAD(N*mf_grad.nb_dof()) ;
-/*    if (p.PARAM.int_value("TRY") == 1 ){ 
-    getfem::compute_gradient(mf, mf_grad, U, GRAD) ;
-    }
-    if (p.PARAM.int_value("TRY") == 1 ){ 
-    getfem::compute_gradient(mf, mf_grad, U, GRAD) ;
-    }  */  
-    if (p.PARAM.int_value("SHOW_DX") == 1 ){  // au lieu d'afficher la solution, on affiche sa dérivée en y
-       getfem::compute_gradient(mf, mf_grad, V, GRAD) ;
-       gmm::resize(V, mf_grad.nb_dof()) ;
-       for (unsigned i=0 ; i< mf_grad.nb_dof(); i++){
-           V[i] = GRAD[2*i+1] ;
-       }
-      cout << "export to " << p.datafilename + ".vtk" << "..\n";
-      getfem::vtk_export exp(p.datafilename + ".vtk",
-			     p.PARAM.int_value("VTK_EXPORT")==1);
-      exp.exporting(mf_grad); 
-      exp.write_point_data(mf_grad, V, "gradient_second_coordinate");
-      cout << "export done, you can view the data file with (for example)\n"
-	   << "mayavi -d " << p.datafilename
-	   << ".vtk -m BandedSurfaceMap -m Outline -f WarpScalar\n";
-    }
-    else{
-      cout << "export to " << p.datafilename + ".vtk" << "..\n";
-      getfem::vtk_export exp(p.datafilename + ".vtk",
-			     p.PARAM.int_value("VTK_EXPORT")==1);
-      exp.exporting(p.mf_u()); 
-      exp.write_point_data(mf, V, "vertical_displacement");
-      cout << "export done, you can view the data file with (for example)\n"
-	   << "mayavi -d " << p.datafilename
-	   << ".vtk -m BandedSurfaceMap -m Outline -f WarpScalar\n";
-    }
-    }
-	   
-
     unsigned q = p.mf_u().get_qdim();
     
     base_small_vector tab_fic(4) ;
@@ -258,151 +215,194 @@ int main(int argc, char *argv[]) {
 	cout << "relative_error:" << gmm::abs(k[0] - k1_exact) / gmm::abs(k1_exact) ;
       }
     
-    }
+    } // End printing SIFs
+     
+    // --------------------------------------------------
+    // Post-treatment for pretty printing of the solution
+    // --------------------------------------------------
     
-     // fin affichage fic
+    int VTK_EXPORT = p.PARAM.int_value("VTK_EXPORT");
+    int MATLAB_EXPORT = p.PARAM.int_value("MATLAB_EXPORT");
+    int DX_EXPORT = p.PARAM.int_value("DX_EXPORT");
+	
+    if (VTK_EXPORT || MATLAB_EXPORT || DX_EXPORT){
+    // Post-traitement pour l'affichage, le plus simple :
+    getfem::mesh mcut;
+    p.mls.global_cut_mesh(mcut);
+    unsigned Q = p.mf_u().get_qdim();
+    assert( Q == 1 ) ;
+    getfem::mesh_fem mf(mcut, Q);
+    mf.set_classical_discontinuous_finite_element(3, 0.001);
+    plain_vector V(mf.nb_dof()) ;
+    getfem::interpolation(p.mf_u(), mf, U, V);  
+    
+    size_type N = mcut.dim() ;
+    getfem::mesh_fem mf_grad(mcut, Q);
+    mf_grad.set_classical_discontinuous_finite_element(2, 0.001);
+        // mf.set_finite_element
+    //	(getfem::fem_descriptor("FEM_PK_DISCONTINUOUS(2, 2, 0.0001)"));
+    plain_vector GRAD(N*mf_grad.nb_dof()) ;
+/*    if (p.PARAM.int_value("TRY") == 1 ){ 
+    getfem::compute_gradient(mf, mf_grad, U, GRAD) ;
+    }
+    if (p.PARAM.int_value("TRY") == 1 ){ 
+    getfem::compute_gradient(mf, mf_grad, U, GRAD) ;
+    }  */  
 
-    //p.compute_H2_error_field(U) ;
 
-//     int VTK_EXPORT = p.PARAM.int_value("VTK_EXPORT");
-//     
-//     if (VTK_EXPORT){   
-//     // visualisation, export au format .vtk
-//     getfem::mesh mcut;
-//     p.mls.global_cut_mesh(mcut);
-//     unsigned Q = p.mf_u().get_qdim();
-//     assert( Q == 1 ) ;
-//     getfem::mesh_fem mf(mcut, Q);
-//     mf.set_classical_discontinuous_finite_element(2, 0.001);
-//     // mf.set_finite_element
-//     //	(getfem::fem_descriptor("FEM_PK_DISCONTINUOUS(2, 2, 0.0001)"));
-//     plain_vector V(mf.nb_dof());
-// 
-//     getfem::interpolation(p.mf_u(), mf, U, V);
-// 
-//     getfem::stored_mesh_slice sl;
-//     getfem::mesh mcut_refined;
-// 
-//     unsigned NX = p.PARAM.int_value("NX"), nn;
-//     if (NX < 6) nn = 12;
-//     else if (NX < 12) nn = 8;
-//     else if (NX < 30) nn = 3;
-//     else nn = 1;
-// 
-//     /* choose an adequate slice refinement based on the distance to the crack tip */
-//     std::vector<bgeot::short_type> nrefine(mcut.convex_index().last_true()+1);
-//     for (dal::bv_visitor cv(mcut.convex_index()); !cv.finished(); ++cv) {
-//       scalar_type dmin=0, d;
-//       base_node Pmin,P;
-//       for (unsigned i=0; i < mcut.nb_points_of_convex(cv); ++i) {
-// 	P = mcut.points_of_convex(cv)[i];
-// 	//d = gmm::vect_norm2(ls_function(P));
-// 	d = gmm::vect_norm2(P) ; 
-// 	if (d < dmin || i == 0) { dmin = d; Pmin = P; }
-//       }
-// 
-//       if (dmin < 1e-5)
-// 	nrefine[cv] = nn*2;
-//       else if (dmin < .1) 
-// 	nrefine[cv] = nn*2;
-//       else nrefine[cv] = nn;
-//       /*if (dmin < .01)
-// 	cout << "cv: "<< cv << ", dmin = " << dmin << "Pmin=" << Pmin << " " << nrefine[cv] << "\n";*/
-//     }
-// 
-// 
-//     getfem::mesh_slicer slicer(mcut); 
-//     getfem::slicer_build_mesh bmesh(mcut_refined);
-//     slicer.push_back_action(bmesh);
-//     slicer.exec(nrefine, getfem::mesh_region::all_convexes());
-//      
-//     /*
-//       sl.build(mcut, 
-//       getfem::slicer_build_mesh(mcut_refined), nrefine);*/
-// 
-//     getfem::mesh_im mim_refined(mcut_refined); 
-//     mim_refined.set_integration_method(getfem::int_method_descriptor
-// 				       ("IM_TRIANGLE(6)"));
-// 
-//     getfem::mesh_fem mf_refined(mcut_refined, Q);
-//     mf_refined.set_classical_discontinuous_finite_element(2, 0.001);
-//     plain_vector W(mf_refined.nb_dof());
-// 
-//     
-//     getfem::interpolation(p.mf_u(), mf_refined, U, W);
-//     
-// //     getfem::mesh_fem mf_grad(mcut, 2) ;
-// //     plain_vector VGRAD(mf_grad.nb_dof() ) ;
-// //     getfem::compute_gradient(p.mls, p.mf_u(), mf_grad_u, U, GRAD) ;
-// //     getfem::mesh_fem mf_grad_refined(mcut_refined, 2) ;
-// //     plain_vector WGRAD(mf_grad_refined.nb_dof()) ;
-// //     getfem::interpolation(mf_grad, mf_grad_refined, GRAD, WGRAD) ;
-// 
-//       cout << "exporting solution to " << p.datafilename + ".vtk" << "..\n";
-//       getfem::vtk_export exp(p.datafilename + ".vtk", false);
-//       exp.exporting(mf_refined); 
-//       exp.write_point_data(mf_refined, W, "vertical_displacement");
+//     // Printing the normal rotation (= derivative of the vertical displacement)
+//     if (p.PARAM.int_value("SHOW_DX") == 1 ){  // au lieu d'afficher la solution, on affiche sa dérivée en y
+//        getfem::compute_gradient(mf, mf_grad, V, GRAD) ;
+//        gmm::resize(V, mf_grad.nb_dof()) ;
+//        for (unsigned i=0 ; i< mf_grad.nb_dof(); i++){
+//            V[i] = GRAD[2*i+1] ;
+//        }
+//       cout << "export to " << p.datafilename + ".vtk" << "..\n";
+//       getfem::vtk_export exp(p.datafilename + ".vtk",
+// 			     p.PARAM.int_value("VTK_EXPORT")==1);
+//       exp.exporting(mf_grad); 
+//       exp.write_point_data(mf_grad, V, "gradient_second_coordinate");
 //       cout << "export done, you can view the data file with (for example)\n"
-// 	"mayavi -d " << p.datafilename  << ".vtk -f "
-// 	"WarpScalar -m BandedSurfaceMap -m Outline\n";
-// 
-//       p.exact_sol.mf.set_qdim(Q);
-//       assert(p.exact_sol.mf.nb_dof() == p.exact_sol.U.size());   
-//       plain_vector EXACT(mf_refined.nb_dof());
-//       getfem::interpolation(p.exact_sol.mf, mf_refined, 
-// 			    p.exact_sol.U, EXACT);
-// 	
-//       plain_vector DIFF(EXACT); gmm::add(gmm::scaled(W,-1),DIFF);
-//       if ((VTK_EXPORT & 2)) {
-// 	cout << "exporting exact solution to VTK\n";
-// 	getfem::vtk_export exp2(p.datafilename + "_exact.vtk");
-// 	exp2.exporting(mf_refined);
-// 	exp2.write_point_data(mf_refined, EXACT, "reference solution");
-// 	cout << "export done, you can view the data file with (for example)\n"
-// 	"mayavi -d " << p.datafilename << "_exact.vtk -f "
-// 	"WarpScalar -m BandedSurfaceMap -m Outline\n";
-//       }
-//       if ((VTK_EXPORT & 4)) {
-// 	cout << "exporting difference with exact solution to VTK\n";
-// 	getfem::vtk_export exp2(p.datafilename + "_diff.vtk");
-// 	exp2.exporting(mf_refined);
-// 	exp2.write_point_data(mf_refined, DIFF, "difference solution");
-// 	cout << "export done, you can view the data file with (for example)\n"
-// 	  "mayavi -d " << p.datafilename  << "_diff.vtk -f "
-// 	"WarpScalar -m BandedSurfaceMap -m Outline\n";
-//       }
+// 	   << "mayavi -d " << p.datafilename
+// 	   << ".vtk -m BandedSurfaceMap -m Outline -f WarpScalar\n";
 //     }
-// 
-//     int MATLAB_EXPORT = p.PARAM.int_value("MATLAB_EXPORT");
-//     if (MATLAB_EXPORT) {
-//       cout << "exporting solution to " << p.datafilename + ".mf" << " and " << p.datafilename << ".U\n";
-//       mf_refined.write_to_file(p.datafilename + ".mf", true);
-//       gmm::vecsave(p.datafilename + ".U", W);
-//       p.exact_sol.mf.set_qdim(Q);
-//       assert(p.exact_sol.mf.nb_dof() == p.exact_sol.U.size());   // ??
-//       plain_vector EXACT(mf_refined.nb_dof());
-//       getfem::interpolation(p.exact_sol.mf, mf_refined, 
-// 			    p.exact_sol.U, EXACT);
-//       gmm::vecsave(p.datafilename + ".EXACT", EXACT);
-// 
-//       cout << "exporting original mesh to " << p.datafilename + "_base.mesh\n";
-//       p.mesh.write_to_file(p.datafilename + "_base.mesh");
-// 
-//       cout << "matlab export done, you can view the results with\n";
-//       cout << "mf=gfMeshFem('load', 'bilaplacian.mf'); U=load('bilaplacian.U')'; "
-// 	"EXACT=load('bilaplacian.EXACT')'; m0=gfMesh('load','bilaplacian_base.mesh');\n";
-//       cout << "gf_plot(mf, U-EXACT, 'refine',1); hold on; gf_plot_mesh(m0); hold off; colorbar;\n";
+//     else{
+//       cout << "export to " << p.datafilename + ".vtk" << "..\n";
+//       getfem::vtk_export exp(p.datafilename + ".vtk",
+// 			     p.PARAM.int_value("VTK_EXPORT")==1);
+//       exp.exporting(p.mf_u()); 
+//       exp.write_point_data(mf, V, "vertical_displacement");
+//       cout << "export done, you can view the data file with (for example)\n"
+// 	   << "mayavi -d " << p.datafilename
+// 	   << ".vtk -m BandedSurfaceMap -m Outline -f WarpScalar\n";
 //     }
-// 
-//     if (p.PARAM.int_value("DX_EXPORT")) {
-//     cout << "export solution to " << p.datafilename + ".dx" << "..\n";
-//     getfem::dx_export exp(p.datafilename + ".dx",
-// 			   p.PARAM.int_value("DX_EXPORT")==1);
-//     exp.exporting(mf_refined); 
-//     exp.write_point_data(mf_refined, W, "vertical_displacement");
-//     cout << "export done for open dx \n" ;
-//     }
-//     //getchar(); 
+    
+
+
+    getfem::stored_mesh_slice sl;
+    getfem::mesh mcut_refined;
+
+    unsigned NX = p.PARAM.int_value("NX"), nn;
+    if (NX < 6) nn = 12;
+    else if (NX < 12) nn = 8;
+    else if (NX < 30) nn = 3;
+    else nn = 1;
+
+    /* choose an adequate slice refinement based on the distance to the crack tip */
+    std::vector<bgeot::short_type> nrefine(mcut.convex_index().last_true()+1);
+    for (dal::bv_visitor cv(mcut.convex_index()); !cv.finished(); ++cv) {
+      scalar_type dmin=0, d;
+      base_node Pmin,P;
+      for (unsigned i=0; i < mcut.nb_points_of_convex(cv); ++i) {
+	P = mcut.points_of_convex(cv)[i];
+	//d = gmm::vect_norm2(ls_function(P));
+	d = gmm::vect_norm2(P) ; 
+	if (d < dmin || i == 0) { dmin = d; Pmin = P; }
+      }
+
+      if (dmin < 1e-5)
+	nrefine[cv] = nn*2;
+      else if (dmin < .1) 
+	nrefine[cv] = nn*2;
+      else nrefine[cv] = nn;
+      /*if (dmin < .01)
+	cout << "cv: "<< cv << ", dmin = " << dmin << "Pmin=" << Pmin << " " << nrefine[cv] << "\n";*/
+    }
+
+
+    getfem::mesh_slicer slicer(mcut); 
+    getfem::slicer_build_mesh bmesh(mcut_refined);
+    slicer.push_back_action(bmesh);
+    slicer.exec(nrefine, getfem::mesh_region::all_convexes());
+     
+    /*
+      sl.build(mcut, 
+      getfem::slicer_build_mesh(mcut_refined), nrefine);*/
+
+    getfem::mesh_im mim_refined(mcut_refined); 
+    mim_refined.set_integration_method(getfem::int_method_descriptor
+				       ("IM_TRIANGLE(6)"));
+
+    getfem::mesh_fem mf_refined(mcut_refined, Q);
+    mf_refined.set_classical_discontinuous_finite_element(2, 0.001);
+    plain_vector W(mf_refined.nb_dof());
+
+    
+    getfem::interpolation(p.mf_u(), mf_refined, U, W);
+    
+//     getfem::mesh_fem mf_grad(mcut, 2) ;
+//     plain_vector VGRAD(mf_grad.nb_dof() ) ;
+//     getfem::compute_gradient(p.mls, p.mf_u(), mf_grad_u, U, GRAD) ;
+//     getfem::mesh_fem mf_grad_refined(mcut_refined, 2) ;
+//     plain_vector WGRAD(mf_grad_refined.nb_dof()) ;
+//     getfem::interpolation(mf_grad, mf_grad_refined, GRAD, WGRAD) ;
+
+      if (VTK_EXPORT){
+      cout << "exporting solution to " << p.datafilename + ".vtk" << "..\n";
+      getfem::vtk_export exp(p.datafilename + ".vtk", false);
+      exp.exporting(mf_refined); 
+      exp.write_point_data(mf_refined, W, "vertical_displacement");
+      cout << "export done, you can view the data file with (for example)\n"
+	"mayavi -d " << p.datafilename  << ".vtk -f "
+	"WarpScalar -m BandedSurfaceMap -m Outline\n";
+
+      p.exact_sol.mf.set_qdim(Q);
+      assert(p.exact_sol.mf.nb_dof() == p.exact_sol.U.size());   
+      plain_vector EXACT(mf_refined.nb_dof());
+      getfem::interpolation(p.exact_sol.mf, mf_refined, 
+			    p.exact_sol.U, EXACT);
+	
+      plain_vector DIFF(EXACT); gmm::add(gmm::scaled(W,-1),DIFF);
+      if ((VTK_EXPORT & 2)) {
+	cout << "exporting exact solution to VTK\n";
+	getfem::vtk_export exp2(p.datafilename + "_exact.vtk");
+	exp2.exporting(mf_refined);
+	exp2.write_point_data(mf_refined, EXACT, "reference solution");
+	cout << "export done, you can view the data file with (for example)\n"
+	"mayavi -d " << p.datafilename << "_exact.vtk -f "
+	"WarpScalar -m BandedSurfaceMap -m Outline\n";
+      }
+      if ((VTK_EXPORT & 4)) {
+	cout << "exporting difference with exact solution to VTK\n";
+	getfem::vtk_export exp2(p.datafilename + "_diff.vtk");
+	exp2.exporting(mf_refined);
+	exp2.write_point_data(mf_refined, DIFF, "difference solution");
+	cout << "export done, you can view the data file with (for example)\n"
+	  "mayavi -d " << p.datafilename  << "_diff.vtk -f "
+	"WarpScalar -m BandedSurfaceMap -m Outline\n";
+      }
+      }
+    
+    if (MATLAB_EXPORT) {
+      cout << "exporting solution to " << p.datafilename + ".mf" << " and " << p.datafilename << ".U\n";
+      mf_refined.write_to_file(p.datafilename + ".mf", true);
+      gmm::vecsave(p.datafilename + ".U", W);
+      p.exact_sol.mf.set_qdim(Q);
+      assert(p.exact_sol.mf.nb_dof() == p.exact_sol.U.size());   // ??
+      plain_vector EXACT(mf_refined.nb_dof());
+      getfem::interpolation(p.exact_sol.mf, mf_refined, 
+			    p.exact_sol.U, EXACT);
+      gmm::vecsave(p.datafilename + ".EXACT", EXACT);
+
+      cout << "exporting original mesh to " << p.datafilename + "_base.mesh\n";
+      p.mesh.write_to_file(p.datafilename + "_base.mesh");
+
+      cout << "matlab export done, you can view the results with\n";
+      cout << "mf=gfMeshFem('load', 'bilaplacian.mf'); U=load('bilaplacian.U')'; "
+	"EXACT=load('bilaplacian.EXACT')'; m0=gfMesh('load','bilaplacian_base.mesh');\n";
+      cout << "gf_plot(mf, U-EXACT, 'refine',1); hold on; gf_plot_mesh(m0); hold off; colorbar;\n";
+    }
+
+    if (DX_EXPORT) {
+    cout << "export solution to " << p.datafilename + ".dx" << "..\n";
+    getfem::dx_export exp(p.datafilename + ".dx",
+			   p.PARAM.int_value("DX_EXPORT")==1);
+    exp.exporting(mf_refined); 
+    exp.write_point_data(mf_refined, W, "vertical_displacement");
+    cout << "export done for open dx \n" ;
+    }
+    }
+    //getchar(); 
     
     
    } GMM_STANDARD_CATCH_ERROR;
