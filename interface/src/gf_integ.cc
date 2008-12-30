20b0680aaf3562d49bc368f78b637137558b14a4
Luis Saavedra <luis94855510@gmail.com>
2008-12-30 09:09:49 +0000

    modification in to @TEXT INTEG:INIT('INTEG_init') for homogeneous autodoc.

--- interface/src/gf_integ.cc
+++ interface/src/gf_integ.cc
@@ -29,65 +29,48 @@ using namespace getfemint;
 MLABCOM*/
 
 /*@TEXT INTEG:INIT('INTEG_init')
-    INTEG:INIT(@string method)
-    Return a FEM Integration Method from a string description.<Par>
-
-    Possible descriptions are (non exhaustive list):<par>
-    * IM_EXACT_SIMPLEX(n)<par>
-      Exact integration on simplices (works only with linear geometric
-      transformations and PK FEMs). Note that 'exact integration'
-      should be avoided in general, since they only apply to linear
-      geometric transformations, are quite slow, and subject to
-      numerical stability problems for high degree FEMs.
-
-      <par>
-    * IM_PRODUCT(a, b)
-      Product of two integration methods.<Par>
-
-    * IM_EXACT_PARALLELEPIPED(n)<par>
-      Exact integration on parallelepipeds.<Par>
-
-    * IM_EXACT_PRISM(n)      <par>
-      Exact integration on prisms.<Par>
-
-    * IM_GAUSS1D(K)       <par>
-      Gauss method on the segment, order K (K=1,3,...99).<Par>
-
-    * IM_NC(N,K)          <par>
-      Newton-Cotes approximative integration on simplexes, order K.<Par>
-
-    * IM_NC_PARALLELEPIPED(N,K) <par>
-      Product of Newton-Cotes integration on parallelepipeds.<Par>
-
-    * IM_NC_PRISM(N,K) <par>
-      Product of Newton-Cotes integration on prisms.<Par>
-
-    * IM_GAUSS_PARALLELEPIPED(N,K) <par>
-      Product of Gauss1D integration on parallelepipeds.<Par>
-
-    * IM_TRIANGLE(K)<par>
-      Gauss methods on triangles (K=1,3,5,6,7,8,9,10,13,17,19).<Par>
-
-    * IM_QUAD(K)<par>
-      Gauss methods on quadrilaterons (K=2, 3, 5, .. 17). Note that IM_GAUSS_PARALLELEPIPED should be prefered for QK FEMs.<Par>
-
-    * IM_TETRAHEDRON(K)<par>
-      Gauss methods on tetrahedrons (K=1, 2, 3, 5, 6 or 8).<Par>
-
-    * IM_SIMPLEX4D(3)<par>
-      Gauss method on a 4-dimensional simplex.<Par>
-
-    * IM_STRUCTURED_COMPOSITE(IM1, K)<par> 
-      Composite method on a grid with K divisions.<Par>
-
-    * IM_HCT_COMPOSITE(IM1)<par>
-      Composite integration suited to the HCT composite finite element.<Par>
-
-    example:<par>
-      INTEG:INIT('IM_PRODUCT(IM_GAUSS1D(5),IM_GAUSS1D(5))')<par>
-      is the same as:<par>
-      INTEG:INIT('IM_GAUSS_PARALLELEPIPED(2,5)')<par>
-      @*/
+INTEG:INIT(@str method)<Par>
+Possible descriptions are (non exhaustive list):<Par>
+* IM_EXACT_SIMPLEX(n)<par>
+   Exact integration on simplices (works only with linear geometric
+   transformations and PK FEMs). Note that 'exact integration'
+   should be avoided in general, since they only apply to linear
+   geometric transformations, are quite slow, and subject to
+   numerical stability problems for high degree FEMs.<par>
+* IM_PRODUCT(a, b)<par>
+   Product of two integration methods.<par>
+* IM_EXACT_PARALLELEPIPED(n)<par>
+   Exact integration on parallelepipeds.<par>
+* IM_EXACT_PRISM(n)<par>
+   Exact integration on prisms.<par>
+* IM_GAUSS1D(K)<par>
+   Gauss method on the segment, order K (K=1,3,...99).<par>
+* IM_NC(N,K)<par>
+   Newton-Cotes approximative integration on simplexes, order K.<par>
+* IM_NC_PARALLELEPIPED(N,K)<par>
+   Product of Newton-Cotes integration on parallelepipeds.<par>
+* IM_NC_PRISM(N,K)<par>
+   Product of Newton-Cotes integration on prisms.<par>
+* IM_GAUSS_PARALLELEPIPED(N,K) <par>
+   Product of Gauss1D integration on parallelepipeds.<par>
+* IM_TRIANGLE(K)<par>
+   Gauss methods on triangles (K=1,3,5,6,7,8,9,10,13,17,19).<par>
+* IM_QUAD(K)<par>
+   Gauss methods on quadrilaterons (K=2, 3, 5, .. 17). Note that
+   IM_GAUSS_PARALLELEPIPED should be prefered for QK FEMs.<par>
+* IM_TETRAHEDRON(K)<par>
+   Gauss methods on tetrahedrons (K=1, 2, 3, 5, 6 or 8).<par>
+* IM_SIMPLEX4D(3)<par>
+   Gauss method on a 4-dimensional simplex.<par>
+* IM_STRUCTURED_COMPOSITE(IM1, K)<par>
+   Composite method on a grid with K divisions.<par>
+* IM_HCT_COMPOSITE(IM1)<par>
+   Composite integration suited to the HCT composite finite element.<Par>
+   example:<par>
+    - INTEG:INIT('IM_PRODUCT(IM_GAUSS1D(5),IM_GAUSS1D(5))')<par>
+   is the same as:<par>
+    - INTEG:INIT('IM_GAUSS_PARALLELEPIPED(2,5)')<par>
+@*/
 
 void gf_integ(getfemint::mexargs_in& in, getfemint::mexargs_out& out)
 {
