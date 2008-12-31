9f0ccdd248bbae62ba41a303f2daa99c2219f06c
Luis Saavedra <luis94855510@gmail.com>
2008-12-31 21:12:27 +0000

    improvement of @TEXT LEVELSET:INIT('LEVELSET_init') for the documentation

--- interface/src/gf_levelset.cc
+++ interface/src/gf_levelset.cc
@@ -26,14 +26,39 @@
 using namespace getfemint;
 
 /*@TEXT LEVELSET:INIT('LEVELSET_init')
-  General constructor for LevelSet objects.<Par>
+@tls = LEVELSET:INIT(...)<Par>
+
+The level-set object is represented by a primary level-set and<par>
+optionally a secondary level-set used to represent fractures (if<par>
+p(x) is the primary level-set function and s(x) is the secondary<par>
+level-set, the crack is defined by p(x)=0 and s(x)<=0: the role<par>
+of the secondary is stop the crack).<Par>
+
+* LEVELSET:INIT(@tmesh m, @int d)<par>
+   Create a @tls object on a @tmesh m represented by a primary<par>
+   function defined on a lagrange @tfem of degree d.<par>
+* LEVELSET:INIT(@tmesh m, @int d, @str poly1)<par>
+   Create a @tls object on a @tmesh m represented by a primary<par>
+   function defined by the polynomial expression poly1 on a<par>
+   lagrange @tfem of degree d.<par>
+* LEVELSET:INIT(@tmesh m, @int d, 'with_secondary' [, @str poly2])<par>
+   Create a @tls object on a @tmesh m represented by a primary<par>
+   function defined by the polynomial expression poly1 and a<par>
+   secondary function, both on a lagrange @tfem of degree d.<par>
+* LEVELSET:INIT(@tmesh m, @int d, @str poly1, @str poly2)<par>
+   Create a @tls object on a @tmesh m represented by a primary<par>
+   function defined by the polynomial expression poly1 and a<par>
+   secondary function defined by the polynomial expression poly2,<par>
+   both on a lagrange @tfem of degree d.<par>
   @*/
-void gf_levelset(getfemint::mexargs_in& in, getfemint::mexargs_out& out)
+void gf_levelset(
+getfemint::mexargs_in& in, getfemint::mexargs_out& out)
 {
   if (in.narg() < 2) THROW_BADARG("Wrong number of input arguments");
-  if (!out.narg_in_range(1,1)) 
+  if (!out.narg_in_range(1,1))
     THROW_BADARG("Wrong number of output arguments");
   getfemint_mesh *mm = in.pop().to_getfemint_mesh();
+
   size_type degree = in.pop().to_integer(1, 20);
   bool with_secondary = false;
   std::string s1, s2;
@@ -43,18 +68,18 @@ void gf_levelset(getfemint::mexargs_in& in, getfemint::mexargs_out& out)
       with_secondary = true; s1 = "";
       if (in.remaining() && in.front().is_string())
 	s1 = in.pop().to_string();
-    } 
+    }
     if (in.remaining() && in.front().is_string()) {
       s2 = in.pop().to_string();
       with_secondary = true;
     }
   }
 
-  if (in.remaining()) 
+  if (in.remaining())
     THROW_BADARG("too many arguments");
-    
 
-  getfem::level_set *ls = 
+
+  getfem::level_set *ls =
     new getfem::level_set(mm->mesh(),dim_type(degree),with_secondary);
   getfemint_levelset *gls = getfemint_levelset::get_from(ls);
   if (s1.size()) gls->values_from_poly(0, s1);
@@ -62,4 +87,3 @@ void gf_levelset(getfemint::mexargs_in& in, getfemint::mexargs_out& out)
 
   out.pop().from_object_id(gls->get_id(), LEVELSET_CLASS_ID);
 }
-
