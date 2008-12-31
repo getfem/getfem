47a1d94865d3f3b1cb2974ab77587161e19c8b61
Luis Saavedra <luis94855510@gmail.com>
2008-12-31 21:18:14 +0000

    Some "parser-functions" are added:
    #@GET LEVELSET:GET('values')
    #@RDATTR LEVELSET:GET('degree')
    #@GET LEVELSET:GET('mf')
    #@RDATTR LEVELSET:GET('memsize')

--- interface/src/gf_levelset_get.cc
+++ interface/src/gf_levelset_get.cc
@@ -28,7 +28,7 @@ using namespace getfemint;
 /*MLABCOM
   FUNCTION I = gf_levelset_get(LS, ...)
     General function for querying information about LEVELSET objects.
-    
+
 MLABCOM*/
 
 void gf_levelset_get(getfemint::mexargs_in& in, getfemint::mexargs_out& out)
@@ -38,24 +38,37 @@ void gf_levelset_get(getfemint::mexargs_in& in, getfemint::mexargs_out& out)
   }
   getfemint_levelset *gls = in.pop().to_getfemint_levelset();
   getfem::level_set &ls = gls->levelset();
+
   std::string cmd = in.pop().to_string();
   if (check_cmd(cmd, "values", in, out, 0, 1, 0, 1)) {
-    size_type il = 0; 
+    /*@GET @mat = LEVELSET:GET('values', @int nls)
+    Return the vector of DOF for nls funtion.
+
+    If nls is 0, the method return the vector of DOF for
+    the primary level-set funtion, so that these values
+    can be set. If nls is 1, the method return the vector
+    of DOF for the secondary level-set functionif any.@*/
+    size_type il = 0;
     if (in.remaining()) il = in.pop().to_integer(0, 1);
     if (il != 0 && !ls.has_secondary())
       THROW_BADARG("The levelset has not secondary term");
     out.pop().from_dcvector(ls.values(unsigned(il)));
   } else if (check_cmd(cmd, "degree", in, out, 0, 0, 0, 1)) {
+    /*@RDATTR @int = LEVELSET:GET('degree')
+    Return the degree of lagrange representation.@*/
     out.pop().from_integer(ls.degree());
   } else if (check_cmd(cmd, "mf", in, out, 0, 0, 0, 1)) {
-    
+    /*@GET @tmf = LEVELSET:GET('mf')
+    Return a reference on the @tmf object.@*/
     getfem::mesh_fem *pmf = const_cast<getfem::mesh_fem*>(&ls.get_mesh_fem());
-    getfemint_mesh_fem *gmf = 
+    getfemint_mesh_fem *gmf =
       getfemint_mesh_fem::get_from(pmf, STATIC_OBJ | CONST_OBJ);
     /*cerr << "nb_dof = " << ls.get_mesh_fem().nb_dof() << "\n";
       cerr << "mf = " << &ls.get_mesh_fem() << " == " << &gmf->mesh_fem() << "\n";*/
     out.pop().from_object_id(gmf->get_id(), gmf->class_id());
   } else if (check_cmd(cmd, "memsize", in, out, 0, 0, 0, 1)) {
+    /*@RDATTR @int = LEVELSET:GET('memsize')
+    Return the amount of memory (in bytes) used by the level-set.@*/
     out.pop().from_integer(int(ls.memsize()));
   } else bad_cmd(cmd);
 }
