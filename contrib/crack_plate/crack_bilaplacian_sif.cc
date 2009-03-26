47684abdb19f3cceee10a416bc1748096c9fa413
Jeremie Lasry <jeremie.lasry@siemens.com>
2009-03-26 15:59:18 +0000

    improvements in SIF computation

--- contrib/crack_plate/crack_bilaplacian_sif.cc
+++ contrib/crack_plate/crack_bilaplacian_sif.cc
@@ -56,7 +56,7 @@ namespace getfem {
       if ((in1 && out1) || (in2 && out2)) bv.add(cv);
       in += in1;
     }
-    if (in < 3) GMM_WARNING1("looks like the radius is too small...");
+    if (in < 8) GMM_WARNING1("looks like the radius is too small...");
     return bv;
   }
 
@@ -104,10 +104,6 @@ namespace getfem {
      integral computed on a ring around the crack tip, for BILAPLACIAN PLATE PROBLEM */
   template <typename VECT>
   void compute_crack_stress_intensity_factors_KL(const level_set &ls, 
-                                              const level_set &ls2,
-                                              const level_set &ls3,
-					      const mesh_level_set &mls2,
-					      const mesh_level_set &mls3,
                                               const mesh_im &mim,
                                               const mesh_im &mim2,
                                               const mesh_im &mim3,
@@ -129,7 +125,6 @@ namespace getfem {
 
     // set mesh_fem
     mesh_fem_global_function mf_mode(mring, 1), mf_mode_3(mring, 1);
-    mesh_fem mf_q(mring,1);
 
     std::vector<pglobal_function> cfun(2), cfun_3(4) ;
     for (unsigned j=4; j < 6; ++j)
@@ -142,48 +137,10 @@ namespace getfem {
     cfun_3[3] = bilaplacian_crack_singular(322, ls, nu, 0.) ;
     mf_mode_3.set_functions(cfun_3);
     mf_mode_3.set_qdim(2) ;
-
     mf_mode.set_qdim(1);
 
-
-
-    //std::string MESH_TYPE = PARAM.string_value("MESH_TYPE","Mesh type ");
+    
     getfem::pfem pf ;
-
-
-//     for (dal::bv_visitor i(mring.convex_index()) ; !i.finished() ; ++i){
-//         pf = mf_pre_u.fem_of_element(mf_pre_u.convex_index()[i]) ;
-//         mf_q.set_finite_element(i, pf);
-//     }
-//     cout << "OKKK\n" ;
-
-//     pf = mf.fem_of_element(mf.convex_index()[mring.convex_index().first_true()]) ;
-//     mf_q.set_finite_element(pf);  // mettre du FVS/HCT réduits
-
-
-
-    /* fill the "q" ring field with a C^1 field, equal to 
-       1 on the inner boundary, and equal to zero on the outer boundary */
-//     std::vector<scalar_type> q(mf_q.nb_dof());
-//     for (unsigned d = 0; d < mf_q.nb_dof(); ++d) {
-//         // getting a getfem::pdof_description about the curent dof
-//         const getfem::mesh::ind_cv_ct cvs = mf_q.convex_to_dof(d);
-//         unsigned cv = cvs[0], ld = unsigned(-1);
-//         //getfem::pfem pf = mf_q.fem_of_element(cv);
-//         // getting the local index  (ld)
-//         for (unsigned dd = 0; dd < mf_q.nb_dof_of_element(cv); dd += 1) {
-//            if (mf_q.ind_dof_of_element(cv)[dd] == d) {
-//              ld = dd; break;}
-//         }
-//         if ( pf->dof_types().at(ld) == getfem::lagrange_dof(2)){ 
-//                 // pf-> dof_types gives a std_vector<pdof_desciption>
-// //        if (ld == 0 || ld == 3 || ld == 6 || ld == 9){
-//            base_node P = mf_q.point_of_dof(d);
-//            q[d] = (gmm::vect_dist2(P, crack_tip) > ring_radius) ? 0. : 1.; 
-//         }
-//         else
-//            q[d] = 0. ;
-//     }
 
     /* fill the "q" ring field with a C^1 field, equal to 
        1 on the inner boundary, and equal to zero on the outer boundary */
@@ -302,7 +259,7 @@ cout << "OK avant assemblage\n" ;
 //             "t1=T1;"
 //             "V()+=D(p).T1(i,j,i).x1(j);");
     generic_assembly
-      assem("D_1_nu=data$1(1); D_nu=data$2(1); minus=data$3(1); D=data$4(1); x1=data$5(mdim(#1)); U1=data$6(#1); U2=data$7(#2); q=data$8(#3); U3=data$9(#4); x2=data$10(mdim(#1));"
+      assem("D_1_nu=data$1(1); D_nu=data$2(1); minus=data$3(1); D=data$4(1); x1=data$5(mdim(#1)); U1=data$6(#1); U2=data$7(#2); q=data$8(#3); U3=data$9(#4);"
             "T=U1(i).U2(j).q(k).comp(Hess(#1).Hess(#2).Grad(#3))(i,:,:,j,:,:,k,:);"
             "t=T;"
             "V()+=minus(p).D_1_nu(p).t(i,j,i,k,j).x1(k) + minus(p).D_nu(p).t(i,i,j,k,j).x1(k);"
@@ -314,21 +271,12 @@ cout << "OK avant assemblage\n" ;
             "T11=U1(i).U2(j).q(k).comp(Hess(#1).Grad(#2).Hess(#3))(i,:,:,j,:,k,:,:);"
             "t11=T11;"
             "V()+=minus(p).D(p).t11(i,i,j,k,k).x1(j);"
-            "V()+=minus(p).D(p).t(i,i,j,k,k).x1(j);"
-// 	    "TT2=U1(i).U2(j).q(k).comp(Hess(#1).Grad(#2).Grad(#3))(i,:,:,j,:,k,:);"
-// 	    "TT3=U1(i).U2(j).q(k).comp(%3, Hess(#1).Grad(#2).Grad(#3))(i,:,:,j,:,k,:);"
-// 	    "tt2=TT2;"
-// 	    "tt3=TT3;"
-// 	    "V()+=D(p).tt2(i,i,j,k).x1(j).x2(k);"
-// 	    "V()+=D(p).tt3(i,i,j,k).x1(j).x2(k);"
-);
+            "V()+=minus(p).D(p).t(i,i,j,k,k).x1(j);");
     assem.push_mf(mf);
     assem.push_mf(mf_mode);
-    assem.push_mf(mf_pre_u);//assem.push_mf(mf_q);
+    assem.push_mf(mf_pre_u);
     assem.push_mf(mf_mode_3);
     assem.push_mi(mim);
-    assem.push_mi(mim2);
-    assem.push_mi(mim3);
     base_vector vD_1_nu(1); vD_1_nu[0] =  D * (1. - nu);
     base_vector vD_nu(1); vD_nu[0] = D * nu ;
     base_vector minus(1); minus[0] = -1. ;
@@ -342,14 +290,60 @@ cout << "OK avant assemblage\n" ;
     assem.push_data(U_mode);
     assem.push_data(q);
     assem.push_data(U_mode_3);
-    assem.push_data(N); // Upward normal to the crack
     base_vector V(1);
     assem.push_vec(V);
 
+//             "TT2=q(k).comp(%2,Grad(#3))(k,:);"
+// 	    "tt2=TT2;" 
+// 	    "V()+=D(p).tt2(k).x2(k);"
 
-
-
-      
+    generic_assembly assem2("U1=data$1(#1);"
+                            "U2=data$2(#2);"
+			    "q=data$3(#3);"
+                            "D=data$4(1);"
+			    "x1=data$5(mdim(#1));"
+                            "x2=data$6(mdim(#1));"
+			    "minus=data$7(1);"
+	    "T=U1(i).U2(j).q(k).comp(Hess(#1).Grad(#2).Grad(#3))(i,:,:,j,:,k,:);"
+	    "t=T;"
+	    "V()+=minus(p).D(p).t(i,i,j,k).x1(j).x2(k);") ;
+    assem2.push_mf(mf);
+    assem2.push_mf(mf_mode);
+    assem2.push_mf(mf_pre_u);
+    assem2.push_mi(mim2);
+    assem2.push_data(U); 
+    assem2.push_data(U_mode);
+    assem2.push_data(q);
+    assem2.push_data(vD); 
+    assem2.push_data(T); // outgoing tangent of the crack 
+    assem2.push_data(N); // Upward normal to the crack
+    assem2.push_data(minus);
+    base_vector W2(1);
+    assem2.push_vec(W2);
+    
+    generic_assembly assem3("U1=data$1(#1);"
+                            "U2=data$2(#2);"
+			    "q=data$3(#3);"
+                            "D=data$4(1);"
+			    "x1=data$5(mdim(#1));"
+                            "x2=data$6(mdim(#1));"
+			    "minus=data$7(1);"
+	    "T=U1(i).U2(j).q(k).comp(Hess(#1).Grad(#2).Grad(#3))(i,:,:,j,:,k,:);"
+	    "t=T;"
+	    "V()+=D(p).t(i,i,j,k).x1(j).x2(k);") ;
+    assem3.push_mf(mf);
+    assem3.push_mf(mf_mode);
+    assem3.push_mf(mf_pre_u);
+    assem3.push_mi(mim3);
+    assem3.push_data(U); 
+    assem3.push_data(U_mode);
+    assem3.push_data(q);
+    assem3.push_data(vD); 
+    assem3.push_data(T); // outgoing tangent of the crack 
+    assem3.push_data(N); // Upward normal to the crack
+    assem3.push_data(minus);
+    base_vector W3(1);
+    assem3.push_vec(W3);
     
     /* fill with the crack opening mode I or mode II */
     for (unsigned mode = 1; mode <= 2; ++mode) {
@@ -389,10 +383,23 @@ cout << "OK avant assemblage\n" ;
       cout << "assemblig SIFs ..." << std::flush; 
       double time = gmm::uclock_sec();
       assem.assembly(cvring);
+      W2[0] = 0. ;
+      assem2.assembly(cvring);
+      W3[0] = 0. ;
+      assem3.assembly(cvring);
       cout << "done (" << gmm::uclock_sec()-time << " sec)\n";
       scalar_type a = 2. * epsilon * M_PI * (1. + nu) / (3. * young_modulus * (3. + nu) ) ;
       V[0] /= 2. * a ; 
-      if (mode == 1) KI = V[0]; else KII = V[0];
+      W2[0] /= 2. * a ;
+      W3[0] /= 2. * a ;
+      scalar_type W = W2[0] + W3[0] ;   
+      if ( mode == 2 ) {
+         cout << "mode " << mode << " ;\n  ";
+	 cout << "W="  << W     << "\n" ;
+         cout << "W2=" << W2[0] << "\n" ;
+	 cout << "W3=" << W3[0] << "\n" ;
+	 }
+      if (mode == 1) KI = V[0]  ; else KII = V[0] ;
     }
   }
 
@@ -415,31 +422,48 @@ void bilaplacian_crack_problem::compute_sif(const plain_vector &U, scalar_type r
   cout << "young modulus: " << E << "\n";
   //scalar_type ring_radius = 0.2;
   
-  scalar_type KI, KII;
+  scalar_type KI, KII ;
 /*  estimate_crack_stress_intensity_factors(ls, mf_u(), U, 
                                           E, 
                                           KI, KII, 1e-2);
   cout << "estimation of crack SIF: " << KI << ", " << KII << "\n";*/
     getfem::level_set ls2(mesh, 1, true) ;
-    getfem::level_set ls3(mesh, 1, true) ;
+    scalar_type x, y ;
+    for (size_type d = 0; d < ls2.get_mesh_fem().nb_dof(); ++d) {
+      x = ls2.get_mesh_fem().point_of_dof(d)[0];
+      y = ls2.get_mesh_fem().point_of_dof(d)[1];
+      ls2.values(0)[d] = y + 1e-6 ;
+      ls2.values(1)[d] = x ;
+      }
     getfem::mesh_level_set mls2(mesh);
-    getfem::mesh_level_set mls3(mesh);
     mls2.add_level_set(ls2);
+    mls2.adapt() ;
+    
+    getfem::level_set ls3(mesh, 1, true) ;
+    for (size_type d = 0; d < ls3.get_mesh_fem().nb_dof(); ++d) {
+      x = ls3.get_mesh_fem().point_of_dof(d)[0];
+      y = ls3.get_mesh_fem().point_of_dof(d)[1];
+      ls3.values(0)[d] = y - 1e-6 ;
+      ls3.values(1)[d] = x ;
+      }  
+    getfem::mesh_level_set mls3(mesh);
     mls3.add_level_set(ls3);
+    mls3.adapt() ;
+    
     
     cout << "initialisations ls2, ls3, mls2, mls3 : OK\n" ;
     std::string INTEGRATION = PARAM.string_value("INTEGRATION_LINE",
 					       "Name of integration method");
     std::string SIMPLEX_INTEGRATION = PARAM.string_value("SIMPLEX_INTEGRATION_LINE",
 					 "Name of simplex integration method");
-    std::string SINGULAR_INTEGRATION = PARAM.string_value("SINGULAR_INTEGRATION_LINE");
+    //std::string SINGULAR_INTEGRATION = PARAM.string_value("SINGULAR_INTEGRATION_LINE");
     
     getfem::pintegration_method ppi = 
     getfem::int_method_descriptor(INTEGRATION);
     getfem::pintegration_method sppi = 
     getfem::int_method_descriptor(SIMPLEX_INTEGRATION);
-    getfem::pintegration_method sing_ppi = (SINGULAR_INTEGRATION.size() ?
-		getfem::int_method_descriptor(SINGULAR_INTEGRATION) : 0);
+    //getfem::pintegration_method sing_ppi = (SINGULAR_INTEGRATION.size() ?
+//		getfem::int_method_descriptor(SINGULAR_INTEGRATION) : 0);
     
    int where = PARAM.int_value("WHERE") ;
 // 1	getfem::mesh_im_level_set::INTEGRATE_INSIDE (integrate over p(x)<0),
@@ -454,57 +478,134 @@ void bilaplacian_crack_problem::compute_sif(const plain_vector &U, scalar_type r
    where = getfem::mesh_im_level_set::INTEGRATE_BOUNDARY ;}
    break;
    }
-   getfem::mesh_im_level_set mim2(mls2, where, sppi, sing_ppi  ) ;
-   getfem::mesh_im_level_set mim3(mls3, where, sppi, sing_ppi  ) ;
+   getfem::mesh_im_level_set mim2(mls2, where, sppi) ;
+   getfem::mesh_im_level_set mim3(mls3, where, sppi) ;
    cout << "SIMPLEX_INTEGRATION = " << SIMPLEX_INTEGRATION << "\n" ;
-   cout << "SINGULAR_INTEGRATION = " << SINGULAR_INTEGRATION << "\n" ;
+   //cout << "SINGULAR_INTEGRATION = " << SINGULAR_INTEGRATION << "\n" ;
    
    
     cout << "initialisations mim2, mim3 : OK\n" ;
     
     mim2.set_integration_method(mesh.convex_index(), ppi);
+    mim2.adapt() ;
+    
     mim3.set_integration_method(mesh.convex_index(), ppi);
+    mim3.adapt() ;
     
     cout << "mim2.set_integration_method(... -> OK\n";
     
-    mls2.add_level_set(ls2);
-    mls3.add_level_set(ls3);
-    mim2.set_simplex_im(sppi, sing_ppi);
-    mim3.set_simplex_im(sppi, sing_ppi);
-    
-					       
-    cout << "Setting the two additional level-set (for integration purpose)... \n" ;
-    ls2.reinit();
-    cout << "ls2.reinit() ; -> OK \n" ;
-    
-    scalar_type x, y ;
-    cout << "ls2.get_mesh_fem().nb_dof() = " << ls2.get_mesh_fem().nb_dof() << "\n" ;
-    for (size_type d = 0; d < ls2.get_mesh_fem().nb_dof(); ++d) {
-      //cout << "d = " << d << "\n" ;
-      x = ls2.get_mesh_fem().point_of_dof(d)[0];
-      y = ls2.get_mesh_fem().point_of_dof(d)[1];
-      ls2.values(0)[d] = y + 0.2 ;
-      ls2.values(1)[d] = x ;
-      }
-    ls2.touch();
-    cout << "ls2.touch() -> OK \n" ;
-    mls2.adapt();
-    cout << "mls2.adapt() -> OK \n" ;
-    mim2.adapt();
-    
-    cout << "ls2 -> OK \n" ;
-    
-    ls3.reinit();
-    for (size_type d = 0; d < ls3.get_mesh_fem().nb_dof(); ++d) {
-      x = ls3.get_mesh_fem().point_of_dof(d)[0];
-      y = ls3.get_mesh_fem().point_of_dof(d)[1];
-      ls3.values(0)[d] = y - 1e-4 ;
-      ls3.values(1)[d] = x ;
-      }
-    ls3.touch();
-    mls3.adapt();
-    mim3.adapt();
     cout << "done.\nEntering in compute_crack_stress_intensity_factors_KL(... \n" ;
-  compute_crack_stress_intensity_factors_KL(ls, ls2, ls3, mls2, mls3, mim, mim2, mim3, mf_pre_u, mf_u(), U, ring_radius, D, nu, E, epsilon, KI, KII);
-  cout << "computation of crack SIF via J-Integral \nK1:" << KI << "\nK2:" << KII << "\n";
+  compute_crack_stress_intensity_factors_KL(ls, mim, mim2, mim3, mf_pre_u, mf_u(), U, ring_radius, D, nu, E, epsilon, KI, KII);
+  cout << "computation of crack SIF via J-Integral \nK1_J:" << KI << "\nK2_J:" << KII << "\n";
 }
+
+void bilaplacian_crack_problem::exact_sif(scalar_type &K1, scalar_type &K2){
+
+   scalar_type E, nu2 ;
+   base_small_vector k(2) ;
+   nu2 = nu * nu ;
+   E = 3. * (1. - nu2) * D / (2. * epsilon * epsilon * epsilon) ;
+
+   switch (sol_ref) {
+      case 0: {
+      K1 = exact_sol.U[2] * (3.*(nu - 1.)) + exact_sol.U[3] * (3. * nu + 5.) ;
+      K1 *= - ( sqrt(2.) * E * epsilon ) / (4. * (1. - nu2) ) ;
+      K2 = (exact_sol.U[0] + 3. * exact_sol.U[1]) ;
+      K2 *= - E * epsilon * sqrt(2.) * (3. + nu) / (4. * (1. + nu) * (1. + nu) ) ;
+      } break;
+      case 1: {
+      K1 = 3. * sqrt(PARAM.real_value("CRACK_SEMI_LENGTH")) / (2. * epsilon * epsilon) ;
+      K2 = 0. ;
+      } break;
+   }
+   cout << "K1_EXACT:"  << K1  << "\n" ;
+   cout << "K2_EXACT:" << K2 << "\n" ;      
+}
+
+size_type my_is_global_dof_type(getfem::pdof_description dof){
+size_type global_dof = 0 ;
+   for (dim_type d = 0; d < 4 ; ++d){
+       if (dof == getfem::global_dof(d)) {
+          global_dof = 1;
+	      }
+   }
+return global_dof ;
+}
+
+void bilaplacian_crack_problem::sif_direct_estimation(const plain_vector &U){
+   
+   unsigned q = mf_u().get_qdim();
+   base_small_vector tab_fic(4) ;
+   unsigned cpt = 0;
+   assert(enrichment_option > 2) ; // global singularities not available in XFEM 
+                                   // standard or without any enrichment.
+
+   // Looking for coefficients corresponding to global singularites
+   for (size_type d=0; d < mf_u().nb_dof(); d += q) {
+      size_type cv = mf_u().first_convex_of_dof(d) ;
+      getfem::pfem pf = mf_u().fem_of_element(cv);
+      unsigned ld = unsigned(-1);
+      for (unsigned dd = 0; dd < mf_u().nb_dof_of_element(cv); dd += q) {
+         if (mf_u().ind_dof_of_element(cv)[dd] == d){ 
+            ld = dd/q; 
+            break;
+         }
+      }   
+      if (ld == unsigned(-1)) {
+         cout << "DOF " << d << "NOT FOUND in " << cv << " BUG BUG\n";
+      } 
+      else {
+         if ( my_is_global_dof_type(pf->dof_types().at(ld)) ){
+         //cout << "coeff:" << U[d] << "\n" ;
+         tab_fic[cpt] = U[d] ;
+         cpt += 1 ;
+         }
+      }
+   }
+
+   scalar_type E, nu2 ;
+   base_small_vector k(2) ;
+   nu2 = nu * nu ;
+   E = 3. * (1. - nu2) * D / (2. * epsilon * epsilon * epsilon) ;
+
+   // Calculation of SIF KI and KII
+
+   if (PARAM.int_value("SING_BASE_TYPE") == 0){
+      k[0] = tab_fic[2] * (3.*(nu - 1.)) + tab_fic[3] * (3. * nu + 5.) ;
+      k[0] *= - ( sqrt(2.) * E * epsilon ) / (4. * (1. - nu2) ) ;
+      k[1] = (tab_fic[0] + 3. * tab_fic[1]) ;
+      k[1] *= - E * epsilon * sqrt(2.) * (3. + nu) / (4. * (1. + nu) * (1. + nu) ) ;
+   }
+   if (PARAM.int_value("SING_BASE_TYPE") == 1){
+      scalar_type A ;
+      A =  sqrt(2) * epsilon * E * (nu + 3.) / (1. - nu2) ;
+      k[0] = - tab_fic[0] * A ;
+      k[1] =   tab_fic[1] * A ; 
+   }
+   cout << "K1_DIRECT:" << k[0] << "\n" ;
+   cout << "K2_DIRECT:" << k[1] << "\n" ;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
