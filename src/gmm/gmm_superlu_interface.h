2db48ace6584bbd58878cb8c136f27509de3008a
Yves Renard <yves.renard@insa-lyon.fr>
2009-10-16 16:37:09 +0000

    minor changes

--- src/gmm/gmm_superlu_interface.h
+++ src/gmm/gmm_superlu_interface.h
@@ -314,9 +314,10 @@ namespace gmm {
       case 3 : options.ColPerm = COLAMD; break;
       }
       StatInit(&stat);
-      
-      Create_CompCol_Matrix(&SA, m, n, nz, csc_A.pr,
-			    (int *)(csc_A.ir), (int *)(csc_A.jc));
+
+      Create_CompCol_Matrix(&SA, m, n, nz, (double *)(&(csc_A.pr[0])),
+			    (int *)(&(csc_A.ir[0])), (int *)(&(csc_A.jc[0])));
+
       Create_Dense_Matrix(&SB, m, 0, &rhs[0], m);
       Create_Dense_Matrix(&SX, m, 0, &sol[0], m);
       memset(&SL,0,sizeof SL);
