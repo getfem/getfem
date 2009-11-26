2de5bd187bcffcc4a81a4f29814de3d258919e6a
Yann Collette <ycollette.nospam@free.fr>
2009-11-26 14:54:01 +0000

    wrong type name

--- interface/src/scilab/sci_gateway/c/sci_spchsolve.c
+++ interface/src/scilab/sci_gateway/c/sci_spchsolve.c
@@ -26,7 +26,7 @@ int sci_spchsolve(char * fname)
   VEC    * vB = NULL, * vOut = NULL;
   int      Index, i, j, res;
   double   value, alpha = 1.0;
-  StrErr   _StrErr;
+  SciErr   _SciErr;
   StrCtx   _StrCtx;
   int      nnz = 0, var_type;
 
@@ -34,9 +34,9 @@ int sci_spchsolve(char * fname)
   CheckLhs(1,1);
 
   // First, access to the input variable (a matrix of strings)
-  _StrErr = getVarAddressFromPosition(&_StrCtx, 1,&p_in_spmat_address);
+  _SciErr = getVarAddressFromPosition(&_StrCtx, 1,&p_in_spmat_address);
 
-  _StrErr = getVarType(&_StrCtx,p_in_spmat_address,&var_type);
+  _SciErr = getVarType(&_StrCtx,p_in_spmat_address,&var_type);
   if (var_type!=sci_sparse)
     {
       Scierror(999,"%s: wrong parameter, a sparse matrix is needed\n",fname);
@@ -49,13 +49,13 @@ int sci_spchsolve(char * fname)
       return 0;
     }
 
-  _StrErr = getSparseMatrix(&_StrCtx, p_in_spmat_address, &p_in_spmat_nb_rows, &p_in_spmat_nb_cols, 
+  _SciErr = getSparseMatrix(&_StrCtx, p_in_spmat_address, &p_in_spmat_nb_rows, &p_in_spmat_nb_cols, 
 			&p_in_spmat_nb_items, &p_in_spmat_items_row, &p_in_spmat_col_pos, &p_in_spmat_val);
 
   // Second, get b
-  _StrErr = getVarAddressFromPosition(&_StrCtx, 2, &p_in_b_dbl_address);
+  _SciErr = getVarAddressFromPosition(&_StrCtx, 2, &p_in_b_dbl_address);
 
-  _StrErr = getMatrixOfDouble(&_StrCtx, p_in_b_dbl_address, &p_in_b_nb_rows, &p_in_b_nb_cols, &p_in_b_dbl_matrix);
+  _SciErr = getMatrixOfDouble(&_StrCtx, p_in_b_dbl_address, &p_in_b_nb_rows, &p_in_b_nb_cols, &p_in_b_dbl_matrix);
 
   ////////////////////////////
   // Proceed the resolution //
@@ -88,7 +88,7 @@ int sci_spchsolve(char * fname)
   p_out_x_dbl_matrix = (double *)MALLOC(p_in_b_nb_rows*sizeof(double));
   memcpy(p_out_x_dbl_matrix,vOut->ve,p_in_b_nb_rows*sizeof(double));
 
-  _StrErr = createMatrixOfDouble(&_StrCtx, Rhs+1, p_in_b_nb_rows, p_in_b_nb_cols, p_out_x_dbl_matrix);
+  _SciErr = createMatrixOfDouble(&_StrCtx, Rhs+1, p_in_b_nb_rows, p_in_b_nb_cols, p_out_x_dbl_matrix);
 
   LhsVar(1) = Rhs+1;
 
--- interface/src/scilab/sci_gateway/c/sci_splusolve.c
+++ interface/src/scilab/sci_gateway/c/sci_splusolve.c
@@ -27,7 +27,7 @@ int sci_splusolve(char * fname)
   PERM   * pivot = NULL;
   int      Index, i, j, res;
   double   value, alpha = 1.0;
-  StrErr   _StrErr;
+  SciErr   _SciErr;
   StrCtx   _StrCtx;
   int      nnz = 0, var_type;
 
@@ -35,9 +35,9 @@ int sci_splusolve(char * fname)
   CheckLhs(1,1);
 
   // First, access to the input variable (a matrix of strings)
-  _StrErr = getVarAddressFromPosition(&_StrCtx, 1,&p_in_spmat_address);
+  _SciErr = getVarAddressFromPosition(&_StrCtx, 1,&p_in_spmat_address);
 
-  _StrErr = getVarType(&_StrCtx, p_in_spmat_address, &var_type);
+  _SciErr = getVarType(&_StrCtx, p_in_spmat_address, &var_type);
   if (var_type!=sci_sparse)
     {
       Scierror(999,"%s: wrong parameter, a sparse matrix is needed\n",fname);
@@ -50,13 +50,13 @@ int sci_splusolve(char * fname)
       return 0;
     }
 
-  _StrErr = getSparseMatrix(&_StrCtx, p_in_spmat_address, &p_in_spmat_nb_rows, &p_in_spmat_nb_cols, 
+  _SciErr = getSparseMatrix(&_StrCtx, p_in_spmat_address, &p_in_spmat_nb_rows, &p_in_spmat_nb_cols, 
 			    &p_in_spmat_nb_items, &p_in_spmat_items_row, &p_in_spmat_col_pos, &p_in_spmat_val);
 
   // Second, get b
-  _StrErr = getVarAddressFromPosition(&_StrCtx, 2, &p_in_b_dbl_address);
+  _SciErr = getVarAddressFromPosition(&_StrCtx, 2, &p_in_b_dbl_address);
 
-  _StrErr = getMatrixOfDouble(&_StrCtx, p_in_b_dbl_address, &p_in_b_nb_rows, &p_in_b_nb_cols, &p_in_b_dbl_matrix);
+  _SciErr = getMatrixOfDouble(&_StrCtx, p_in_b_dbl_address, &p_in_b_nb_rows, &p_in_b_nb_cols, &p_in_b_dbl_matrix);
 
   ////////////////////////////
   // Proceed the resolution //
@@ -92,7 +92,7 @@ int sci_splusolve(char * fname)
   p_out_x_dbl_matrix = (double *)MALLOC(p_in_b_nb_rows*sizeof(double));
   memcpy(p_out_x_dbl_matrix,vOut->ve,p_in_b_nb_rows*sizeof(double));
 
-  _StrErr = createMatrixOfDouble(&_StrCtx, Rhs+1, p_in_b_nb_rows, p_in_b_nb_cols, p_out_x_dbl_matrix);
+  _SciErr = createMatrixOfDouble(&_StrCtx, Rhs+1, p_in_b_nb_rows, p_in_b_nb_cols, p_out_x_dbl_matrix);
 
   LhsVar(1) = Rhs+1;
 
