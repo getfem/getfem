3f9e16d69eb0a3e4f0bf7ea06541624a79d266c6
Yves Renard <yves.renard@insa-lyon.fr>
2010-02-04 20:25:44 +0000

    minor changes

--- contrib/mixed_dynamic_friction/mixed_scalar_hyperbolic.m
+++ contrib/mixed_dynamic_friction/mixed_scalar_hyperbolic.m
@@ -5,8 +5,9 @@ if (0)
   %
   % last data produced
   %
-      
-  A = load('mixed_scalar_hyperbolic.data');
+  
+  A = load('mixed_scalar_hyperbolic_P2P1_NX20_DT01.data');
+  % A = load('mixed_scalar_hyperbolic_P2.data');
 
   % energy curves
 
@@ -31,17 +32,85 @@ if (0)
 
 end;
 
+
 %
-% Convergence for h and dt decreasing and P1+/P0 method
+% Convergence for dt decreasing and P2/P1 method
+%
+
+if (0)
+
+  A1 = load('mixed_scalar_hyperbolic_P2P1_NX20_DT01.data');
+  A2 = load('mixed_scalar_hyperbolic_P2P1_NX20_DT001.data');
+  % A3 = load('mixed_scalar_hyperbolic_P2P1_NX20_DT0001.data');
+
+  % energy curves
+
+  plot(A1(:, 1), A1(:, 2), '-k', 'linewidth', 2, 'MarkerSize', 15);
+  hold on;
+  plot(A2(:, 1), A2(:, 2), '--k', 'linewidth', 2, 'MarkerSize', 15);
+  % plot(A3(:, 1), A3(:, 2), '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  hold off;
+  % axis([0 0.7 0 0.1]);
+  axis([0 0.8 0 0.02]);
+  xlabel('t');
+  ylabel('total energy');
+  legend('dt = 0.01', 'dt = 0.001', 'Location', 'SouthWest');
+  axesobj = findobj('type', 'axes'); set(axesobj, 'fontname', 'times'); set(axesobj, 'fontunits', 'points'); set(axesobj, 'fontsize', 24); set(axesobj, 'fontweight', 'bold'); set(axesobj, 'linewidth', 2);
+  pause;
+  print(gcf,'-dpdf','-r450', 'energy.pdf');
+
+  % displacement curves
+
+  plot(A1(:, 1), A1(:, 3), '-k', 'linewidth', 2, 'MarkerSize', 15);
+  hold on;
+  plot(A2(:, 1), A2(:, 3), '--k', 'linewidth', 2, 'MarkerSize', 15);
+  % plot(A3(:, 1), A3(:, 3), '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  hold off;
+  % axis([0 0.7 -0.01 0.06]);
+  axis([0 0.8 -0.01 0.06]);
+  xlabel('t');
+  ylabel('center point displacement');
+  legend('dt = 0.01', 'dt = 0.001', 'Location', 'SouthWest');
+  axesobj = findobj('type', 'axes'); set(axesobj, 'fontname', 'times'); set(axesobj, 'fontunits', 'points'); set(axesobj, 'fontsize', 24); set(axesobj, 'fontweight', 'bold'); set(axesobj, 'linewidth', 2);
+  pause;
+  print(gcf,'-dpdf','-r450', 'displacement.pdf');
+
+  % contact stress curves
+
+  plot(A1(:, 1), A1(:, 4)*8, '-k', 'linewidth', 2, 'MarkerSize', 15);
+  hold on;
+  plot(A2(:, 1), A2(:, 4)*8, '--k', 'linewidth', 2, 'MarkerSize', 15);
+  % plot(A3(:, 1), A3(:, 4)*8, '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  hold off;
+  % axis([0 0.8 -0.6 0.02]);
+  xlabel('t');
+  ylabel('center point contact stress');
+legend('dt = 0.01', 'dt = 0.001', 'Location', 'SouthWest');
+  axesobj = findobj('type', 'axes'); set(axesobj, 'fontname', 'times'); set(axesobj, 'fontunits', 'points'); set(axesobj, 'fontsize', 24); set(axesobj, 'fontweight', 'bold'); set(axesobj, 'linewidth', 2);
+  pause;
+  print(gcf,'-dpdf','-r450', 'stress.pdf');
+
+
+end;
+
+
+
+
+
+
+
+%
+% Convergence for h and dt decreasing and P2/P1 method
 %
 
 if (1)
 
-  A1 = load('mixed_scalar_hyperbolic_P1P0_NX4_DT01.data');
-  A2 = load('mixed_scalar_hyperbolic_P1P0_NX10_DT005.data');
-  A3 = load('mixed_scalar_hyperbolic_P1P0_NX20_DT0025.data');
-  A4 = load('mixed_scalar_hyperbolic_P1P0_NX40_DT00125.data');
-  A5 = load('mixed_scalar_hyperbolic_P1P0_NX80_DT000625.data');
+  A1 = load('mixed_scalar_hyperbolic_P2P1_NX4_DT01.data'); h1 = 1/4;
+  A2 = load('mixed_scalar_hyperbolic_P2P1_NX10_DT005.data'); h2 = 1/10;
+  A3 = load('mixed_scalar_hyperbolic_P2P1_NX20_DT0025.data'); h3 = 1/20;
+  A4 = load('mixed_scalar_hyperbolic_P2P1_NX40_DT00125.data'); h4 = 1/40;
+  A5 = load('mixed_scalar_hyperbolic_P2P1_NX80_DT000625.data'); h5 = 1/80;
+  A6 = load('mixed_scalar_hyperbolic_P2P1_NX160_DT0003125.data'); h6 = 1/160;
 
   % energy curves
 
@@ -49,8 +118,85 @@ if (1)
   hold on;
   plot(A2(:, 1), A2(:, 2), '--k', 'linewidth', 2, 'MarkerSize', 15);
   plot(A3(:, 1), A3(:, 2), '-.k', 'linewidth', 2, 'MarkerSize', 15);
-  plot(A4(:, 1), A4(:, 2), '-.k', 'linewidth', 2, 'MarkerSize', 15);
-  plot(A5(:, 1), A5(:, 2), '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A4(:, 1), A4(:, 2), ':k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A5(:, 1), A5(:, 2), '-k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A6(:, 1), A6(:, 2), '--k', 'linewidth', 2, 'MarkerSize', 15);
+  hold off;
+  % axis([0 0.7 0 0.1]);
+  axis([0 0.7 0 0.02]);
+  xlabel('t');
+  ylabel('total energy');
+  legend('h = 0.25, dt = 0.01', 'h = 0.1, dt = 0.005', 'h = 0.5, dt = 0.0025', 'h = 0.25, dt = 0.00125', 'h = 0.125, dt = 0.000625', 'h = 0.0625, dt = 0.0003125', 'Location', 'SouthWest');
+  axesobj = findobj('type', 'axes'); set(axesobj, 'fontname', 'times'); set(axesobj, 'fontunits', 'points'); set(axesobj, 'fontsize', 24); set(axesobj, 'fontweight', 'bold'); set(axesobj, 'linewidth', 2);
+  pause;
+  print(gcf,'-depsc','-r450', 'energy.eps');
+
+  % displacement curves
+
+  plot(A1(:, 1), A1(:, 3)+0.025, '-k', 'linewidth', 2, 'MarkerSize', 15);
+  hold on;
+  plot(A2(:, 1), A2(:, 3)+0.020, '--k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A3(:, 1), A3(:, 3)+0.015, '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A4(:, 1), A4(:, 3)+0.010, '-b', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A5(:, 1), A5(:, 3)+0.005, '--b', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A6(:, 1), A6(:, 3), '-.b', 'linewidth', 2, 'MarkerSize', 15);
+  hold off;
+  % axis([0 0.7 -0.01 0.06]);
+  axis([0 0.7 -0.01 0.06]);
+  xlabel('t');
+  ylabel('center point displacement');
+  legend('h = 0.25, dt = 0.01', 'h = 0.1, dt = 0.005', 'h = 0.5, dt = 0.0025', 'h = 0.25, dt = 0.00125', 'h = 0.125, dt = 0.000625', 'h = 0.0625, dt = 0.0003125', 'Location', 'SouthWest');
+  axesobj = findobj('type', 'axes'); set(axesobj, 'fontname', 'times'); set(axesobj, 'fontunits', 'points'); set(axesobj, 'fontsize', 16); set(axesobj, 'fontweight', 'bold'); set(axesobj, 'linewidth', 2);
+  pause;
+  print(gcf,'-depsc','-r450', 'displacement.eps');
+
+  % contact stress curves
+
+  plot(A1(:, 1), A1(:, 4) / ((h1*10)^2), '-k', 'linewidth', 2, 'MarkerSize', 15);
+  hold on;
+  plot(A2(:, 1), A2(:, 4) / ((h2*10)^2) - 0.1, '--k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A3(:, 1), A3(:, 4) / ((h3*10)^2) - 0.2, '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A4(:, 1), A4(:, 4) / ((h4*10)^2) - 0.3, '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A5(:, 1), A5(:, 4) / ((h5*10)^2) - 0.4, '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A6(:, 1), A6(:, 4) / ((h6*10)^2) - 0.5, ':k', 'linewidth', 2, 'MarkerSize', 15);
+  hold off;
+  % axis([0 0.7 -0.6 0.02]);
+  xlabel('t');
+  ylabel('center point contact stress');
+legend('dt = 0.24', 'dt = 0.1', 'dt = 0.05', 'dt = 0.003125', 'Location', 'SouthWest');
+  axesobj = findobj('type', 'axes'); set(axesobj, 'fontname', 'times'); set(axesobj, 'fontunits', 'points'); set(axesobj, 'fontsize', 24); set(axesobj, 'fontweight', 'bold'); set(axesobj, 'linewidth', 2);
+  pause;
+  print(gcf,'-depsc','-r450', 'stress');
+
+
+end;
+
+
+
+
+
+%
+% Convergence for h and dt decreasing and P1+/P0 method
+%
+
+if (0)
+
+  A1 = load('mixed_scalar_hyperbolic_P1P0_NX4_DT01.data'); h1 = 1/4;
+  A2 = load('mixed_scalar_hyperbolic_P1P0_NX10_DT005.data'); h2 = 1/10;
+  A3 = load('mixed_scalar_hyperbolic_P1P0_NX20_DT0025.data'); h3 = 1/20;
+  A4 = load('mixed_scalar_hyperbolic_P1P0_NX40_DT00125.data'); h4 = 1/40;
+  A5 = load('mixed_scalar_hyperbolic_P1P0_NX80_DT000625.data'); h5 = 1/80;
+  A6 = load('mixed_scalar_hyperbolic_P1P0_NX160_DT0003125.data'); h6 = 1/160;
+
+  % energy curves
+
+  plot(A1(:, 1), A1(:, 2), '-k', 'linewidth', 2, 'MarkerSize', 15);
+  hold on;
+  plot(A2(:, 1), A2(:, 2), '--k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A3(:, 1), A3(:, 2), '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A4(:, 1), A4(:, 2), ':k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A5(:, 1), A5(:, 2), '-k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A6(:, 1), A6(:, 2), '--k', 'linewidth', 2, 'MarkerSize', 15);
   hold off;
   % axis([0 0.7 0 0.1]);
   axis([0 0.7 0 0.02]);
@@ -59,45 +205,56 @@ if (1)
   legend('dt = 0.08', 'dt = 0.04', 'dt = 0.02', 'dt = 0.01', 'dt = 0.005', 'Location', 'SouthWest');
   axesobj = findobj('type', 'axes'); set(axesobj, 'fontname', 'times'); set(axesobj, 'fontunits', 'points'); set(axesobj, 'fontsize', 24); set(axesobj, 'fontweight', 'bold'); set(axesobj, 'linewidth', 2);
   pause;
-  print(gcf,'-deps','-r450', 'energy.eps');
+  print(gcf,'-depsc','-r450', 'energy.eps');
 
   % displacement curves
 
-  plot(A1(:, 1), A1(:, 3), '-k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A1(:, 1), A1(:, 3)+0.025, '-k', 'linewidth', 2, 'MarkerSize', 15);
   hold on;
-  plot(A2(:, 1), A2(:, 3), '--k', 'linewidth', 2, 'MarkerSize', 15);
-  plot(A3(:, 1), A3(:, 3), '-.k', 'linewidth', 2, 'MarkerSize', 15);
-  plot(A4(:, 1), A4(:, 3), '-.k', 'linewidth', 2, 'MarkerSize', 15);
-  plot(A5(:, 1), A5(:, 3), '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A2(:, 1), A2(:, 3)+0.020, '--k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A3(:, 1), A3(:, 3)+0.015, '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A4(:, 1), A4(:, 3)+0.010, '-b', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A5(:, 1), A5(:, 3)+0.005, '--b', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A6(:, 1), A6(:, 3), '-.b', 'linewidth', 2, 'MarkerSize', 15);
   hold off;
   % axis([0 0.7 -0.01 0.06]);
   axis([0 0.7 -0.01 0.06]);
   xlabel('t');
   ylabel('center point displacement');
-  legend('dt = 0.01', 'dt = 0.001', 'dt = 0.0001', 'Location', 'SouthWest');
-  axesobj = findobj('type', 'axes'); set(axesobj, 'fontname', 'times'); set(axesobj, 'fontunits', 'points'); set(axesobj, 'fontsize', 24); set(axesobj, 'fontweight', 'bold'); set(axesobj, 'linewidth', 2);
+  legend('h = 0.25, dt = 0.01', 'h = 0.1, dt = 0.005', 'h = 0.5, dt = 0.0025', 'h = 0.25, dt = 0.00125', 'h = 0.125, dt = 0.000625', 'h = 0.0625, dt = 0.0003125', 'Location', 'SouthWest');
+  axesobj = findobj('type', 'axes'); set(axesobj, 'fontname', 'times'); set(axesobj, 'fontunits', 'points'); set(axesobj, 'fontsize', 16); set(axesobj, 'fontweight', 'bold'); set(axesobj, 'linewidth', 2);
   pause;
-  print(gcf,'-deps','-r450', 'displacement.eps');
+  print(gcf,'-depsc','-r450', 'displacement.eps');
 
   % contact stress curves
 
-  plot(A1(:, 1), A1(:, 4), '-k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A1(:, 1), A1(:, 4) / (h1*10)^2, '-k', 'linewidth', 2, 'MarkerSize', 15);
   hold on;
-  plot(A2(:, 1), A2(:, 4), '--k', 'linewidth', 2, 'MarkerSize', 15);
-  plot(A3(:, 1), A3(:, 4), '-.k', 'linewidth', 2, 'MarkerSize', 15);
-  plot(A4(:, 1), A4(:, 4), '-.k', 'linewidth', 2, 'MarkerSize', 15);
-  plot(A5(:, 1), A5(:, 4), '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A2(:, 1), A2(:, 4) / (h2*10)^2, '--k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A3(:, 1), A3(:, 4) / (h3*10)^2, '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A4(:, 1), A4(:, 4) / (h4*10)^2, '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A5(:, 1), A5(:, 4) / (h5*10)^2, '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  plot(A6(:, 1), A6(:, 4) / (h6*10)^2, '-.k', 'linewidth', 2, 'MarkerSize', 15);
   hold off;
-  axis([0 0.7 -0.6 0.02]);
+  % axis([0 0.7 -0.6 0.02]);
   xlabel('t');
   ylabel('center point contact stress');
   legend('dt = 0.01', 'dt = 0.001', 'dt = 0.0001', 'Location', 'SouthWest');
   axesobj = findobj('type', 'axes'); set(axesobj, 'fontname', 'times'); set(axesobj, 'fontunits', 'points'); set(axesobj, 'fontsize', 24); set(axesobj, 'fontweight', 'bold'); set(axesobj, 'linewidth', 2);
   pause;
-  print(gcf,'-deps','-r450', 'stress');
+  print(gcf,'-depsc','-r450', 'stress');
 
 
 end;
+
+
+
+
+
+
+
+
+
 
 
 
@@ -105,7 +262,7 @@ end;
 % Convergence in energy curves
 %
 
-if (1)
+if (0)
   % P1+/P0 method
   A = [0.1    0.04   0.01     0.004     0.001     0.0004    0.0001;  % dt
        165.9  1.129  0.3874  8.083e-2  9.166e-3  8.333e-4  9.166e-5; % NX=4
@@ -114,19 +271,19 @@ if (1)
        207    208    96.94   22.84499  0.8558    0.2424    0.019999; % NX=40
        207    211    75.33   168       4.30      0.5608    0.06833   % NX=80
        ];
-  loglog(A(1,:), A(2, :), '-k', 'linewidth', 2, 'MarkerSize', 15);
+  loglog(A(1,:)*4, A(2, :),  '-k', 'linewidth', 2, 'MarkerSize', 15);
   hold on;
-  loglog(A(1,:), A(3, :), '--k', 'linewidth', 2, 'MarkerSize', 15);
-  loglog(A(1,:), A(4, :), '-.k', 'linewidth', 2, 'MarkerSize', 15);
-  loglog(A(1,:), A(5, :), '-.k', 'linewidth', 2, 'MarkerSize', 15);
-  loglog(A(1,:), A(6, :), '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  loglog(A(1,:)*10, A(3, :), '--k', 'linewidth', 2, 'MarkerSize', 15);
+  loglog(A(1,:)*20, A(4, :), '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  loglog(A(1,:)*40, A(5, :),  '-b.', 'linewidth', 2, 'MarkerSize', 15);
+  loglog(A(1,:)*80, A(6, :), '--b.', 'linewidth', 2, 'MarkerSize', 15);
   hold off;
-  xlabel('dt');
+  xlabel('dt/h');
   ylabel('relative error on total energy (%)');
   legend('h = 0.25', 'h = 0.1', 'h = 0.05', 'h = 0.025', 'h = 0.0125', 'Location', 'SouthEast');
   axesobj = findobj('type', 'axes'); set(axesobj, 'fontname', 'times'); set(axesobj, 'fontunits', 'points'); set(axesobj, 'fontsize', 24); set(axesobj, 'fontweight', 'bold'); set(axesobj, 'linewidth', 2);
   pause;
-  print(gcf,'-deps','-r450', 'toto');
+  print(gcf,'-depsc','-r450', 'toto1');
 
 
 
@@ -138,19 +295,19 @@ if (1)
        195    166    6.0674   2.44666   0.0641666 0.032499  0.00166666; % NX=40
        204    198    23.15    5.53160   0.45083   0.1816    0.0033333   % NX=80
        ];
-  loglog(A(1,:), A(2, :), '-k', 'linewidth', 2, 'MarkrSize', 15);
+  loglog(A(1,:)*4, A(2, :), '-k', 'linewidth', 2, 'MarkerSize', 15);
   hold on;
-  loglog(A(1,:), A(3, :), '--k', 'linewidth', 2, 'MarkerSize', 15);
-  loglog(A(1,:), A(4, :), '-.k', 'linewidth', 2, 'MarkerSize', 15);
-  loglog(A(1,:), A(5, :), '-.k', 'linewidth', 2, 'MarkerSize', 15);
-  loglog(A(1,:), A(6, :), '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  loglog(A(1,:)*10, A(3, :), '--k', 'linewidth', 2, 'MarkerSize', 15);
+  loglog(A(1,:)*20, A(4, :), '-.k', 'linewidth', 2, 'MarkerSize', 15);
+  loglog(A(1,:)*40, A(5, :), '-b.', 'linewidth', 2, 'MarkerSize', 15);
+  loglog(A(1,:)*80, A(6, :), '--b.', 'linewidth', 2, 'MarkerSize', 15);
   hold off;
-  xlabel('dt');
+  xlabel('dt/h');
   ylabel('relative error on total energy (%)');
   legend('h = 0.25', 'h = 0.1', 'h = 0.05', 'h = 0.025', 'h = 0.0125', 'Location', 'SouthEast');
   axesobj = findobj('type', 'axes'); set(axesobj, 'fontname', 'times'); set(axesobj, 'fontunits', 'points'); set(axesobj, 'fontsize', 24); set(axesobj, 'fontweight', 'bold'); set(axesobj, 'linewidth', 2);
   pause;
-  print(gcf,'-deps','-r450', 'toto');
+  print(gcf,'-depsc','-r450', 'toto2');
 
 end;
 
@@ -182,7 +339,7 @@ if (0)
   legend('dt = 0.01', 'dt = 0.001', 'dt = 0.0001', 'Location', 'SouthWest');
   axesobj = findobj('type', 'axes'); set(axesobj, 'fontname', 'times'); set(axesobj, 'fontunits', 'points'); set(axesobj, 'fontsize', 24); set(axesobj, 'fontweight', 'bold'); set(axesobj, 'linewidth', 2);
   pause;
-  print(gcf,'-deps','-r450', 'energy.eps');
+  print(gcf,'-depsc','-r450', 'energy.eps');
 
   % displacement curves
 
@@ -198,7 +355,7 @@ if (0)
   legend('dt = 0.01', 'dt = 0.001', 'dt = 0.0001', 'Location', 'SouthWest');
   axesobj = findobj('type', 'axes'); set(axesobj, 'fontname', 'times'); set(axesobj, 'fontunits', 'points'); set(axesobj, 'fontsize', 24); set(axesobj, 'fontweight', 'bold'); set(axesobj, 'linewidth', 2);
   pause;
-  print(gcf,'-deps','-r450', 'displacement.eps');
+  print(gcf,'-depsc','-r450', 'displacement.eps');
 
   % contact stress curves
 
@@ -213,7 +370,7 @@ if (0)
   legend('dt = 0.01', 'dt = 0.001', 'dt = 0.0001', 'Location', 'SouthWest');
   axesobj = findobj('type', 'axes'); set(axesobj, 'fontname', 'times'); set(axesobj, 'fontunits', 'points'); set(axesobj, 'fontsize', 24); set(axesobj, 'fontweight', 'bold'); set(axesobj, 'linewidth', 2);
   pause;
-  print(gcf,'-deps','-r450', 'stress');
+  print(gcf,'-depsc','-r450', 'stress');
 
 
 end;
