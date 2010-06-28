81f464c358d8ff7a1bda48a90fd8ef07c7683834
Khalil Mansouri <khalil.mansouri@gmail.com>
2010-06-28 14:06:22 +0000

    Work in progress

--- contrib/xfem_large_strain/getfem_nonlinear_elastoptim.h
+++ contrib/xfem_large_strain/getfem_nonlinear_elastoptim.h
@@ -20,14 +20,43 @@
 //===========================================================================
 
 
+#include "getfem/getfem_modeling.h"
 #include "getfem/getfem_models.h"
+#include "getfem/getfem_assembling_tensors.h"
+#include "getfem/getfem_derivatives.h"
+#include "getfem/getfem_interpolation.h"
+#include "getfem/getfem_assembling.h" 
+#include "getfem/getfem_export.h"   
+#include "getfem/getfem_derivatives.h"
+#include "getfem/getfem_regular_meshes.h"
+#include "getfem/getfem_model_solvers.h"
 #include "getfem/getfem_nonlinear_elasticity.h"
+#include "getfem/getfem_mesh_im_level_set.h"
+#include "getfem/getfem_mesh_fem_level_set.h"
+#include "getfem/getfem_mesh_fem_product.h"
+#include "getfem/getfem_mesh_fem_global_function.h"
+#include "getfem/getfem_spider_fem.h"
+#include "getfem/getfem_mesh_fem_sum.h"
+#include "getfem/getfem_superlu.h"
+#include "gmm/gmm.h"
+#include "gmm/gmm_inoutput.h"
+
+
 
 namespace getfem {
 
 
 
+  inline scalar_type mat_euclidean_sp(base_matrix &Sigma, base_matrix &Ealpha) {
+    return gmm::vect_sp((std::vector<scalar_type> &)Sigma, (std::vector<scalar_type> &)Ealpha);
+  }
 
+
+  //==============================================================================================================================
+  /*************************/ 
+  /* Term 1 dL/dbeta       */
+  /*************************/
+  //=============================================================================================================================
   template<typename VECT1, typename VECT2>
   class elasticity_nonlinear_optim_term1
     : public getfem::nonlinear_elem_term {
@@ -38,8 +67,7 @@ namespace getfem {
     const mesh_fem *mf_data;
     const VECT2 &PARAMS;
     const getfem::level_set &ls;
-    size_type N, cv_old;
-    size_type NFem;
+    size_type N, cv_old,  NFem;
     const abstract_hyperelastic_law &AHL;
     base_vector params, coeff, valP;
     base_matrix E, Sigma, gradU;
@@ -55,13 +83,11 @@ namespace getfem {
       : mf_u(mf_u_), U(mf_u_.nb_basic_dof()), U_ls(mf_u_.nb_basic_dof()),
 	mf_p(mf_p_),
 	P(mf_p_.nb_basic_dof()), P_ls(mf_p_.nb_basic_dof()),
-	mf_data(mf_data_), PARAMS(PARAMS_), ls(ls_);
-	N(mf_.linked_mesh().dim()), NFem(mf_.get_qdim()), AHL(AHL_),
+	mf_data(mf_data_), PARAMS(PARAMS_), ls(ls_),
+	N(mf_u_.linked_mesh().dim()), NFem(mf_u_.get_qdim()), AHL(AHL_),
 	params(AHL_.nb_params()), E(N, N), Sigma(N, N), gradU(NFem, N),
 	tt(N, N, N, N), sizes_(NFem, N, NFem, N) {
-      switch (version) {
-	sizes_.resize(1); sizes_[0] = 1; break;
-      }
+      sizes_.resize(1); sizes_[0] = 1;
       cv_old = size_type(-1);
       mf_u.extend_vector(U_, U);
       mf_p.extend_vector(P_, P);
@@ -87,7 +113,7 @@ namespace getfem {
       t[0] = (J - scalar_type(1)) * valP[0];
     }
     virtual void prepare(fem_interpolation_context& ctx, size_type nb) {
-      GMM_ASSERT(nb != 0, "Oops");
+      GMM_ASSERT1(nb != 0, "Oops");
       if (nb == 1) {
 
 	valP.resize(1);
@@ -95,7 +121,7 @@ namespace getfem {
 	coeff.resize(mf_p.nb_basic_dof_of_element(cv));
 	gmm::copy(gmm::sub_vector
 	     (P_ls, gmm::sub_index(mf_p.ind_basic_dof_of_element(cv))), coeff);
-	ctx.pf()->interpolation(ctx, coeff, valP);
+	ctx.pf()->interpolation(ctx, coeff, valP, 1);
 	
 
 	if (cv != cv_old) {
@@ -109,18 +135,145 @@ namespace getfem {
 	valP[0] *= log(x*x+y*y) / scalar_type(2);
       } else if (mf_data) {
 	size_type cv = ctx.convex_num();
-	size_type nb = AHL.nb_params();
-	coeff.resize(mf_data->nb_basic_dof_of_element(cv)*nb);
+	size_type nbp = AHL.nb_params();
+	coeff.resize(mf_data->nb_basic_dof_of_element(cv)*nbp);
 	for (size_type i = 0; i < mf_data->nb_basic_dof_of_element(cv); ++i)
-	  for (size_type k = 0; k < nb; ++k)
-	    coeff[i * nb + k]
-	      = PARAMS[mf_data->ind_basic_dof_of_element(cv)[i]*nb+k];
-	ctx.pf()->interpolation(ctx, coeff, params, dim_type(nb));
+	  for (size_type k = 0; k < nbp; ++k)
+	    coeff[i * nbp + k]
+	      = PARAMS[mf_data->ind_basic_dof_of_element(cv)[i]*nbp+k];
+	ctx.pf()->interpolation(ctx, coeff, params, dim_type(nbp));
       }
     } 
     
   };
 
+//========================================================================================================================
+ /*************************/
+ /* Term 2 dL/dalpha      */
+ /*************************/
+//========================================================================================================================
+ template<typename VECT1, typename VECT2>
+  class elasticity_nonlinear_optim_term2
+    : public getfem::nonlinear_elem_term {
+    const mesh_fem &mf_u;
+    std::vector<scalar_type> U, U_ls;
+    const mesh_fem &mf_p;
+    std::vector<scalar_type> P, P_ls;
+    const mesh_fem *mf_data;
+    const VECT2 &PARAMS;
+    const getfem::level_set &ls;
+    size_type N, cv_old, NFem;
+    const abstract_hyperelastic_law &AHL;
+    base_vector params, coeff, valP, u_ls;
+    base_matrix E, Sigma, gradU, gradU_ls, U_ls_theta;// U_ls_theta le terme U_ls multiplier par le vecteur 
+    base_tensor tt;
+    bgeot::multi_index sizes_;
+    mesher_level_set mls_x, mls_y;
+  public:
+    elasticity_nonlinear_optim_term2
+    (const mesh_fem &mf_u_, const VECT1 &U_, const VECT1 &U_ls_, 
+     const mesh_fem &mf_p_, const VECT1 &P_, const VECT1 &P_ls_,
+     const mesh_fem *mf_data_, const VECT2 &PARAMS_,
+     const abstract_hyperelastic_law &AHL_, const getfem::level_set &ls_) 
+      : mf_u(mf_u_), U(mf_u_.nb_basic_dof()), U_ls(mf_u_.nb_basic_dof()),
+	mf_p(mf_p_),
+	P(mf_p_.nb_basic_dof()), P_ls(mf_p_.nb_basic_dof()),
+	mf_data(mf_data_), PARAMS(PARAMS_), ls(ls_),
+	N(mf_u_.linked_mesh().dim()), NFem(mf_u_.get_qdim()), AHL(AHL_),
+	params(AHL_.nb_params()), u_ls(N), E(N, N), Sigma(N, N), gradU(NFem, N),
+	tt(N, N, N, N), sizes_(NFem, N, NFem, N) {
+      sizes_.resize(1); sizes_[0] = 1;
+      cv_old = size_type(-1);
+      mf_u.extend_vector(U_, U);
+      mf_p.extend_vector(P_, P);
+      mf_u.extend_vector(U_ls_, U_ls);
+      mf_p.extend_vector(P_ls_, P_ls);
+      if (gmm::vect_size(PARAMS) == AHL_.nb_params())
+	gmm::copy(PARAMS, params);
+    }
+    const bgeot::multi_index &sizes() const {  return sizes_; }
+    virtual void compute(getfem::fem_interpolation_context& ctx,
+			 bgeot::base_tensor &t) {
+      size_type cv = ctx.convex_num();
+      coeff.resize(mf_u.nb_basic_dof_of_element(cv));
+      gmm::copy(gmm::sub_vector
+		(U, gmm::sub_index(mf_u.ind_basic_dof_of_element(cv))), coeff);
+      ctx.pf()->interpolation_grad(ctx, coeff, gradU, mf_u.get_qdim());
+
+
+      gmm::mult(gmm::transposed(gradU), gradU, E);
+      gmm::add(gradU, E);
+      gmm::add(gmm::transposed(gradU), E);
+      gmm::scale(E, scalar_type(0.5));
+
+
+      for (unsigned int alpha = 0; alpha < N; ++alpha)
+	gradU(alpha, alpha)+= scalar_type(1);
+      
+      scalar_type J = gmm::lu_det(gradU);
+
+
+      // Computation of d/dalpha (grad U)
+
+      coeff.resize(mf_u.nb_basic_dof_of_element(cv));
+      gmm::copy(gmm::sub_vector
+		(U_ls, gmm::sub_index(mf_u.ind_basic_dof_of_element(cv))), coeff);
+      ctx.pf()->interpolation_grad(ctx, coeff, gradU_ls, mf_u.get_qdim());
+      scalar_type x = mls_x(ctx.xref());
+      scalar_type y = mls_y(ctx.xref());
+      scalar_type r2 = x*x+y*y;
+      gmm::scale(gradU_ls, log(r2) / scalar_type(2));
+
+      ctx.pf()->interpolation(ctx, coeff, u_ls, mf_u.get_qdim());
+      base_vector Runit(2);
+      Runit[0] = x / r2; Runit[1] = y / r2; 
+      gmm::rank_one_update(gradU_ls, Runit, u_ls);
+
+      // Computation of d/dalpha E
+      
+      base_matrix Ealpha(N, N);
+      gmm::mult(gmm::transposed(gradU_ls), gradU, Sigma);
+      gmm::add(Sigma, gmm::transposed(Sigma), Ealpha);
+      gmm::scale(Ealpha, 0.5);
+
+      // The term sigma:dE/dalpha
+      AHL.sigma(E, Sigma, params);
+      t[0] = mat_euclidean_sp(Sigma, Ealpha);
+
+      // The term pJF^{-T}:dgradu/dalpha
+      gmm::lu_inverse(gradU);
+      t[0] += valP[0] * J * mat_euclidean_sp(gradU_ls, gradU);
+    }
+    virtual void prepare(fem_interpolation_context& ctx, size_type nb) {
+      GMM_ASSERT1(nb != 0, "Oops");
+      if (nb == 1) {
+	size_type cv = ctx.convex_num();
+
+	valP.resize(1);
+	coeff.resize(mf_p.nb_basic_dof_of_element(cv));
+	gmm::copy(gmm::sub_vector
+	     (P_ls, gmm::sub_index(mf_p.ind_basic_dof_of_element(cv))), coeff);
+	ctx.pf()->interpolation(ctx, coeff, valP, 1);
+
+	if (cv != cv_old) {
+	  mls_x = ls.mls_of_convex(cv, 1);
+	  mls_y = ls.mls_of_convex(cv, 0);
+	  cv_old = cv;
+	}
+
+      } else if (mf_data) {
+	size_type cv = ctx.convex_num();
+	size_type nbp = AHL.nb_params();
+	coeff.resize(mf_data->nb_basic_dof_of_element(cv)*nbp);
+	for (size_type i = 0; i < mf_data->nb_basic_dof_of_element(cv); ++i)
+	  for (size_type k = 0; k < nbp; ++k)
+	    coeff[i * nbp + k]
+	      = PARAMS[mf_data->ind_basic_dof_of_element(cv)[i]*nbp+k];
+	ctx.pf()->interpolation(ctx, coeff, params, dim_type(nbp));
+      }
+    } 
+    
+  };
 
 
 
@@ -142,7 +295,7 @@ namespace getfem {
    const mesh_region &rg = mesh_region::all_convexes()) {
     VECT1 &R1 = const_cast<VECT1 &>(R1_);
     VECT1 &R2 = const_cast<VECT1 &>(R2_);
-    GMM_ASSERT1(mf.get_qdim() >= mf.linked_mesh().dim(),
+    GMM_ASSERT1(mf_u.get_qdim() >= mf_u.linked_mesh().dim(),
 		"wrong qdim for the mesh_fem");
 
 
@@ -152,13 +305,13 @@ namespace getfem {
 
     std::vector<scalar_type> P_ls(gmm::vect_size(P));
 
-    int d = mf_u.dim();
+    dim_type d = mf_u.linked_mesh().dim();
     dal::bit_vector p_enriched_dof;
     for (dal::bv_visitor cv(mf_p.linked_mesh().convex_index());
 	 !cv.finished(); ++cv) {
       pfem pf = mf_p.fem_of_element(cv);
-      for (size_type j = 0; j< pf->nb_dof(cv); ++j)
-	if (pf->dof_type()[j] == global_dof(d)) {
+      for (size_type j = 0; j < pf->nb_dof(cv); ++j)
+	if (pf->dof_types()[j] == global_dof(d)) {
 	  size_type dof = mf_p.ind_basic_dof_of_element(cv)[j];
 	  p_enriched_dof.add(dof);
 	  P_ls[dof] = P[dof];
@@ -174,8 +327,8 @@ namespace getfem {
 	 !cv.finished(); ++cv) {
       pfem pf = mf_u.fem_of_element(cv);
       for (size_type j = 0; j< pf->nb_dof(cv); ++j)
-	if (pf->dof_type()[j] == global_dof(d)) {
-	  for (k = 0; k < d; ++k) {
+	if (pf->dof_types()[j] == global_dof(d)) {
+	  for (size_type k = 0; k < d; ++k) {
 	    size_type dof = mf_u.ind_basic_dof_of_element(cv)[j*d+k];
 	    u_enriched_dof.add(dof);
 	    U_ls[dof] = U[dof];
@@ -183,128 +336,50 @@ namespace getfem {
 	}
     }
 
-  
-    elasticity_nonlinear_optim_term1<VECT2, VECT3>
-      nterm(mf_u, U, U_ls, mf_p, P, P_ls, mf_data, PARAMS, AHL, ls);
-
-    getfem::generic_assembly assem;
-    if (mf_data)
-      assem.set("t=comp(NonLin(#1,#2,#3))); V() += t(i)");
-    else
-      assem.set("t=comp(NonLin(#1,#2)); V() += t(i)");
-    // comp() to be optimized ?
-    assem.push_mi(mim);
-    assem.push_mf(mf_u);
-    assem.push_mf(mf_p);
-    if (mf_data) assem.push_mf(*mf_data);
-    assem.push_nonlinear_term(&nterm);
-    assem.push_vec(R2);
-    assem.assembly(rg);
-  }
-
-
-  struct nonlinear_elasticity_optim_brick : public virtual_brick {
-
-    const abstract_hyperelastic_law &AHL;
-    const getfem::level_set &ls;
+    {
+      
+      elasticity_nonlinear_optim_term1<VECT2, VECT3>
+	nterm(mf_u, U, U_ls, mf_p, P, P_ls, mf_data, PARAMS, AHL, ls);
+      
+      getfem::generic_assembly assem;
+      if (mf_data)
+	assem.set("t=comp(NonLin(#1,#2,#3))); V() += t(i)");
+      else
+	assem.set("t=comp(NonLin(#1,#2)); V() += t(i)");
+      // comp() to be optimized ?
+      assem.push_mi(mim);
+      assem.push_mf(mf_u);
+      assem.push_mf(mf_p);
+      if (mf_data) assem.push_mf(*mf_data);
+      assem.push_nonlinear_term(&nterm);
+      assem.push_vec(R2);
+      assem.assembly(rg);
+      
+    }
     
-    virtual void asm_real_tangent_terms(const model &md, size_type /* ib */,
-                                        const model::varnamelist &vl,
-                                        const model::varnamelist &dl,
-                                        const model::mimlist &mims,
-                                        model::real_matlist &matl,
-                                        model::real_veclist &vecl,
-                                        model::real_veclist &,
-                                        size_type region,
-                                        build_version version) const {
-      GMM_ASSERT1(mims.size() == 1,
-		  "Nonlinear elasticity brick need a single mesh_im");
-      GMM_ASSERT1(vl.size() == 4,
-		  "Nonlinear elasticity brick need a single variable");
-      GMM_ASSERT1(dl.size() == 1,
-		  "Wrong number of data for nonlinear elasticity brick, "
-                  << dl.size() << " should be 1 (vector).");
-      GMM_ASSERT1(matl.size() == 7,  "Wrong number of terms for nonlinear "
-		  "elasticity brick");
-
-      const model_real_plain_vector &u = md.real_variable(vl[0]);
-      const mesh_fem &mf_u = *(md.pmesh_fem_of_variable(vl[0]));
-
-      const model_real_plain_vector &p = md.real_variable(vl[1]);
-      const mesh_fem &mf_p = *(md.pmesh_fem_of_variable(vl[1]));
-
-
-      const model_real_plain_vector &alpha = md.real_variable(vl[2]);
-      const model_real_plain_vector &beta = md.real_variable(vl[3]);
-
-      const mesh_fem *mf_params = md.pmesh_fem_of_variable(dl[0]);
-      const model_real_plain_vector &params = md.real_variable(dl[0]);
-      const mesh_im &mim = *mims[0];
-
-      size_type sl = gmm::vect_size(params);
-      if (mf_params) sl = sl * mf_params->get_qdim() / mf_params->nb_dof();
-      GMM_ASSERT1(sl == AHL.nb_params(), "Wrong number of coefficients for the "
-		  "nonlinear constitutive elastic law");
-
-      mesh_region rg(region);
-      mf_u.linked_mesh().intersect_with_mpi_region(rg);
-
-      if (version & model::BUILD_MATRIX) {
-	gmm::clear(matl[0]);
-	GMM_TRACE2("Nonlinear elasticity stiffness matrix assembly");
-	asm_nonlinear_elasticity_tangent_matrix
-	  (matl[0], mim, mf_u, u, mf_params, params, AHL, rg);
-      }
-
-
-      if (version & model::BUILD_RHS) {
-	asm_nonlinear_elasticity_optim_rhs(vecl[0], vecl[3], mim, 
-					   mf_u, u, mf_p, p, alpha, beta,
-					   mf_params, params, AHL, ls, rg);
-	gmm::scale(vecl[0], scalar_type(-1));
-      }
+    {
+      elasticity_nonlinear_optim_term2<VECT2, VECT3>
+	nterm(mf_u, U, U_ls, mf_p, P, P_ls, mf_data, PARAMS, AHL, ls);
+      
+      getfem::generic_assembly assem;
+      if (mf_data)
+	assem.set("t=comp(NonLin(#1,#2,#3))); V() += t(i)");
+      else
+	assem.set("t=comp(NonLin(#1,#2)); V() += t(i)");
+      // comp() to be optimized ?
+      assem.push_mi(mim);
+      assem.push_mf(mf_u);
+      assem.push_mf(mf_p);
+      if (mf_data) assem.push_mf(*mf_data);
+      assem.push_nonlinear_term(&nterm);
+      assem.push_vec(R1);
+      assem.assembly(rg);
 
     }
+    
 
-
-    nonlinear_elasticity_optim_brick(const abstract_hyperelastic_law &AHL_,
-				     const getfem::level_set &ls_)
-      : AHL(AHL_), ls(ls_) {
-      set_flags("Nonlinear elasticity brick", false /* is linear*/,
-                true /* is symmetric */, true /* is coercive */,
-		true /* is real */, false /* is complex */);
-    }
-
-  };
-  
-  //=========================================================================
-  //  Add a nonlinear elasticity brick.  
-  //=========================================================================
-
-  size_type add_nonlinear_elasticity_optim_brick
-  (model &md, const mesh_im &mim, const std::string &varname_u,
-   const std::string &varname_p,  const std::string &varname_alpha,
-   const std::string &varname_beta,
-   const abstract_hyperelastic_law &AHL, const std::string &dataname_law,
-   const std::string &dataname_ls, const getfem::level_set &ls,
-   size_type region) {
-    pbrick pbr = new nonlinear_elasticity_brick(AHL, ls);
-
-    model::termlist tl;
-    tl.push_back(model::term_description(varname_alpha, varname_u, true));
-    tl.push_back(model::term_description(varname_alpha, varname_p, true));
-    tl.push_back(model::term_description(varname_alpha, varname_alpha, true));
-    tl.push_back(model::term_description(varname_beta, varname_u, true));
-    tl.push_back(model::term_description(varname_beta, varname_p, true));
-    tl.push_back(model::term_description(varname_beta, varname_alpha, true));
-    tl.push_back(model::term_description(varname_beta, varname_beta, true));
-    model::varnamelist dl(1, dataname);
-    model::varnamelist vl(1, varname_u);
-    vl.push_back(varname_p);
-    vl.push_back(varname_alpha);
-    vl.push_back(varname_beta);
-    return md.add_brick(pbr, vl, dl, tl, model::mimlist(1,&mim), region);
   }
+
 
 
 
--- contrib/xfem_large_strain/vertex_large_strain.cc
+++ contrib/xfem_large_strain/vertex_large_strain.cc
@@ -63,6 +63,132 @@ typedef getfem::modeling_standard_sparse_vector sparse_vector;
 typedef getfem::modeling_standard_sparse_matrix sparse_matrix;
 typedef getfem::modeling_standard_plain_vector  plain_vector;
 
+
+
+
+/**************************************************************************/
+/*                                                                        */
+/* Brick definition.                                                      */
+/*                                                                        */
+/**************************************************************************/
+
+namespace getfem {
+
+struct nonlinear_elasticity_optim_brick : public virtual_brick {
+  
+  const abstract_hyperelastic_law &AHL;
+  const getfem::level_set &ls;
+  
+  virtual void asm_real_tangent_terms(const model &md, size_type /* ib */,
+				      const model::varnamelist &vl,
+				      const model::varnamelist &dl,
+                                        const model::mimlist &mims,
+                                        model::real_matlist &matl,
+                                        model::real_veclist &vecl,
+                                        model::real_veclist &,
+                                        size_type region,
+                                        build_version version) const {
+      GMM_ASSERT1(mims.size() == 1,
+		  "Nonlinear elasticity brick need a single mesh_im");
+      GMM_ASSERT1(vl.size() == 4,
+		  "Nonlinear elasticity brick need a single variable");
+      GMM_ASSERT1(dl.size() == 1,
+		  "Wrong number of data for nonlinear elasticity brick, "
+                  << dl.size() << " should be 1 (vector).");
+      GMM_ASSERT1(matl.size() == 7,  "Wrong number of terms for nonlinear "
+		  "elasticity brick");
+
+      const model_real_plain_vector &u = md.real_variable(vl[0]);
+      const mesh_fem &mf_u = *(md.pmesh_fem_of_variable(vl[0]));
+
+      const model_real_plain_vector &p = md.real_variable(vl[1]);
+      const mesh_fem &mf_p = *(md.pmesh_fem_of_variable(vl[1]));
+
+
+      const model_real_plain_vector &alpha = md.real_variable(vl[2]);
+      const model_real_plain_vector &beta = md.real_variable(vl[3]);
+
+      const mesh_fem *mf_params = md.pmesh_fem_of_variable(dl[0]);
+      const model_real_plain_vector &params = md.real_variable(dl[0]);
+      const mesh_im &mim = *mims[0];
+
+      size_type sl = gmm::vect_size(params);
+      if (mf_params) sl = sl * mf_params->get_qdim() / mf_params->nb_dof();
+      GMM_ASSERT1(sl == AHL.nb_params(), "Wrong number of coefficients for the "
+		  "nonlinear constitutive elastic law");
+
+      mesh_region rg(region);
+      mf_u.linked_mesh().intersect_with_mpi_region(rg);
+
+      if (version & model::BUILD_MATRIX) {
+	gmm::clear(matl[0]);
+	GMM_TRACE2("Nonlinear elasticity stiffness matrix assembly");
+	asm_nonlinear_elasticity_tangent_matrix
+	  (matl[0], mim, mf_u, u, mf_params, params, AHL, rg);
+      }
+
+
+      if (version & model::BUILD_RHS) {
+	asm_nonlinear_elasticity_optim_rhs(vecl[0], vecl[3], mim, 
+					   mf_u, u, mf_p, p, alpha, beta,
+					   mf_params, params, AHL, ls, rg);
+	gmm::scale(vecl[0], scalar_type(-1));
+      }
+
+    }
+
+
+    nonlinear_elasticity_optim_brick(const abstract_hyperelastic_law &AHL_,
+				     const getfem::level_set &ls_)
+      : AHL(AHL_), ls(ls_) {
+      set_flags("Nonlinear elasticity brick", false /* is linear*/,
+                true /* is symmetric */, true /* is coercive */,
+		true /* is real */, false /* is complex */);
+    }
+
+  };
+  
+  //=========================================================================
+  //  Add a nonlinear elasticity brick.  
+  //=========================================================================
+
+  size_type add_nonlinear_elasticity_optim_brick
+  (model &md, const mesh_im &mim, const std::string &varname_u,
+   const std::string &varname_p,  const std::string &varname_alpha,
+   const std::string &varname_beta,
+   const abstract_hyperelastic_law &AHL, const std::string &dataname_law,
+   const getfem::level_set &ls, size_type region = size_type(-1)) {
+    pbrick pbr = new nonlinear_elasticity_optim_brick(AHL, ls);
+
+    model::termlist tl;
+    tl.push_back(model::term_description(varname_alpha, varname_u, true));
+    tl.push_back(model::term_description(varname_alpha, varname_p, true));
+    tl.push_back(model::term_description(varname_alpha, varname_alpha, true));
+    tl.push_back(model::term_description(varname_beta, varname_u, true));
+    tl.push_back(model::term_description(varname_beta, varname_p, true));
+    tl.push_back(model::term_description(varname_beta, varname_alpha, true));
+    tl.push_back(model::term_description(varname_beta, varname_beta, true));
+    model::varnamelist dl(1, dataname_law);
+    model::varnamelist vl(1, varname_u);
+    vl.push_back(varname_p);
+    vl.push_back(varname_alpha);
+    vl.push_back(varname_beta);
+    return md.add_brick(pbr, vl, dl, tl, model::mimlist(1,&mim), region);
+  }
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
 /**************************************************************************/
 /*                                                                        */
 /*  Structure for the crack problem.                                      */
@@ -778,12 +904,13 @@ bool crack_problem::solve(plain_vector &U, plain_vector &P) {
   getfem::model model;
   size_type alpha,beta;
   model.add_fem_variable("u", mf_u());
-  model.add_fixed_size_variable("alpha", alpha);
-  model.add_fixed_size_variable("beta", beta);
+  //model.add_fixed_size_variable("alpha", alpha);
+  //model.add_fixed_size_variable("beta", beta);
   
   model.add_initialized_fixed_size_data("Mooney_Riv_coefficient", pr);
   
-  getfem::add_nonlinear_elasticity_brick( model, mim, "u", getfem::Mooney_Rivlin_hyperelastic_law(),"Mooney_Riv_coefficient");
+  getfem::add_nonlinear_elasticity_brick(model, mim, "u",
+					 getfem::Mooney_Rivlin_hyperelastic_law(), "Mooney_Riv_coefficient");
   // Incompressibility
   if (mixed_pressure) {
     model.add_fem_variable("p", mf_pe());
@@ -791,7 +918,8 @@ bool crack_problem::solve(plain_vector &U, plain_vector &P) {
     }
   // Add nonlinear elasticity optimazition brick
 
-  add_nonlinear_elasticity_optim_brick(model, mim, "u" , "p","alpha","beta",getfem::Mooney_Rivlin_hyperelastic_law(),"Mooney_Riv_coefficient","ls_crack",ls);
+  add_nonlinear_elasticity_optim_brick(model, mim, "u" , "p", "alpha", "beta",
+	   getfem::Mooney_Rivlin_hyperelastic_law(), "Mooney_Riv_coefficient", ls);
 
  //  getfem::mdbrick_nonlinear_elasticity<>  ELAS(*pl, mim, mf_u(), pr);
  //   getfem::mdbrick_nonlinear_incomp<> INCOMP(ELAS, mf_pe());
