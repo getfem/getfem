1c4403b5564195c87fc22993be13cc81442d3f7c
Farshid Dabaghi <farshid.dabaghi@insa-lyon.fr>
2012-02-17 09:36:44 +0000

    work in progress

--- interface/tests/matlab/demo_dynamic_contact.m
+++ interface/tests/matlab/demo_dynamic_contact.m
@@ -151,75 +151,46 @@ gf_model_set(md, 'add continuous contact with rigid obstacle brick', ...
       mim, 'u', 'lambda_n', 'obstacle', 'r', GAMMAC, 1);
 
 nbdofl = size(ldof,1);
-U0 = zeros(nbdofu, 1);
-V0 = zeros(nbdofu, 1);
+U0 = (gf_mesh_fem_get(mfu, 'eval', {'0.5-0.5*x'}))'
+
+gf_plot(mfu, U0');
+pause;
+
+MV0 = zeros(nbdofu, 1);
 LAMBDA0 = zeros(nbdofl, 1);
 FF = gf_asm('volumic source', mim, mfu, mfd, F');
 K = gf_asm('linear elasticity', mim, mfu, mfd, ones(nbdofd,1)*clambda, ones(nbdofd,1)*cmu);
+LL0 = gf_asm('boundary', GAMMAC, 'l=data(#2);V(#1)+=comp(vBase(#1).Base(#2).Normal())(:,i,k,i).l(k)', mim, mfu, mflambda_partial, LAMBDA0);
 
-
-for t = 0:dt:1
+nit = 0;
+for t = 0:dt:100
   
-  LL = gf_asm('boundary', GAMMAC, 'l=data(#2);V(#1)+=comp(vBase(#1).Base(#2).Normal())(:,i,k,i).l(k)', mim, mfu, mflambda_partial, LAMBDA0);
-
   % calcul de B
-  B = M*U0 + dt*M*V0 + dt*dt*(1-beta)*(-K*U0+FF+LL);
+  B = M*U0 + dt*MV0 + dt*dt*(1-beta)*(-K*U0+FF+LL0);
   
   gf_model_set(md, 'set private rhs', ind_rhs, B)
   gf_model_get(md, 'solve', 'max_res', 1E-9, 'very noisy', 'max_iter', niter);
 
-  U1 = gf_model_get(md, 'variable', 'u');
-  LAMBDA1 = gf_model_get(md, 'variable', 'u');
-  lambda_n = gf_model_get(md, 'variable', 'lambda_n');
+  U1 = (gf_model_get(md, 'variable', 'u'))';
+  LAMBDA1 = (gf_model_get(md, 'variable', 'lambda_n'))';
+  LL1 = gf_asm('boundary', GAMMAC, 'l=data(#2);V(#1)+=comp(vBase(#1).Base(#2).Normal())(:,i,k,i).l(k)', mim, mfu, mflambda_partial, LAMBDA1);
 
-  % TODO : computation of the velocity V1 ...
-
+  
+  MV1 = MV0 + dt*(gamma*(FF-LL1-K*U1) + (1-gamma)*(FF-LL0-K*U0));
+    
+  
+  ++nit;
+  if (mod(nit, 10) == 0)
+    gf_plot(mfu, U1');
+    pause(0.1);
+  end;
+  
+  
   U0 = U1;
   LAMBDA0 = LAMBDA1;
-  V0 = V1;
+  MV0 = MV1;
+  LL0 = LL1;
 end
     
-if (d > 1)
-  VM = gf_model_get(md, 'compute_isotropic_linearized_Von_Mises_or_Tresca', ...
- 	            'u', 'clambda', 'cmu', mfvm);
-end
 
 
-% set a custom colormap
-% r=[0.7 .7 .7]; l = r(end,:); s=63; s1=20; s2=25; s3=48;s4=55; for i=1:s, c1 = max(min((i-s1)/(s2-s1),1),0);c2 = max(min((i-s3)/(s4-s3),1),0); r(end+1,:)=(1-c2)*((1-c1)*l + c1*[1 0 0]) + c2*[1 .8 .2]; end; colormap(r);
-
-figure(2);
-if (d == 3)
-  c=[0.1;0;20]; x=[1;0;0]; y=[0;1;0]; z=[0;0;1];
-  % Whole boundary
-  % sl2=gf_slice({'boundary',{'none'}}, m, 5);
-  % Slice, 3 planes
-  % sl2=gf_slice({'boundary',{'union',{'planar',+1,c,x},{'planar',+1,c,y},{'planar',+1,c,z}}},m,5);
-  % Slice, 2 planes
-  % sl2=gf_slice({'boundary',{'union',{'planar',+1,c,x},{'planar',+1,c,y}}},m,5);
-  % Slice, 1 plane
-  sl2=gf_slice({'boundary',{'planar',+1,c,x}}, m, 5);
-
-  P=gf_slice_get(sl2,'pts'); dP=gf_compute(mfu,U,'interpolate on',sl2);
-  gf_slice_set(sl2, 'pts', P+dP);
-  VMsl=gf_compute(mfvm,VM,'interpolate on',sl2);
-  set(gcf,'renderer','zbuffer');
-  h=gf_plot_slice(sl2,'mesh','on','mesh_slice_edges','off','data',VMsl);
-  view(-80,-15); axis on; camlight; gf_colormap('chouette');
-  % map=[1:-1/10:0]'*[1 1 1]; colormap(map); % for NB
-    
-  % gf_plot(mfvm, VM, 'mesh', 'off', 'cvlst', ...
-  %        gf_mesh_get(mfu,'outer faces'), 'deformation', U, ...
-  %        'deformation_mf', mfu, 'deformation_scale', 1, 'refine', 8);
-  % view(-5,-10); camlight; colormap(map);
-  xlabel('x'); ylabel('y'); zlabel('z');
-  title('Sliced deformed configuration (not really a small deformation of course ...)');
-elseif (d == 2)
-  gf_plot(mfvm, VM, 'deformed_mesh', 'on', 'deformation', U, ...
-          'deformation_mf', mfu, 'deformation_scale', 1, 'refine', 8);
-  xlabel('x'); ylabel('y');
-  title('Deformed configuration (not really a small deformation of course ...)');
-end;
-
-colorbar;
-pause(0.1);
