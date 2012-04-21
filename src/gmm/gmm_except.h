4dc1681624cebcfe2cf90188fb90511c5e70a2ad
Konstantinos Poulios <logari81@gmail.com>
2012-04-21 18:32:30 +0000

    Fix issue with gcc-4.7 (from Philipp Büttgenbach)

--- src/gmm/gmm_except.h
+++ src/gmm/gmm_except.h
@@ -76,7 +76,7 @@ namespace gmm {
   }
 # define GMM_THROW_(type, errormsg) {					\
     std::stringstream msg;						\
-    msg << "Error in "__FILE__ << ", line "				\
+    msg << "Error in " << __FILE__ << ", line "				\
 	<< __LINE__ << " " << GMM_PRETTY_FUNCTION << ": \n"		\
 	<< errormsg << ends;						\
     throw (type)(msg.str());						\
@@ -100,7 +100,7 @@ namespace gmm {
 
 # define GMM_THROW_(type, errormsg) {					\
     std::stringstream msg;						\
-    msg << "Error in "__FILE__ << ", line "				\
+    msg << "Error in " << __FILE__ << ", line "				\
 	<< __LINE__ << " " << GMM_PRETTY_FUNCTION << ": \n"		\
 	<< errormsg;						\
     std::cerr << msg.str() << std::endl;                                \
@@ -158,7 +158,7 @@ namespace gmm {
 
 #define GMM_WARNING_MSG(level_, thestr)  {			       \
       std::stringstream msg;                                           \
-      msg << "Level " << level_ << " Warning in "__FILE__ << ", line " \
+      msg << "Level " << level_ << " Warning in " << __FILE__ << ", line " \
           << __LINE__ << ": " << thestr;		       \
        std::cerr << msg.str() << std::endl;                            \
     }
@@ -220,7 +220,7 @@ namespace gmm {
 #define GMM_TRACE_MSG(level_, thestr)  {			       \
     GMM_TRACE_MSG_MPI {						       \
       std::stringstream msg;                                           \
-      msg << "Trace " << level_ << " in "__FILE__ << ", line "         \
+      msg << "Trace " << level_ << " in " << __FILE__ << ", line "     \
           << __LINE__ << ": " << thestr;        		       \
       std::cout << msg.str() << std::endl;			       \
     }                                                                  \
