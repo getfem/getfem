1f42c8441ff9a4a6732add1ec3e39ee6c3ab0f71
Lim Liang Jin <lj@plaxis.nl>
2013-10-04 10:34:57 +0000

    moved model_pb struct definition to the header file.

--- src/getfem/getfem_model_solvers.h
+++ src/getfem/getfem_model_solvers.h
@@ -810,7 +810,7 @@ namespace getfem {
   }
 
   /* ***************************************************************** */
-  /*     Intermediary structure for Newton algorithms.                 */
+  /*   Intermediary structure for Newton algorithms with MODEL_STATE.  */
   /* ***************************************************************** */
 
   template <typename MODEL_STATE>
@@ -885,6 +885,254 @@ namespace getfem {
     model_problem(MODEL_STATE &MS_, mdbrick_abstract<MODEL_STATE> &pb_,
                   abstract_newton_line_search &ls_)
       : MS(MS_), pb(pb_), ls(ls_) {}
+
+  };
+
+  
+  /* ***************************************************************** */
+  /*  Intermediary structure for Newton algorithms with getfem::model. */
+  /* ***************************************************************** */
+
+  #define TRACE_SOL 0
+
+  template <typename MAT, typename VEC> 
+  struct model_pb {
+
+    typedef MAT MATRIX;
+    typedef VEC VECTOR;
+    typedef typename gmm::linalg_traits<VECTOR>::value_type T;
+    typedef typename gmm::number_traits<T>::magnitude_type R;
+
+    model &md;
+    bool is_reduced;
+    std::vector<size_type> &sind;
+    gmm::sub_index I;
+    abstract_newton_line_search &ls;
+    VECTOR stateinit, &state;
+    const VECTOR &rhs;
+    const MATRIX &K;
+    MATRIX Kr;
+    VECTOR rhsr;
+    bool with_pseudo_potential;
+
+    void compute_tangent_matrix(void) {
+      md.to_variables(state);
+      md.assembly(model::BUILD_MATRIX);
+      if (is_reduced) {
+	gmm::resize(Kr, sind.size(), sind.size());
+	gmm::copy(gmm::sub_matrix(K, I, I), Kr);
+      }
+    }
+
+    const MATRIX &tangent_matrix(void) { return (is_reduced ? Kr : K); }
+    
+    inline T scale_residual(void) const { return T(1); }
+
+    void compute_residual(void) {
+      md.to_variables(state);
+      md.assembly(model::BUILD_RHS);
+      if (is_reduced) {
+	gmm::resize(rhsr, sind.size());
+	gmm::copy(gmm::sub_vector(rhs, I), rhsr);
+      }
+    }
+
+    void compute_pseudo_potential(void)
+    { md.to_variables(state); md.assembly(model::BUILD_PSEUDO_POTENTIAL); }
+
+    void perturbation(void) {
+      R res = gmm::vect_norm2(state), ampl = std::max(res*R(1E-20), R(1E-50));
+      std::vector<R> V(gmm::vect_size(state));
+      gmm::fill_random(V);
+      gmm::add(gmm::scaled(V, ampl), state);
+    }
+
+    const VECTOR &residual(void) { return (is_reduced ? rhsr : rhs); }
+
+    R residual_norm(void) { // A norm1 seems to be better than a norm2
+                            // at least for contact problems.
+      if (is_reduced)
+	return gmm::vect_norm1(rhsr)/R(gmm::vect_size(rhsr));
+      else
+	return gmm::vect_norm1(rhs)/R(gmm::vect_size(rhs));
+    }
+
+    R compute_res(bool comp = true) {
+      if (with_pseudo_potential) {
+	compute_pseudo_potential();
+	return md.pseudo_potential();
+      } else {
+	if (comp) compute_residual();
+	return residual_norm();
+      }
+    }
+
+
+    R line_search(VECTOR &dr, const gmm::iteration &iter) {
+      size_type nit = 0;
+      gmm::resize(stateinit, md.nb_dof());
+      gmm::copy(state, stateinit);
+      R alpha(1), res, /* res_init, */ R0;
+	  
+      /* res_init = */ res = compute_res(false);      
+      // cout << "first residual = " << residual() << endl << endl;
+      R0 = (is_reduced ? gmm::real(gmm::vect_sp(dr, rhsr))
+	               : gmm::real(gmm::vect_sp(dr, rhs)));
+
+      // store the initial residual for the reprojection step.
+//       size_type N = gmm::vect_size(residual());
+//       VECTOR first_rhs(N); gmm::copy(residual(), first_rhs); // à éliminer
+
+
+      // Compute the second derivative at alpha = 0 (value 2*a)
+      // not very effective ... precision problem ?
+      
+//       R EPS = 1e-6;
+//       gmm::add(gmm::sub_vector(stateinit, I), gmm::scaled(dr, EPS),
+//                                               gmm::sub_vector(state, I));
+//       R res2 = compute_res();
+      
+//       R a = (res2 + EPS * res - res) / gmm::sqr(EPS);
+//       R delta = gmm::sqr(res)-R(4)*a*res;
+//       R alpha_bis = R(1);
+//       if (delta > 0) {
+// 	R l1 = (res - sqrt(delta)) / (R(2)*a);
+// 	R l2 = (res - sqrt(delta)) / (R(2)*a);
+// 	if (l1 > 0) alpha_bis = std::min(alpha_bis, l1);
+// 	if (l2 > 0) alpha_bis = std::min(alpha_bis, l2);
+//       }
+//       if (a > 0)
+// 	alpha_bis = std::min(alpha_bis, res / (R(2)*a));
+//       cout << "alpha_bis = " << alpha_bis << endl;
+      
+//       gmm::add(gmm::sub_vector(stateinit, I), gmm::scaled(dr, alpha_bis),
+//                                               gmm::sub_vector(state, I));
+
+//       R res3 = compute_res();
+//       cout << "res3 = " << res3 << endl;
+
+//       cout.precision(16);
+//       cout << "res = " << res << " res2 = " << res2 << endl;
+//       cout << "a = " << (res2 + EPS * res - res) / gmm::sqr(EPS) << endl;
+//       cout << "aa = " << (res2 + EPS * res - res) << endl;
+      
+#if TRACE_SOL  
+      static int trace_number = 0;
+      int trace_iter = 0;
+      {
+	std::stringstream trace_name;
+	trace_name << "line_search_state" << std::setfill('0')
+		   << std::setw(3) << trace_number << "_000_init";
+	gmm::vecsave(trace_name.str(),stateinit);
+      }
+      trace_number++;
+#endif 
+
+      ls.init_search(res, iter.get_iteration(), R0);
+      do {
+	alpha = ls.next_try();
+	gmm::add(gmm::sub_vector(stateinit, I), gmm::scaled(dr, alpha),
+		 gmm::sub_vector(state, I));
+#if TRACE_SOL
+	{
+	  trace_iter++;
+	  std::stringstream trace_name;
+	  trace_name  << "line_search_state" << std::setfill('0')
+		      << std::setw(3) << trace_number << "_"
+		      << std::setfill('0') << std::setw(3) << trace_iter;
+	  gmm::vecsave(trace_name.str(), state);
+	}
+#endif 
+	res = compute_res();
+	// cout << "residual = " << residual() << endl << endl;
+	R0 = (is_reduced ? gmm::real(gmm::vect_sp(dr, rhsr))
+	                 : gmm::real(gmm::vect_sp(dr, rhs)));
+
+	++ nit;
+      } while (!ls.is_converged(res, R0));
+
+      if (alpha != ls.converged_value() || with_pseudo_potential) {
+	alpha = ls.converged_value();
+	gmm::add(gmm::sub_vector(stateinit, I), gmm::scaled(dr, alpha),
+		 gmm::sub_vector(state, I));
+	res = ls.converged_residual();
+	compute_residual();
+      }
+
+//     if (1) { // reprojection step ... only for non reduced case ...
+//     GMM_ASSERT1(!is_reduced, "to be done ...");
+// 	// detection des ddls "coupables"
+// 	R mean = R(0);
+// 	for (size_type i = 0; i < N; ++i) mean += gmm::abs(residual()[i]);
+// 	mean /= R(N);
+// 	cout << "mean = " << mean << endl << "selected : ";
+	
+// 	VECTOR new_dr(N);
+// 	size_type Ndof = 0;
+// 	for (size_type i = 0; i < N; ++i)
+// 	  if (gmm::abs(residual()[i])
+// 	      > std::max(gmm::abs(first_rhs[i]), mean) * R(4)) {
+// 	    cout << i << " ";
+// 	    new_dr[i] = dr[i];
+// 	    ++Ndof;
+// 	  }
+// 	cout << endl;
+	
+// 	if (Ndof > 0) {
+
+// 	  R EPS = 1e-8;
+// 	  R THR = 0.8;
+// 	  R mini_alpha = alpha * THR;
+
+// 	  cout << "performing post-correction" << endl;
+// 	  cout << "res = " << res << " res_init = " << res_init << endl;
+	  
+
+// 	  // divided difference
+// 	  gmm::add(gmm::scaled(new_dr, -EPS), state);
+// 	  R res2 = compute_res();
+// 	  cout << "pente = " << (res - res2) / EPS << endl;
+// 	  R beta = (res - res2) / EPS;
+// 	  mini_alpha = (res2 - THR*std::min(res_init, res))/((beta != R(0)) ? beta : R(1));
+
+// 	  cout << "mini_alpha = " << mini_alpha << endl;
+// 	  if (mini_alpha > alpha * THR)
+// 	    mini_alpha = alpha * THR;
+
+// 	  if (mini_alpha > 0) {
+	  
+// 	    gmm::add(gmm::scaled(new_dr, -mini_alpha+EPS), state);
+// 	    R res4 = compute_res(), res3;
+// 	    cout << "res4 = " << res4 << endl;
+// 	    size_type mini_it = 0;
+// 	    do {
+// 	      res3 = res4;
+// 	      mini_alpha /= R(2); mini_it++;
+// 	      gmm::add(gmm::scaled(new_dr, mini_alpha), state);
+// 	      res4 = compute_res();
+// 	      cout << "res4 = " << res4 << endl;
+// 	    } while (res4 < res3 && mini_it <= 6);
+	    
+// 	    if (res4 < res3) { res3 = res4; }
+// 	    else gmm::add(gmm::scaled(new_dr, -mini_alpha), state);
+	    
+// 	    if (res3 > res) { // cancel correction
+// 	      gmm::add(stateinit, gmm::scaled(dr, alpha), state);
+// 	    }
+// 	  }
+// 	  else gmm::add(stateinit, gmm::scaled(dr, alpha), state);
+// 	  res = compute_res();
+// 	}
+//       }
+      return alpha;
+    }
+
+    model_pb(model &m, abstract_newton_line_search &ls_, VECTOR &st,
+	     const VECTOR &rhs_, const MATRIX &K_, bool reduced_,
+	     std::vector<size_type> &sind_,
+	     bool with_pseudo_pot = false)
+      : md(m), is_reduced(reduced_), sind(sind_), I(sind_), ls(ls_), state(st),
+	rhs(rhs_), K(K_), with_pseudo_potential(with_pseudo_pot) {}
 
   };
 
