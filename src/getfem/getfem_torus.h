194fb8a259c4c7e46129cff05b7b7391d8c00d87
Lim Liang Jin <lj@plaxis.nl>
2014-05-14 08:25:23 +0000

    added torus implementation, work in progress

---
+++ src/getfem/getfem_torus.h
@@ -0,0 +1,126 @@
+/* -*- c++ -*- (enables emacs c++ mode) */
+/*===========================================================================
+ 
+ Copyright (C) 2012-2012 Liang Jin Lim
+ 
+ This file is a part of GETFEM++
+ 
+ Getfem++  is  free software;  you  can  redistribute  it  and/or modify it
+ under  the  terms  of the  GNU  Lesser General Public License as published
+ by  the  Free Software Foundation;  either version 3 of the License,  or
+ (at your option) any later version along with the GCC Runtime Library
+ Exception either version 3.1 or (at your option) any later version.
+ This program  is  distributed  in  the  hope  that it will be useful,  but
+ WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ or  FITNESS  FOR  A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+ License and GCC Runtime Library Exception for more details.
+ You  should  have received a copy of the GNU Lesser General Public License
+ along  with  this program;  if not, write to the Free Software Foundation,
+ Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301, USA.
+ 
+ As a special exception, you  may use  this file  as it is a part of a free
+ software  library  without  restriction.  Specifically,  if   other  files
+ instantiate  templates  or  use macros or inline functions from this file,
+ or  you compile this  file  and  link  it  with other files  to produce an
+ executable, this file  does  not  by itself cause the resulting executable
+ to be covered  by the GNU Lesser General Public License.  This   exception
+ does not  however  invalidate  any  other  reasons why the executable file
+ might be covered by the GNU Lesser General Public License.
+ 
+===========================================================================*/
+/**
+@file getfem_torus.h
+@brief Provides mesh and mesh fem of torus.
+@date May 2014
+@author Liang Jin Lim
+*/
+
+#pragma once
+
+#ifndef GETFEM_TORUS_H__
+#define GETFEM_TORUS_H__
+
+#include "getfem/getfem_mesh_fem.h"
+
+
+namespace bgeot{
+
+/**An adaptor that adapts a two dimensional geometric_trans to include radial dimension.*/
+struct torus_geom_trans : public geometric_trans{
+
+  virtual void poly_vector_val(const base_node &, bgeot::base_vector &) const;
+  virtual void poly_vector_val(const base_node &, const bgeot::convex_ind_ct &,
+    bgeot::base_vector &) const;
+  virtual void poly_vector_grad(const base_node &, bgeot::base_matrix &) const;
+  inline virtual void poly_vector_grad(const base_node &,
+    const bgeot::convex_ind_ct &, bgeot::base_matrix &) const;
+  inline virtual void compute_K_matrix
+    (const bgeot::base_matrix &, const bgeot::base_matrix &, bgeot::base_matrix &) const;
+
+  virtual void poly_vector_hess(const base_node &, bgeot::base_matrix &) const;
+
+  torus_geom_trans(bgeot::pgeometric_trans poriginal_trans);
+
+private:
+  pgeometric_trans poriginal_trans_;
+};
+
+pgeometric_trans torus_geom_trans_descriptor(pgeometric_trans poriginal_trans);
+
+}
+
+namespace getfem
+{
+  /**Torus fem, the real grad base value is modified to compute radial grad of F/R.
+     It stores a reference to the original fem object.
+  */
+  class torus_fem : public virtual_fem{
+
+  public :
+    virtual size_type index_of_global_dof(size_type cv, size_type i) const;
+    void base_value(const base_node &, base_tensor &) const;
+    void grad_base_value(const base_node &, base_tensor &) const;
+    void hess_base_value(const base_node &, base_tensor &) const;
+    void real_base_value(const fem_interpolation_context& c,
+      base_tensor &t, bool = true) const;
+    void real_grad_base_value(const fem_interpolation_context& c,
+      base_tensor &t, bool = true) const;
+    void real_hess_base_value(const fem_interpolation_context&,
+      base_tensor &, bool = true) const;
+
+    torus_fem(pfem pfem) : virtual_fem(*pfem), poriginal_fem_(pfem)
+    {
+      init();
+    }
+
+  protected :
+    void init();
+
+  private :
+    pfem poriginal_fem_;
+  };
+  
+  /**Copy an original 2D mesh to become a torus mesh with radial dimension.*/
+  class torus_mesh : public mesh
+  {
+  private:
+    bool is_adapted_;
+    
+  public:    
+    void adapt();
+    void adapt(const getfem::mesh &original_mesh);
+  };
+
+  /**Mesh fem object that adapts */
+  class torus_mesh_fem : public mesh_fem{
+  public:
+
+    torus_mesh_fem(torus_mesh &mesh, bgeot::dim_type dim) : mesh_fem(mesh, dim){}
+    void adapt_to_torus();
+
+  private:
+    void del_torus_fem_();
+  };
+}
+
+#endif /* GETFEM_TORUS_H__  */
---
+++ src/getfem_torus.cc
@@ -0,0 +1,331 @@
+#include "getfem/getfem_torus.h"
+
+namespace bgeot{
+
+  /**torus_structure which extends a 2 dimensional structure with a radial dimension*/
+  class torus_structure : public convex_structure{
+
+    friend pconvex_structure torus_structure_descriptor(pconvex_structure);
+  };
+
+  class torus_reference : public convex_of_reference{
+
+  public :
+    scalar_type is_in(const base_node& point) const{
+      GMM_ASSERT1(point.size() >= 2, "Invalid dimension of pt " << point);
+      base_node point_2d = point;
+      point_2d.resize(2);
+      return ori_ref_convex_->is_in(point_2d);
+    }
+
+    scalar_type is_in_face(bgeot::short_type f, const base_node& point) const{
+      GMM_ASSERT1(point.size() >= 2, "Invalid dimension of pt " << point);
+      base_node point2D = point;
+      point2D.resize(2);
+      return ori_ref_convex_->is_in_face(f, point2D);
+    }  
+    torus_reference(bgeot::pconvex_ref originalConvexRef){
+      cvs = torus_structure_descriptor(originalConvexRef->structure());
+      convex<base_node>::points().resize(cvs->nb_points());
+      normals_.resize(6);
+
+      const std::vector<base_small_vector> &ori_normals = originalConvexRef->normals();
+      const stored_point_tab &ori_points = originalConvexRef->points();
+      std::copy(ori_normals.begin(), ori_normals.end(), normals_.begin());
+      std::copy(ori_points.begin(), ori_points.end(), convex<base_node>::points().begin());
+      for(size_type pt = 0; pt < convex<base_node>::points().size(); ++pt){
+        convex<base_node>::points()[pt].resize(3);
+      }
+      ppoints = store_point_tab(convex<base_node>::points());
+    }
+
+  private:
+    bgeot::pconvex_ref ori_ref_convex_;
+  };
+
+  DAL_SIMPLE_KEY(torus_structure_key, pconvex_structure);
+
+  pconvex_structure torus_structure_descriptor(pconvex_structure ori_structure){
+
+    dal::pstatic_stored_object o = dal::search_stored_object(torus_structure_key(ori_structure));
+    if (o) return dal::stored_cast<convex_structure>(o);
+
+    torus_structure *p = new torus_structure;
+    p->Nc = ori_structure->dim() + 1;
+    p->nbpt = ori_structure->nb_points();
+    p->nbf = ori_structure->nb_faces();
+
+    p->faces_struct.resize(p->nbf);
+    p->faces.resize(p->nbf);
+
+    for (size_type j = 0; j < p->nbf; ++j){
+      p->faces_struct[j] = ori_structure->faces_structure()[j];
+      short_type nbIndex = ori_structure->nb_points_of_face(j);
+      p->faces[j].resize(nbIndex);
+      p->faces[j] = ori_structure->ind_points_of_face(j);
+    }
+
+    p->dir_points_.resize(ori_structure->ind_dir_points().size());
+    p->dir_points_ = ori_structure->ind_dir_points();
+
+    p->basic_pcvs = ori_structure->basic_structure().get();
+
+    torus_structure_key *key = new torus_structure_key(ori_structure);
+    dal::add_stored_object(key, p, dal::PERMANENT_STATIC_OBJECT);
+    return p;
+  }
+
+  DAL_SIMPLE_KEY(torus_reference_key, pconvex_ref);
+
+  pconvex_ref ptorus_reference(pconvex_ref ori_convex_reference)
+  {
+    dal::pstatic_stored_object o 
+      = dal::search_stored_object(torus_reference_key(ori_convex_reference));
+
+    if (o) return dal::stored_cast<bgeot::convex_of_reference>(o);
+    pconvex_ref p = new torus_reference(ori_convex_reference);
+    dal::add_stored_object(new torus_reference_key(ori_convex_reference), p,
+      p->structure(), &(p->points()), dal::PERMANENT_STATIC_OBJECT);
+    return p;
+  }
+
+  void torus_geom_trans::poly_vector_val(const base_node &pt, bgeot::base_vector &val) const{
+    base_node pt_2d(pt);
+    pt_2d.resize(2);
+    poriginal_trans_->poly_vector_val(pt_2d, val);
+  }
+
+  void torus_geom_trans::poly_vector_val(const base_node &pt, const bgeot::convex_ind_ct &ind_ct,
+    bgeot::base_vector &val) const{
+      base_node pt_2d(pt);
+      pt_2d.resize(2);
+      poriginal_trans_->poly_vector_val(pt_2d, ind_ct, val);     
+  }
+
+  void torus_geom_trans::poly_vector_grad(const base_node &pt, bgeot::base_matrix &pc) const{
+    base_node pt2D(pt);
+    pt2D.resize(2);
+    std::cout << pt << std::endl;
+    bgeot::base_matrix pc2D(nb_points(), 2);
+    poriginal_trans_->poly_vector_grad(pt2D, pc2D);
+
+    bgeot::base_vector base_value;
+    poriginal_trans_->poly_vector_val(pt2D, base_value);
+
+    pc.resize(nb_points(), 3);
+    for (size_type i = 0; i < nb_points(); ++i)
+    {
+      for (bgeot::dim_type n = 0; n < 2; ++n)
+      {
+        pc(i, n) = pc2D(i, n);
+      }
+      pc(i, 2) = base_value[i]; // radial direction, pc = base_x;
+    }
+  }
+
+  void torus_geom_trans::poly_vector_grad(const base_node &pt,
+    const bgeot::convex_ind_ct &ind_ct, bgeot::base_matrix &pc) const{
+      base_node pt2D(pt);
+      pt2D.resize(2);
+      bgeot::base_matrix pc2D(ind_ct.size(), 2);
+      poriginal_trans_->poly_vector_grad(pt2D, pc2D);
+      pc.resize(ind_ct.size(), dim());
+      for (size_type i = 0; i < ind_ct.size(); ++i)
+      {
+        for (bgeot::dim_type n = 0; n < 2; ++n)
+        {
+          pc(i, n) = pc2D(i, n);
+        }
+      }
+  }
+
+  void torus_geom_trans::compute_K_matrix
+    (const bgeot::base_matrix &G, const bgeot::base_matrix &pc, bgeot::base_matrix &K) const{
+      bgeot::geometric_trans::compute_K_matrix(G, pc, K);
+      K(2, 2) = 0.0;
+      for(int j = 0; j < nb_points(); ++ j) K(2, 2) += G(0, j) * pc(j, 2);
+      for(int i = 0; i < 2; ++i) K(2, i) = K(i, 2) = 0;
+  }
+
+  void torus_geom_trans::poly_vector_hess(const base_node &pt, bgeot::base_matrix &pc) const{
+    GMM_ASSERT1(false, "Sorry, Hessian is not supported in sxisymmetric transformation.");
+  }
+
+  torus_geom_trans::torus_geom_trans(pgeometric_trans poriginal_trans) 
+    : poriginal_trans_(poriginal_trans){
+      geometric_trans::is_lin = poriginal_trans_->is_linear();
+      geometric_trans::cvr = ptorus_reference(poriginal_trans_->convex_ref());
+      complexity_ = poriginal_trans_->complexity();
+      fill_standard_vertices();
+  }
+
+  DAL_SIMPLE_KEY(torus_geom_trans_key, pgeometric_trans);
+
+  pgeometric_trans torus_geom_trans_descriptor(pgeometric_trans poriginal_trans){
+    dal::pstatic_stored_object o 
+      = dal::search_stored_object(torus_geom_trans_key(poriginal_trans));
+
+    if (o) return dal::stored_cast<torus_geom_trans>(o);
+
+    bgeot::pgeometric_trans p = new torus_geom_trans(poriginal_trans);
+    dal::add_stored_object(new torus_geom_trans_key(poriginal_trans), 
+      p, dal::PERMANENT_STATIC_OBJECT);
+    return p;
+  }
+}
+
+namespace getfem
+{
+
+  void torus_fem::init()
+  {
+    cvr = poriginal_fem_->ref_convex(0);
+    dim_ = cvr->structure()->dim();
+    is_equiv = real_element_defined = true;
+    is_polycomp = is_pol = is_lag = false;
+    es_degree = 5;
+    ntarget_dim = 3;
+
+    std::stringstream nm;
+    nm << "FEM_TORUS(" << poriginal_fem_->debug_name() << ")";
+    debug_name_ = nm.str();
+
+    init_cvs_node();
+    GMM_ASSERT1(poriginal_fem_->target_dim() == 1, "Vectorial fems not supported");
+    size_type nb_dof_origin = poriginal_fem_->nb_dof(0);
+    for (size_type k = 0; k < nb_dof_origin; ++k)
+    {
+      for(size_type j = 0; j < 3; ++j){
+        add_node(xfem_dof(poriginal_fem_->dof_types()[k], j + k*3), 
+          poriginal_fem_->node_of_dof(0, k));
+      }
+    }
+  }
+
+  size_type torus_fem::index_of_global_dof
+    (size_type /*cv*/, size_type i) const
+  { return i; }
+
+  void torus_fem::base_value(const base_node &, base_tensor &) const
+  { GMM_ASSERT1(false, "No base values, real only element."); }
+
+  void torus_fem::grad_base_value(const base_node &, base_tensor &) const
+  { GMM_ASSERT1(false, "No grad values, real only element."); }
+
+  void torus_fem::hess_base_value(const base_node &, base_tensor &) const{ 
+    GMM_ASSERT1(false, "No hess values, real only element.");
+  }
+
+  void torus_fem::real_base_value(const fem_interpolation_context& c,
+    base_tensor &t, bool) const{
+
+      GMM_ASSERT1(!(poriginal_fem_->is_on_real_element()), "Original FEM must not be real.");
+
+      base_tensor u;
+      poriginal_fem_->base_value(c.xref(), t);
+      if (!(poriginal_fem_->is_equivalent()))
+      { 
+        u = t; 
+        t.mat_transp_reduction(u, c.M(), 0); 
+      }
+  }
+
+  void torus_fem::real_grad_base_value
+    (const getfem::fem_interpolation_context& c, base_tensor &t, bool) const 
+  {
+    GMM_ASSERT1(!(poriginal_fem_->is_on_real_element()), "Original FEM must not be real.");
+
+    base_tensor u2d;
+    base_tensor u3d;
+    base_tensor v;
+    poriginal_fem_->grad_base_value(c.xref(), u2d);
+    GMM_ASSERT1(!u2d.empty(), "Original FEM is unable to provide grad base value!");
+
+    base_tensor n2d;
+    poriginal_fem_->base_value(c.xref(), n2d);
+    GMM_ASSERT1(!u2d.empty(), "Original FEM is unable to provide base value!");
+
+    bgeot::multi_index tensorSize(u2d.sizes());
+    tensorSize[1] = 1;
+    tensorSize[2] += 1;
+    u3d.adjust_sizes(tensorSize);
+    for(int i = 0; i < u2d.sizes()[0]; ++i)
+    {
+      for(int j = 0; j < tensorSize[1]; ++j)
+      {
+        if(j < 2)
+        {
+          for(int k = 0; k < u2d.sizes()[2]; ++k)
+          {
+            u3d(i,j,k) = u2d(i,0,k);
+          }
+        }
+        else u3d(i,j,2) = n2d[i] / c.xref()[0];
+      }
+    }
+    t.mat_transp_reduction(u3d, c.B(), 2);
+  }
+
+  void torus_fem::real_hess_base_value
+    (const getfem::fem_interpolation_context &c, base_tensor &t, bool) const 
+  {
+    GMM_ASSERT1(false, "Hessian not yet implemented in torus fem.");
+  }
+
+  static bgeot::size_type key_count = 0;
+  DAL_SIMPLE_KEY(torus_fem_key, bgeot::size_type);
+
+  getfem::pfem new_torus_fem(getfem::pfem pfem){
+    ++key_count;
+    getfem::pfem pfem_torus= new torus_fem(pfem);
+    dal::add_stored_object(new torus_fem_key(key_count), pfem_torus);
+    return pfem_torus;
+  }
+
+  void del_torus_fem(getfem::pfem pfem){    
+    const torus_fem *ptorus_fem = dynamic_cast<const torus_fem*>(pfem.get());
+    if(ptorus_fem != nullptr) dal::del_stored_object(pfem);
+  }
+
+  void torus_mesh_fem::adapt_to_torus(){
+
+    for (dal::bv_visitor cv(linked_mesh().convex_index()); !cv.finished(); ++cv){
+      pfem poriginal_fem = fem_of_element(cv);
+      if(poriginal_fem == nullptr) continue;
+
+      del_torus_fem(poriginal_fem);
+
+      pfem pfem_torus = new_torus_fem(poriginal_fem);
+      set_finite_element(cv, pfem_torus);
+    }
+    touch();
+  }
+
+  void torus_mesh::adapt(const getfem::mesh &original_mesh){
+    clear();
+    GMM_ASSERT1(original_mesh.dim() == 2, "Adapting torus feature must be a 2d mesh");
+    mesh::copy_from(original_mesh);
+    adapt();
+  }
+
+  void torus_mesh::adapt(){
+
+    bgeot::node_tab node_tab_copy(pts);
+    this->pts.clear();
+    for(size_type pt = 0; pt < node_tab_copy.size(); ++pt){
+      node_tab_copy[pt].resize(3);
+      this->pts.add_node(node_tab_copy[pt]);
+    }
+
+    for(size_type i = 0; i < this->convex_tab.size(); ++i){
+      bgeot::pconvex_structure pstructure 
+        = bgeot::torus_structure_descriptor(convex_tab[i].cstruct);
+      convex_tab[i].cstruct = pstructure;
+    }
+
+    for(size_type i = 0; i < this->gtab.size(); ++i){
+      bgeot::pgeometric_trans pgeom_trans = bgeot::torus_geom_trans_descriptor(gtab[i]);
+      gtab[i] = pgeom_trans;
+    }
+  }
+}
