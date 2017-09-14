2b4e0ca5ea94e35bf8fbfa827f7ce6cada6ce951
mb <mb@plaxis.com>
2017-09-14 11:09:58 +0200

    Add getter for auto_basic.

--- src/getfem/bgeot_convex_ref.h
+++ src/getfem/bgeot_convex_ref.h
@@ -108,6 +108,7 @@ namespace bgeot {
      *  positive in the other side.
      */
     virtual scalar_type is_in_face(short_type, const base_node &) const =0;
+    bool is_basic() const;
     /// return the normal vector for each face.
     const std::vector<base_small_vector> &normals() const
     { return normals_; }
