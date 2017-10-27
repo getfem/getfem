121fc5709c7ef14533e5684987f99a40d2032ada
Yves Renard <Yves.Renard@insa-lyon.fr>
2017-10-27 21:07:13 +0200

    work in progress

--- interface/tests/matlab/demo_large_sliding_contact.m
+++ interface/tests/matlab/demo_large_sliding_contact.m
@@ -19,7 +19,7 @@
 clear all;
 gf_workspace('clear all');
 
-test_case = 0; % 0 = 2D punch on a rigid obstacle
+test_case = 3; % 0 = 2D punch on a rigid obstacle
                % 1 = 2D punch on a deformable obstacle (one slave, one master)
                % 2 = 2D with two different meshes
                % 3 = 2D with multi-body and only one mesh
@@ -33,7 +33,7 @@ f_coeff = 0.5;              % Friction coefficient
 
 test_tangent_matrix = false;
 nonlinear_elasticity = false;
-max_iter = 50;
+max_iter = 100;
 draw_mesh = false;
 do_plot = true;
 generic_assembly_contact_brick = true;
