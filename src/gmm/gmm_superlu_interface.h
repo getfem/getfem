a73bdd2f44c268e23eb4d5215ac33798a0ee4234
Konstantinos Poulios <logari81@gmail.com>
2025-01-06 17:39:45 +0100

    One more effort to tide up the SuperLU interface, fixes clang build

--- src/gmm/gmm_superlu_interface.h
+++ src/gmm/gmm_superlu_interface.h
@@ -55,138 +55,201 @@ typedef int int_t;
 # undef EMPTY
 #endif
 
+namespace SuperLU {
 #if defined(GMM_NO_SUPERLU_INCLUDE_SUBDIR)
-
   #include "slu_Cnames.h"
   #include "supermatrix.h"
   #include "slu_util.h"
-
-  namespace SuperLU_S {
-  #include "slu_sdefs.h"
-  }
-  namespace SuperLU_D {
-  #include "slu_ddefs.h"
-  }
-  namespace SuperLU_C {
-  #include "slu_cdefs.h"
-  }
-  namespace SuperLU_Z {
-  #include "slu_zdefs.h"
-  }
-
+  #include "slu_scomplex.h"
+  #include "slu_dcomplex.h"
 #else
-
   #include "superlu/slu_Cnames.h"
   #include "superlu/supermatrix.h"
   #include "superlu/slu_util.h"
-
-  namespace SuperLU_S {
-  #include "superlu/slu_sdefs.h"
-  }
-  namespace SuperLU_D {
-  #include "superlu/slu_ddefs.h"
-  }
-  namespace SuperLU_C {
-  #include "superlu/slu_cdefs.h"
-  }
-  namespace SuperLU_Z {
-  #include "superlu/slu_zdefs.h"
-  }
-
+  #include "superlu/slu_scomplex.h"
+  #include "superlu/slu_dcomplex.h"
 #endif
-
-#if (SUPERLU_MAJOR_VERSION > 6)
-#  define SuperLuComplexFloat SuperLU_C::singlecomplex
-#else
-#  define SuperLuComplexFloat SuperLU_C::complex
-#endif
+  // Hard copy from {s,d,c,z}lu_sdefs.h headers from SuperLU version 7
+  // This is a fragile solution, but SuperLU headers contaminate the
+  // global namespace too much (breaking linking with BLAS for clang).
+  static_assert(SUPERLU_MAJOR_VERSION >= 7,
+                "GMM library supports only SuperLU version 7 or later");
+  extern "C"{
+  extern void
+  sgssv(superlu_options_t *, SuperMatrix *, int *, int *, SuperMatrix *,
+        SuperMatrix *, SuperMatrix *, SuperLUStat_t *, int_t *info);
+  extern void
+  dgssv(superlu_options_t *, SuperMatrix *, int *, int *, SuperMatrix *,
+        SuperMatrix *, SuperMatrix *, SuperLUStat_t *, int_t *info);
+  extern void
+  cgssv(superlu_options_t *, SuperMatrix *, int *, int *, SuperMatrix *,
+        SuperMatrix *, SuperMatrix *, SuperLUStat_t *, int_t *info);
+  extern void
+  zgssv(superlu_options_t *, SuperMatrix *, int *, int *, SuperMatrix *,
+        SuperMatrix *, SuperMatrix *, SuperLUStat_t *, int_t *info);
+  extern void
+  sgssvx(superlu_options_t *, SuperMatrix *, int *, int *, int *,
+         char *, float *, float *, SuperMatrix *, SuperMatrix *,
+         void *, int_t lwork, SuperMatrix *, SuperMatrix *,
+         float *, float *, float *, float *,
+         GlobalLU_t *, mem_usage_t *, SuperLUStat_t *, int_t *info);
+  extern void
+  dgssvx(superlu_options_t *, SuperMatrix *, int *, int *, int *,
+         char *, double *, double *, SuperMatrix *, SuperMatrix *,
+         void *, int_t lwork, SuperMatrix *, SuperMatrix *,
+         double *, double *, double *, double *,
+         GlobalLU_t *, mem_usage_t *, SuperLUStat_t *, int_t *info);
+  extern void
+  cgssvx(superlu_options_t *, SuperMatrix *, int *, int *, int *,
+         char *, float *, float *, SuperMatrix *, SuperMatrix *,
+         void *, int_t lwork, SuperMatrix *, SuperMatrix *,
+         float *, float *, float *, float *,
+         GlobalLU_t *, mem_usage_t *, SuperLUStat_t *, int_t *info);
+  extern void
+  zgssvx(superlu_options_t *, SuperMatrix *, int *, int *, int *,
+         char *, double *, double *, SuperMatrix *, SuperMatrix *,
+         void *, int_t lwork, SuperMatrix *, SuperMatrix *,
+         double *, double *, double *, double *,
+         GlobalLU_t *, mem_usage_t *, SuperLUStat_t *, int_t *info);
+  extern void
+  sCreate_CompCol_Matrix(SuperMatrix *, int, int, int_t, float *,
+                         int_t *, int_t *, Stype_t, Dtype_t, Mtype_t);
+  extern void
+  dCreate_CompCol_Matrix(SuperMatrix *, int, int, int_t, double *,
+                         int_t *, int_t *, Stype_t, Dtype_t, Mtype_t);
+  extern void
+  cCreate_CompCol_Matrix(SuperMatrix *, int, int, int_t, singlecomplex *,
+                         int_t *, int_t *, Stype_t, Dtype_t, Mtype_t);
+  extern void
+  zCreate_CompCol_Matrix(SuperMatrix *, int, int, int_t, doublecomplex *,
+                         int_t *, int_t *, Stype_t, Dtype_t, Mtype_t);
+  extern void
+  sCreate_Dense_Matrix(SuperMatrix *, int, int, float *, int,
+                       Stype_t, Dtype_t, Mtype_t);
+  extern void
+  dCreate_Dense_Matrix(SuperMatrix *, int, int, double *, int,
+                       Stype_t, Dtype_t, Mtype_t);
+  extern void
+  cCreate_Dense_Matrix(SuperMatrix *, int, int, singlecomplex *, int,
+                       Stype_t, Dtype_t, Mtype_t);
+  extern void
+  zCreate_Dense_Matrix(SuperMatrix *, int, int, doublecomplex *, int,
+                       Stype_t, Dtype_t, Mtype_t);
+  }
+}
 
 namespace gmm {
 
   /*  interface for Create_CompCol_Matrix */
 
-  inline void Create_CompCol_Matrix(SuperMatrix *A, int m, int n, int nnz,
-                                    float *a, int *ir, int *jc) {
-    SuperLU_S::sCreate_CompCol_Matrix(A, m, n, nnz, a, ir, jc,
-                                      SLU_NC, SLU_S, SLU_GE);
+  inline void Create_CompCol_Matrix(SuperLU::SuperMatrix *A, int m, int n,
+                                    int nnz, float *a, int *ir, int *jc) {
+    SuperLU::sCreate_CompCol_Matrix(A, m, n, nnz, a, ir, jc,
+                                    SuperLU::SLU_NC, SuperLU::SLU_S,
+                                    SuperLU::SLU_GE);
   }
 
-  inline void Create_CompCol_Matrix(SuperMatrix *A, int m, int n, int nnz,
-                                    double *a, int *ir, int *jc) {
-    SuperLU_D::dCreate_CompCol_Matrix(A, m, n, nnz, a, ir, jc,
-                                      SLU_NC, SLU_D, SLU_GE);
+  inline void Create_CompCol_Matrix(SuperLU::SuperMatrix *A, int m, int n,
+                                    int nnz, double *a, int *ir, int *jc) {
+    SuperLU::dCreate_CompCol_Matrix(A, m, n, nnz, a, ir, jc,
+                                    SuperLU::SLU_NC, SuperLU::SLU_D,
+                                    SuperLU::SLU_GE);
   }
 
-  inline void Create_CompCol_Matrix(SuperMatrix *A, int m, int n, int nnz,
-                                    std::complex<float> *a, int *ir, int *jc) {
-    SuperLU_C::cCreate_CompCol_Matrix(A, m, n, nnz, (SuperLuComplexFloat *)(a),
-                                      ir, jc, SLU_NC, SLU_C, SLU_GE);
+  inline void Create_CompCol_Matrix(SuperLU::SuperMatrix *A, int m, int n,
+                                    int nnz, std::complex<float> *a,
+                                    int *ir, int *jc) {
+    SuperLU::cCreate_CompCol_Matrix(A, m, n, nnz,
+                                    (SuperLU::singlecomplex *)(a),
+                                    ir, jc, SuperLU::SLU_NC, SuperLU::SLU_C,
+                                    SuperLU::SLU_GE);
   }
 
-  inline void Create_CompCol_Matrix(SuperMatrix *A, int m, int n, int nnz,
-                             std::complex<double> *a, int *ir, int *jc) {
-    SuperLU_Z::zCreate_CompCol_Matrix(A, m, n, nnz,
-                                      (SuperLU_Z::doublecomplex *)(a), ir, jc,
-                                      SLU_NC, SLU_Z, SLU_GE);
+  inline void Create_CompCol_Matrix(SuperLU::SuperMatrix *A, int m, int n,
+                                    int nnz, std::complex<double> *a,
+                                    int *ir, int *jc) {
+    SuperLU::zCreate_CompCol_Matrix(A, m, n, nnz,
+                                    (SuperLU::doublecomplex *)(a), ir, jc,
+                                    SuperLU::SLU_NC, SuperLU::SLU_Z,
+                                    SuperLU::SLU_GE);
   }
 
   /*  interface for Create_Dense_Matrix */
 
-  inline void Create_Dense_Matrix(SuperMatrix *A, int m, int n, float *a, int k)
-  { SuperLU_S::sCreate_Dense_Matrix(A, m, n, a, k, SLU_DN, SLU_S, SLU_GE); }
-  inline void Create_Dense_Matrix(SuperMatrix *A, int m, int n, double *a,
-                                  int k)
-  { SuperLU_D::dCreate_Dense_Matrix(A, m, n, a, k, SLU_DN, SLU_D, SLU_GE); }
-  inline void Create_Dense_Matrix(SuperMatrix *A, int m, int n,
-                                  std::complex<float> *a, int k) {
-    SuperLU_C::cCreate_Dense_Matrix(A, m, n, (SuperLuComplexFloat *)(a),
-                                    k, SLU_DN, SLU_C, SLU_GE);
+  inline void Create_Dense_Matrix(SuperLU::SuperMatrix *A, int m, int n,
+                                  float *a, int k) {
+    SuperLU::sCreate_Dense_Matrix(A, m, n, a, k, SuperLU::SLU_DN,
+                                                 SuperLU::SLU_S,
+                                                 SuperLU::SLU_GE);
   }
-  inline void Create_Dense_Matrix(SuperMatrix *A, int m, int n,
+  inline void Create_Dense_Matrix(SuperLU::SuperMatrix *A, int m, int n,
+                                  double *a, int k) {
+    SuperLU::dCreate_Dense_Matrix(A, m, n, a, k, SuperLU::SLU_DN,
+                                                 SuperLU::SLU_D,
+                                                 SuperLU::SLU_GE);
+  }
+  inline void Create_Dense_Matrix(SuperLU::SuperMatrix *A, int m, int n,
+                                  std::complex<float> *a, int k) {
+    SuperLU::cCreate_Dense_Matrix(A, m, n,
+                                  (SuperLU::singlecomplex *)(a),
+                                  k, SuperLU::SLU_DN, SuperLU::SLU_C,
+                                  SuperLU::SLU_GE);
+  }
+  inline void Create_Dense_Matrix(SuperLU::SuperMatrix *A, int m, int n,
                                   std::complex<double> *a, int k) {
-    SuperLU_Z::zCreate_Dense_Matrix(A, m, n, (SuperLU_Z::doublecomplex *)(a),
-                                    k, SLU_DN, SLU_Z, SLU_GE);
+    SuperLU::zCreate_Dense_Matrix(A, m, n, (SuperLU::doublecomplex *)(a),
+                                  k, SuperLU::SLU_DN, SuperLU::SLU_Z,
+                                  SuperLU::SLU_GE);
   }
 
   /*  interface for gssv */
 
-#define DECL_GSSV(NAMESPACE,FNAME,KEYTYPE) \
-  inline void SuperLU_gssv(superlu_options_t *options, SuperMatrix *A, int *p, \
-  int *q, SuperMatrix *L, SuperMatrix *U, SuperMatrix *B,               \
-  SuperLUStat_t *stats, int *info, KEYTYPE) {                           \
-  NAMESPACE::FNAME(options, A, p, q, L, U, B, stats, info);             \
+#define DECL_GSSV(FNAME,KEYTYPE) \
+  inline void SuperLU_gssv(SuperLU::superlu_options_t *options,     \
+                           SuperLU::SuperMatrix *A, int *p, int *q, \
+                           SuperLU::SuperMatrix *L,                 \
+                           SuperLU::SuperMatrix *U,                 \
+                           SuperLU::SuperMatrix *B,                 \
+                           SuperLU::SuperLUStat_t *stats,           \
+                           int *info, KEYTYPE) {                    \
+  SuperLU::FNAME(options, A, p, q, L, U, B, stats, info);           \
   }
 
-  DECL_GSSV(SuperLU_S,sgssv, float)
-  DECL_GSSV(SuperLU_C,cgssv, std::complex<float>)
-  DECL_GSSV(SuperLU_D,dgssv, double)
-  DECL_GSSV(SuperLU_Z,zgssv, std::complex<double>)
+  DECL_GSSV(sgssv, float)
+  DECL_GSSV(cgssv, std::complex<float>)
+  DECL_GSSV(dgssv, double)
+  DECL_GSSV(zgssv, std::complex<double>)
 
   /*  interface for gssvx */
 
-#define DECL_GSSVX(NAMESPACE,FNAME,FLOATTYPE,KEYTYPE) \
-  inline float SuperLU_gssvx(superlu_options_t *options, SuperMatrix *A, \
-                             int *perm_c, int *perm_r, int *etree,       \
-                             char *equed,                                \
-                             FLOATTYPE *R, FLOATTYPE *C, SuperMatrix *L, \
-                             SuperMatrix *U, void *work, int lwork,      \
-                             SuperMatrix *B, SuperMatrix *X,             \
-                             FLOATTYPE *recip_pivot_growth,              \
-                             FLOATTYPE *rcond, FLOATTYPE *ferr,          \
-                             FLOATTYPE *berr,                            \
-                             SuperLUStat_t *stats, int *info, KEYTYPE) { \
-    mem_usage_t mem_usage;                                               \
-    GlobalLU_t Glu;                                                      \
-    NAMESPACE::FNAME(options, A, perm_c, perm_r, etree, equed, R, C, L,  \
-                     U, work, lwork, B, X, recip_pivot_growth, rcond,    \
-                     ferr, berr, &Glu, &mem_usage, stats, info);         \
-    return mem_usage.for_lu; /* bytes used by the factor storage */      \
+#define DECL_GSSVX(FNAME,FLOATTYPE,KEYTYPE) \
+  inline float SuperLU_gssvx(SuperLU::superlu_options_t *options,     \
+                             SuperLU::SuperMatrix *A,                 \
+                             int *perm_c, int *perm_r, int *etree,    \
+                             char *equed,                             \
+                             FLOATTYPE *R, FLOATTYPE *C,              \
+                             SuperLU::SuperMatrix *L,                 \
+                             SuperLU::SuperMatrix *U,                 \
+                             void *work, int lwork,                   \
+                             SuperLU::SuperMatrix *B,                 \
+                             SuperLU::SuperMatrix *X,                 \
+                             FLOATTYPE *recip_pivot_growth,           \
+                             FLOATTYPE *rcond, FLOATTYPE *ferr,       \
+                             FLOATTYPE *berr,                         \
+                             SuperLU::SuperLUStat_t *stats,           \
+                             int *info, KEYTYPE) {                    \
+    SuperLU::mem_usage_t mem_usage;                                            \
+    SuperLU::GlobalLU_t Glu;                                                   \
+    SuperLU::FNAME(options, A, perm_c, perm_r, etree, equed, R, C, L, \
+                     U, work, lwork, B, X, recip_pivot_growth, rcond, \
+                     ferr, berr, &Glu, &mem_usage, stats, info);      \
+    return mem_usage.for_lu; /* bytes used by the factor storage */   \
   }
 
-  DECL_GSSVX(SuperLU_S, sgssvx,  float, float)
-  DECL_GSSVX(SuperLU_C, cgssvx,  float, std::complex<float>)
-  DECL_GSSVX(SuperLU_D, dgssvx, double, double)
-  DECL_GSSVX(SuperLU_Z, zgssvx, double, std::complex<double>)
+  DECL_GSSVX(sgssvx,  float, float)
+  DECL_GSSVX(cgssvx,  float, std::complex<float>)
+  DECL_GSSVX(dgssvx, double, double)
+  DECL_GSSVX(zgssvx, double, std::complex<double>)
 
   /* ********************************************************************* */
   /*   SuperLU solve interface                                             */
@@ -217,20 +280,20 @@ namespace gmm {
       GMM_WARNING2("CAUTION : it seems that SuperLU has a problem"
                    " for nearly dense sparse matrices");
 
-    superlu_options_t options;
+    SuperLU::superlu_options_t options;
     set_default_options(&options);
-    options.ColPerm = NATURAL;
-    options.PrintStat = NO;
-    options.ConditionNumber = YES;
+    options.ColPerm = SuperLU::NATURAL;
+    options.PrintStat = SuperLU::NO;
+    options.ConditionNumber = SuperLU::YES;
     switch (permc_spec) {
-    case 1 : options.ColPerm = MMD_ATA; break;
-    case 2 : options.ColPerm = MMD_AT_PLUS_A; break;
-    case 3 : options.ColPerm = COLAMD; break;
+    case 1 : options.ColPerm = SuperLU::MMD_ATA; break;
+    case 2 : options.ColPerm = SuperLU::MMD_AT_PLUS_A; break;
+    case 3 : options.ColPerm = SuperLU::COLAMD; break;
     }
-    SuperLUStat_t stat;
+    SuperLU::SuperLUStat_t stat;
     StatInit(&stat);
 
-    SuperMatrix SA, SL, SU, SB, SX; // SuperLU format.
+    SuperLU::SuperMatrix SA, SL, SU, SB, SX; // SuperLU format.
     Create_CompCol_Matrix(&SA, m, n, nz, (T *)(&csc_A.pr[0]),
                           (int *)(&csc_A.ir[0]),
                           (int *)(&csc_A.jc[0]));
@@ -281,9 +344,9 @@ namespace gmm {
     typedef typename number_traits<T>::magnitude_type R;
 
     csc_matrix<T> csc_A;
-    mutable SuperMatrix SA, SL, SB, SU, SX;
-    mutable SuperLUStat_t stat;
-    mutable superlu_options_t options;
+    mutable SuperLU::SuperMatrix SA, SL, SB, SU, SX;
+    mutable SuperLU::SuperLUStat_t stat;
+    mutable SuperLU::superlu_options_t options;
     float memory_used;
     mutable std::vector<int> etree, perm_r, perm_c;
     mutable std::vector<R> Rscale, Cscale;
@@ -344,13 +407,13 @@ namespace gmm {
     int nz = int(nnz(csc_A));
 
     set_default_options(&options);
-    options.ColPerm = NATURAL;
-    options.PrintStat = NO;
-    options.ConditionNumber = NO;
+    options.ColPerm = SuperLU::NATURAL;
+    options.PrintStat = SuperLU::NO;
+    options.ConditionNumber = SuperLU::NO;
     switch (permc_spec) {
-      case 1 : options.ColPerm = MMD_ATA; break;
-      case 2 : options.ColPerm = MMD_AT_PLUS_A; break;
-      case 3 : options.ColPerm = COLAMD; break;
+      case 1 : options.ColPerm = SuperLU::MMD_ATA; break;
+      case 2 : options.ColPerm = SuperLU::MMD_AT_PLUS_A; break;
+      case 3 : options.ColPerm = SuperLU::COLAMD; break;
     }
     StatInit(&stat);
 
@@ -398,12 +461,12 @@ namespace gmm {
   void SuperLU_factor<T>::solve(const VECTX &X, const VECTB &B,
                                 int transp) const {
     gmm::copy(B, rhs);
-    options.Fact = FACTORED;
-    options.IterRefine = NOREFINE;
+    options.Fact = SuperLU::FACTORED;
+    options.IterRefine = SuperLU::NOREFINE;
     switch (transp) {
-      case LU_NOTRANSP: options.Trans = NOTRANS; break;
-      case LU_TRANSP: options.Trans = TRANS; break;
-      case LU_CONJUGATED: options.Trans = CONJ; break;
+      case LU_NOTRANSP: options.Trans = SuperLU::NOTRANS; break;
+      case LU_TRANSP: options.Trans = SuperLU::TRANS; break;
+      case LU_CONJUGATED: options.Trans = SuperLU::CONJ; break;
       default: GMM_ASSERT1(false, "invalid value for transposition option");
     }
     StatInit(&stat);
