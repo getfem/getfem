46e79f41980b1f0c649abb12ef588a2d7036e554
Konstantinos Poulios <logari81@gmail.com>
2026-04-15 14:30:34 +0200

    Improvements and fixes in the python version of the main GetFEM tutorial
      - Avoid use of bricks and move to modern GWFL expressions,
      - Fix mistakes related to the plate thickness
      - Avoid unnecessary intermediate parameters
      - Fix calculation of Von Mises
      - Documentation, and C++, Matlab/Octave/Scilab codes still need to be
        updated accordingly

--- interface/tests/python/demo_thermo_elasticity_electrical_coupling.py
+++ interface/tests/python/demo_thermo_elasticity_electrical_coupling.py
@@ -22,7 +22,6 @@
 ############################################################################
 
 import numpy as np
-
 import getfem as gf
 
 # Deformation of a plate under the coupling of thermal, elasticity, and
@@ -39,34 +38,33 @@ import getfem as gf
 #
 # Elastic problem: The plate is clamped at rhe left boundary and a
 #   traction density of force F is prescribed at the right boundary.
+#   Plane stress conditions are assumed.
 # Electric problem: The potential is prescribed to be 0V at the right
 #   boundary and 0.1V at the left boundary.
 # Thermal problem: A thermal insulation condition is prescribed at the
-#   left and hole boudnaries. The remaining boundary and the plate itself
-#   is supposed to be submitted to heat transfer with respect to the
-#   air at 20oC.
+#   left and hole boudnaries. The remaining boundary and the plate front
+#   and back surfaces are supposed to be transfer heat by convection
+#   with respect to the surrounding air at 20 deg C.
 # Coupling terms:
-#   - Joule heating: source term  sigma|Grad_V|^2
-#   - Dependance of the thermal conductivity in temperature :
-#     sigma = 1/(rho_0(1+alpha(theta-T0)))
-#     with T0 = 20oC, rho_0 the resistance temperature coefficient at T0
-#     and alpha the second resistance temperature coefficient.
+#   - Joule heating: source term  1/rho ||Grad_V||^2
+#   - Dependance of the thermal resistivity on temperature :
+#     rho = rho_0(1+alpha(T-T0))
+#     with T0 = 20 deg C, rho_0 the resistivity at T0
+#     and alpha the resistivity-temperature coefficient.
 #   - Thermal expansion:
-#     stress_tensor = clambdastar div(u) I + 2 cmu epsilon(u) - beta theta I
-#     with beta = alpha_th E/(1-2nu), alpha_th being the thermal
-#     expansion coefficient.
+#     stress_tensor = E/(1+nu) ( nu/(1-nu) (div(u) - 2 alpha_th DT) I
+#                               + (epsilon(u) - alpha_th DT I) )
+#     with alpha_th being the thermal expansion coefficient.
 # The first two coupling terms are nonlinear ones.
 
 
 #
 # Physical parameters
 #
-epsilon = 1.       # Thickness of the plate (cm)
+
+t = 1.             # Thickness of the plate (cm)
 E = 21E6           # Young Modulus (N/cm^2)
 nu = 0.3           # Poisson ratio
-clambda = E*nu/((1+nu)*(1-2*nu)) # First Lame coefficient (N/cm^2)
-cmu = E/(2*(1+nu))               # Second Lame coefficient (N/cm^2)
-clambdastar = 2*clambda*cmu/(clambda+2*cmu) # Lame coefficient for Plane stress (N/cm^2)
 F = 100E2          # Force density at the right boundary (N/cm^2)
 kappa = 4.         # Thermal conductivity (W/(cm K))
 D = 10.            # Heat transfer coefficient (W/(K cm^2))
@@ -79,15 +77,16 @@ alpha = 0.0039     # Second resistance temperature coefficient.
 #
 # Numerical parameters
 #
+
 h = 2.                    # Approximate mesh size
 elements_degree = 2       # Degree of the finite element methods
-export_mesh = True        # Draw the mesh after mesh generation or not
+export_mesh = False       # Export the mesh after mesh generation or not
 solve_in_two_steps = 2    # Solve the elasticity problem separately (1)
                           # or in a coupled way (0) or both and compare (2)
-
 #
 # Mesh generation. Meshes can also been imported from several formats.
 #
+
 mo1 = gf.MesherObject('rectangle', [0., 0.], [100., 25.])
 mo2 = gf.MesherObject('ball', [25., 12.5], 8.)
 mo3 = gf.MesherObject('ball', [50., 12.5], 8.)
@@ -102,6 +101,7 @@ mesh = gf.Mesh('generate', mo, h, 2)
 #
 # Boundary selection
 #
+
 fb1 = mesh.outer_faces_in_box([1., 1.], [99., 24.])    # Boundary of the holes
 fb2 = mesh.outer_faces_with_direction([ 1., 0.], 0.01) # Right boundary
 fb3 = mesh.outer_faces_with_direction([-1., 0.], 0.01) # Left boundary
@@ -133,9 +133,9 @@ mesh.region_merge(HOLE1_BOUND, HOLE3_BOUND)
 np.testing.assert_array_equal(mesh.region(HOLE_BOUND), mesh.region(HOLE1_BOUND))
 
 if (export_mesh):
-    mesh.export_to_vtk('mesh.vtk');
+    mesh.export_to_vtu('mesh.vtu');
     print('\nYou can view the mesh for instance with');
-    print('mayavi2 -d mesh.vtk -f ExtractEdges -m Surface \n');
+    print('mayavi2 -d mesh.vtu -f ExtractEdges -m Surface \n');
 
 #
 # Definition of finite elements methods and integration method
@@ -155,51 +155,42 @@ mim = gf.MeshIm(mesh, elements_degree*2)   # Integration method
 #
 
 md=gf.Model('real');
-md.add_fem_variable('u', mfu)       # Displacement of the structure
-md.add_fem_variable('theta', mft)   # Temperature
-md.add_fem_variable('V', mft)       # Electric potential
+md.add_fem_variable('u', mfu)   # Displacement of the structure
+md.add_fem_variable('T', mft)   # Temperature
+md.add_fem_variable('V', mft)   # Electric potential
+md.add_initialized_data('t', t)
 
 # Membrane elastic deformation
-md.add_initialized_data('cmu', [cmu])
-md.add_initialized_data('clambdastar', [clambdastar])
-md.add_isotropic_linearized_elasticity_brick(mim, 'u', 'clambdastar', 'cmu')
-
+md.add_initialized_data('E', E)
+md.add_initialized_data('nu', nu)
+md.add_initialized_data('alpha_th', alpha_th)
+md.add_initialized_data('T0', T0)
+md.add_macro('sigma',
+             'E/(1+nu)*( nu/(1-nu)*(Div(u)-3*alpha_th*T)*Id(2)'
+                       '+(Sym(Grad(u))-alpha_th*T*Id(2)) )')
+md.add_linear_term(mim, 't*sigma:Grad(Test_u)')
 md.add_Dirichlet_condition_with_multipliers(mim, 'u', elements_degree-1, LEFT_BOUND)
-md.add_initialized_data('Fdata', [F*epsilon, 0])
-md.add_source_term_brick(mim, 'u', 'Fdata', RIGHT_BOUND)
+md.add_initialized_data('F', F)
+md.add_linear_term(mim, '-t*F*Test_u(1)', RIGHT_BOUND)
 
-# Electrical field
-sigmaeps = '(eps/(rho_0*(1+alpha*(theta-T0))))'
-md.add_initialized_data('eps', [epsilon])
-md.add_initialized_data('rho_0', [rho_0])
-md.add_initialized_data('alpha', [alpha])
-md.add_initialized_data('T0', [T0])
-md.add_nonlinear_term(mim, sigmaeps+'*(Grad_V.Grad_Test_V)')
+# Electric field
+md.add_initialized_data('rho_0', rho_0)
+md.add_initialized_data('alpha', alpha)
+md.add_macro('rho', 'rho_0*(1+alpha*(T-T0))')
+md.add_nonlinear_term(mim, 't/rho * Grad(V).Grad(Test_V)')
 md.add_Dirichlet_condition_with_multipliers(mim, 'V', elements_degree-1, RIGHT_BOUND)
-md.add_initialized_data('DdataV', [0.1])
+md.add_initialized_data('DdataV', 0.1)
 md.add_Dirichlet_condition_with_multipliers(mim, 'V', elements_degree-1, LEFT_BOUND, 'DdataV')
 
 # Thermal problem
-md.add_initialized_data('kaeps', [kappa*epsilon])
-md.add_generic_elliptic_brick(mim, 'theta', 'kaeps')
-md.add_initialized_data('D2', [D*2])
-md.add_initialized_data('D2airt', [air_temp*D*2])
-md.add_mass_brick(mim, 'theta', 'D2')
-md.add_source_term_brick(mim, 'theta', 'D2airt')
-md.add_initialized_data('Deps', [D/epsilon])
-md.add_initialized_data('Depsairt', [air_temp*D/epsilon])
-md.add_Fourier_Robin_brick(mim, 'theta', 'Deps', TOP_BOUND)
-md.add_source_term_brick(mim, 'theta', 'Depsairt', TOP_BOUND)
-md.add_Fourier_Robin_brick(mim, 'theta', 'Deps', BOTTOM_BOUND)
-md.add_source_term_brick(mim, 'theta', 'Depsairt', BOTTOM_BOUND)
-
+md.add_initialized_data('kappaT', kappa)
+md.add_initialized_data('D', D)
+md.add_initialized_data('T_air', air_temp)
+md.add_linear_term(mim, 't*kappaT*Grad(T).Grad(Test_T) + 2*D*(T-T_air)*Test_T')
+md.add_linear_term(mim, 't*D*(T-T_air).Test_T', TOP_BOUND)
+md.add_linear_term(mim, 't*D*(T-T_air).Test_T', BOTTOM_BOUND)
 # Joule heating term
-md.add_nonlinear_term(mim, '-'+sigmaeps+'*Norm_sqr(Grad_V)*Test_theta')
-
-# Thermal expansion term
-md.add_initialized_data('beta', [alpha_th*E/(1-2*nu)])
-md.add_linear_term(mim, 'beta*(T0-theta)*Trace(Grad_Test_u)')
-
+md.add_nonlinear_term(mim, '-t/rho * Norm_sqr(Grad(V))*Test_T')
 
 #
 # Model solve
@@ -210,11 +201,11 @@ if (solve_in_two_steps >= 1):
   print('First problem with', md.nbdof(), ' dofs')
   md.solve('max_res', 1E-9, 'max_iter', 100, 'noisy')
   md.enable_variable('u')
-  md.disable_variable('theta')
+  md.disable_variable('T')
   md.disable_variable('V')
   print('Second problem with ', md.nbdof(), ' dofs')
   md.solve('max_res', 1E-9, 'max_iter', 100, 'noisy')
-  md.enable_variable('theta')
+  md.enable_variable('T')
   md.enable_variable('V')
   U1 = md.variable('u')
   
@@ -229,20 +220,21 @@ if (solve_in_two_steps == 2):
   U2 = md.variable('u')
   print (np.linalg.norm(U2-U1));
   if (np.linalg.norm(U2-U1) > 1E-10):
-      print("Too big difference between solve in one and two steps"); exit(1)
+      print("Too big difference between solve in one and two steps")
+      exit(1)
 
 #
 # Solution export
-#  
-U = md.variable('u')
-V = md.variable('V')
-THETA = md.variable('theta')
-VM = md.compute_isotropic_linearized_Von_Mises_or_Tresca('u', 'clambdastar', 'cmu', mfvm)
-CO = np.reshape(md.interpolation('-'+sigmaeps+'*Grad_V', mfvm), (2, mfvm.nbdof()), 'F')
+#
 
-mfvm.export_to_vtk('displacement_with_von_mises.vtk', mfvm,  VM, 'Von Mises Stresses', mfu, U, 'Displacements')
-print('You can view solutions with for instance:\nmayavi2 -d displacement_with_von_mises.vtk -f WarpVector -m Surface')
-mft.export_to_vtk('temperature.vtk', mft, THETA, 'Temperature')
-print('mayavi2 -d temperature.vtk -f WarpScalar -m Surface')
-mft.export_to_vtk('electric_potential.vtk', mft, V, 'Electric potential')
-print('mayavi2 -d electric_potential.vtk -f WarpScalar -m Surface')
+#VM = md.compute_isotropic_linearized_Von_Mises_or_Tresca('u', 'clambdastar', 'cmu', mfvm) #wrong
+VM = md.local_projection(mim, "sqrt(Norm_sqr(sigma)+sqr(sigma(1,2))-sigma(1,1)*sigma(2,2))", mfvm)
+#VM = md.local_projection(mim, "sqrt(1.5)*Norm_sqr([1,0;0,1;0,0]*sigma*[1,0,0;0,1,0]-1/3*Trace(sigma)*Id(3))", mfvm)
+CO = md.interpolation('-t/rho * Grad(V)', mfvm).reshape(2, mfvm.nbdof(), order='F')
+mfvm.export_to_vtu('displacement_with_von_mises.vtu', mfvm,  VM, 'Von Mises Stresses',
+                                                      mfu, md.variable('u'), 'Displacements')
+mft.export_to_vtu('temperature.vtu', mft, md.variable('T'), 'Temperature')
+mft.export_to_vtu('electric_potential.vtu', mft, md.variable('V'), 'Electric potential')
+print('You can view solutions with for instance:\nmayavi2 -d displacement_with_von_mises.vtu -f WarpVector -m Surface')
+print('mayavi2 -d temperature.vtu -f WarpScalar -m Surface')
+print('mayavi2 -d electric_potential.vtu -f WarpScalar -m Surface')
